fadcf80ae44cb01a476b9da6907434e7d6311615
pebers <pebers@thoughtmachine.net>
2016-02-09 19:13:43 +0000

    Another test

---
+++ src/core/lock_test.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"io/ioutil"
+	"os"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAcquireRepoLock(t *testing.T) {
+	// Grab the lock
+	AcquireRepoLock()
+	// Now we should be able to open the file (ie. it exists)
+	lockFile, err := os.Open(lockFilePath)
+	assert.NoError(t, err)
+	defer lockFile.Close()
+	assert.Error(t, syscall.Flock(int(lockFile.Fd()), syscall.LOCK_EX|syscall.LOCK_NB))
+	// Let it go again
+	ReleaseRepoLock()
+	// Now we can get it
+	assert.NoError(t, syscall.Flock(int(lockFile.Fd()), syscall.LOCK_EX|syscall.LOCK_NB))
+	// Let it go so the following tests aren't confused :)
+	assert.NoError(t, syscall.Flock(int(lockFile.Fd()), syscall.LOCK_UN))
+}
+
+func TestReadLastOperation(t *testing.T) {
+	assert.NoError(t, ioutil.WriteFile(lockFilePath, []byte("op plz"), 0644))
+	assert.Equal(t, []string{"op", "plz"}, ReadLastOperationOrDie())
+	// Can't really test a failure case because of the "or die" bit :(
+}
