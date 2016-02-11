0e5d187d08d4001721e56327f04a98ca83ce3777
pebers <pebers@thoughtmachine.net>
2016-02-11 09:47:28 +0000

    General cleanup of bash completion code

--- src/misc/plz_complete.sh
+++ src/misc/plz_complete.sh
@@ -6,7 +6,7 @@
 _PleaseCompleteMe() {
   local cur
   COMPREPLY=()
-  _get_comp_words_by_ref -n "/:" cur
+  _get_comp_words_by_ref -n : cur
 
   if [[ "$cur" == -* ]]; then
       COMPREPLY=( $( compgen -W "`plz --help 2>&1 | grep -Eo -- '--?[a-z_]+'`" -- $cur ) )
@@ -14,13 +14,11 @@ _PleaseCompleteMe() {
       if [[ "$COMP_CWORD" == "1" ]]; then
 	  COMPREPLY=( $( compgen -W "build test cover query clean run update" -- $cur ) )
       else
-	  if [[ "$COMP_CWORD" == "2" && "${COMP_WORDS[1]}" == "query" ]]; then
-	      COMPREPLY=( $( compgen -W "somepath alltargets deps print completions affectedtests input output" -- $cur ) )
-	  else
-	      local IFS=$'\n'
-	      COMPREPLY=( $( compgen -W "`plz --noupdate -p query completions --cmd ${COMP_WORDS[1]} $cur 2>/dev/null`" -- $cur ) )
-	      unset IFS
-	  fi
+	      if [[ "$COMP_CWORD" == "2" && "${COMP_WORDS[1]}" == "query" ]]; then
+	          COMPREPLY=( $( compgen -W "somepath alltargets deps print completions affectedtests input output" -- $cur ) )
+	      else
+	          COMPREPLY=( $( compgen -W "`plz --noupdate -p query completions --cmd ${COMP_WORDS[1]} $cur 2>/dev/null`" -- $cur ) )
+	      fi
       fi
   fi
   __ltrim_colon_completions "$cur"
@@ -28,4 +26,3 @@ _PleaseCompleteMe() {
 }
 
 complete -F _PleaseCompleteMe -o filenames plz
-COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
