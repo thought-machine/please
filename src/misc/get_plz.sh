e6c293197ec4c25158842da9b425c9bac4f8fa94
Peter Ebden <pebers@thoughtmachine.net>
2016-02-13 19:18:47 +0000

    Change get_plz to look into ~/.please

--- src/misc/get_plz.sh
+++ src/misc/get_plz.sh
@@ -3,7 +3,7 @@
 # Downloads a precompiled copy of Please from our s3 bucket and installs it.
 set -eu
 
-REVISION=`curl -s https://s3-eu-west-1.amazonaws.com/please-build/latest_version`
+VERSION=`curl -s https://s3-eu-west-1.amazonaws.com/please-build/latest_version`
 # Find the os / arch to download. You can do this quite nicely with go env
 # but we use this script on machines that don't necessarily have Go itself.
 OS=`uname`
@@ -16,17 +16,22 @@ else
     exit 1
 fi
 
-PLEASE_URL="https://s3-eu-west-1.amazonaws.com/please-build/${GOOS}_amd64/${REVISION}/please.tar.gz"
+PLEASE_URL="https://s3-eu-west-1.amazonaws.com/please-build/${GOOS}_amd64/${VERSION}/please.tar.gz"
 
-if [ ! -d /opt/please ]; then
-    sudo mkdir -p /opt/please
-    sudo chown `whoami` /opt/please
+LOCATION="${HOME}/.please"
+DIR="${LOCATION}/${VERSION}"
+mkdir -p "$DIR"
+
+echo "Downloading Please ${VERSION} to ${DIR}..."
+curl -fsSL "${PLEASE_URL}" | tar -xzpf- --strip-components=1 -C "$DIR"
+# Link it all back up a dir
+for x in `ls "$DIR"`; do
+    ln -sf "${DIR}/${x}" "$LOCATION"
+done
+ln -sf "${DIR}/please" "${LOCATION}/plz"
+
+if [ ! -f /usr/local/bin/plz ]; then
+    sudo ln -sf "${LOCATION}/plz" /usr/local/bin/plz
 fi
-
-rm -f /opt/please/*[^/]
-
-curl -sSL "${PLEASE_URL}" | tar -zxpf- -C /opt/
-ln -sf /opt/please/please /usr/local/bin/plz
-
 echo "Please installed."
 plz --help
