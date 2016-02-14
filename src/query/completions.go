45ecf978c0abc6dd822f444133e58626b4ff1477
Peter Ebden <pebers@thoughtmachine.net>
2016-02-14 11:43:25 +0000

    Quick and dirty fix for #18: treat \: in build target as just :

--- src/query/completions.go
+++ src/query/completions.go
@@ -23,9 +23,10 @@ func QueryCompletionLabels(config core.Configuration, args []string, repoRoot st
 	if strings.HasSuffix(args[0], ":") {
 		args[0] += "all"
 	}
-	label := core.ParseBuildLabel(args[0], repoRoot)
+	args[0] = strings.Replace(args[0], "\\:", ":", -1)
+	labels := core.ParseBuildLabels([]string{args[0]})
 	// Return this label without the trailing bit.
-	return []core.BuildLabel{core.BuildLabel{PackageName: label.PackageName, Name: "all"}}
+	return []core.BuildLabel{core.BuildLabel{PackageName: labels[0].PackageName, Name: "all"}}
 }
 
 func queryCompletionPackages(config core.Configuration, query, repoRoot string) {
