eb587861f40b23cff3364cb14b9c48e08d0fdd94
fabian <fabian@thoughtmachine.net>
2016-02-16 13:28:31 +0000

    Adds support for go generate.

---
+++ test/go_rules/generate.go
@@ -0,0 +1,11 @@
+package generate
+//go:generate stringer -type=Pill
+type Pill int
+
+const (
+    Placebo Pill = iota
+    Aspirin
+    Ibuprofen
+    Paracetamol
+    Acetaminophen = Paracetamol
+)
---
+++ test/go_rules/test/generate.go
@@ -0,0 +1,14 @@
+package generate_test
+
+//go:generate stringer -type=Pill
+type Pill int
+
+
+
+const (
+    Placebo Pill = iota
+    Aspirin
+    Ibuprofen
+    Paracetamol
+    Acetaminophen = Paracetamol
+)
