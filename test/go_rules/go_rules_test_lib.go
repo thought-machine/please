886aec6c48685bdf9e6e374d114dc42516941492
Peter Ebden <peter.ebden@gmail.com>
2016-05-14 22:25:10 +0100

    Test case for go generate

--- test/go_rules/go_rules_test_lib.go
+++ test/go_rules/go_rules_test_lib.go
@@ -6,3 +6,13 @@ import "go_rules/test"
 func GetAnswer() int {
 	return test.GetAnswer()
 }
+
+//go:generate stringer -type=Cat
+type Cat int
+
+const (
+	Ginger Cat = iota
+	Tortoiseshell
+	Bengal
+	Halp
+)
