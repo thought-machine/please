7bf5b375db1eda46afcd8000b19e9f55b0f3f4b4
Peter Ebden <peter.ebden@gmail.com>
2016-05-15 15:26:31 +0100

    Make `query somepath` expand :all labels.

--- src/query/somepath.go
+++ src/query/somepath.go
@@ -7,11 +7,43 @@ import "core"
 // Finds and returns a path between two targets.
 // Useful for a "why on earth do I depend on this thing" type query.
 func QuerySomePath(graph *core.BuildGraph, label1 core.BuildLabel, label2 core.BuildLabel) {
-	target1 := graph.TargetOrDie(label1)
-	target2 := graph.TargetOrDie(label2)
-	if !printSomePath(graph, target1, target2) && !printSomePath(graph, target2, target1) {
+	// Awkwardly either target can be :all. This is an extremely useful idiom though so despite
+	// trickiness is worth supporting.
+	// Of course this calculation is also quadratic but it's not very obvious how to avoid that.
+	if label1.IsAllTargets() {
+		for _, target := range graph.PackageOrDie(label1.PackageName).Targets {
+			if querySomePath1(graph, target, label2, false) {
+				return
+			}
+		}
 		fmt.Printf("Couldn't find any dependency path between %s and %s\n", label1, label2)
+	} else {
+		querySomePath1(graph, graph.TargetOrDie(label1), label2, true)
 	}
+}
+
+func querySomePath1(graph *core.BuildGraph, target1 *core.BuildTarget, label2 core.BuildLabel, print bool) bool {
+	// Now we do the same for label2.
+	if label2.IsAllTargets() {
+		for _, target2 := range graph.PackageOrDie(label2.PackageName).Targets {
+			if querySomePath2(graph, target1, target2, false) {
+				return true
+			}
+		}
+		return false
+	} else {
+		return querySomePath2(graph, target1, graph.TargetOrDie(label2), print)
+	}
+}
+
+func querySomePath2(graph *core.BuildGraph, target1, target2 *core.BuildTarget, print bool) bool {
+	if !printSomePath(graph, target1, target2) && !printSomePath(graph, target2, target1) {
+		if print {
+			fmt.Printf("Couldn't find any dependency path between %s and %s\n", target1.Label, target2.Label)
+		}
+		return false
+	}
+	return true
 }
 
 // This is just a simple DFS through the graph.
