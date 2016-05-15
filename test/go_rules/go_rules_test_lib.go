ecea0c5ac2bcb6eff15b27cd05b81d9f7930647b
Peter Ebden <peter.ebden@gmail.com>
2016-05-15 13:25:58 +0100

    Fix go_generate for cases that import code within the repo (#82)
    * Test case for go generate
    
    * Fixing go_generate for cases where we import something from the local repo; needs to both add pkg/linux_amd64 and do the move-all-libs-up-one-dir trick that go_binary already does.

--- test/go_rules/go_rules_test_lib.go
+++ test/go_rules/go_rules_test_lib.go
@@ -1,8 +1,18 @@
 // Intermediate library used in this test.
 package parse
 
-import "go_rules/test"
+import "test/go_rules/test"
 
 func GetAnswer() int {
 	return test.GetAnswer()
 }
+
+//go:generate stringer -type=Cat
+type Cat int
+
+const (
+	Ginger Cat = iota
+	Tortoiseshell
+	Bengal
+	Halp
+)
