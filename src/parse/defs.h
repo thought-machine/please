018a598ce6766519f823e20df7c9b73f9d8cd1c7
Peter Ebden <peter.ebden@gmail.com>
2016-05-19 15:03:26 +0100

    attempt at pushing stuff back into C

--- src/parse/defs.h
+++ src/parse/defs.h
@@ -2,7 +2,6 @@
 // This is used by both cgo and cffi to generate their own interfaces.
 typedef unsigned char uint8;
 typedef long long int64;
-typedef char* (ParseFileCallback)(char*, char*, size_t);
 typedef size_t (AddTargetCallback)(size_t, char*, char*, char*, uint8, uint8, uint8, uint8, uint8, uint8, uint8, uint8, uint8, int64, int64, int64, char*);
 typedef char* (AddStringCallback)(size_t, char*);
 typedef char* (AddTwoStringsCallback)(size_t, char*, char*);
@@ -12,46 +11,11 @@ typedef char* (AddOutputCallback)(size_t, char*, char*);
 typedef char** (GlobCallback)(char*, char**, long long, char**, long long, uint8);
 typedef char* (GetIncludeFileCallback)(size_t, char*);
 typedef char** (GetLabelsCallback)(size_t, char*, char*);
-typedef void (SetConfigValueCallback)(char*, char*);
-typedef char* (PreBuildCallbackRunner)(void*, size_t, char*);
-typedef char* (PostBuildCallbackRunner)(void*, size_t, char*, char*);
 typedef void (SetBuildFunctionCallback)(void*, char*, size_t);
 typedef void (LogCallback)(int64, size_t, char*);
 typedef uint8 (ValidateCallback)(char*);
 
-struct PleaseCallbacks {
-    AddTargetCallback* add_target;
-    AddStringCallback* add_src;
-    AddStringCallback* add_data;
-    AddStringCallback* add_dep;
-    AddStringCallback* add_exported_dep;
-    AddStringCallback* add_tool;
-    AddStringCallback* add_out;
-    AddStringCallback* add_vis;
-    AddStringCallback* add_label;
-    AddStringCallback* add_hash;
-    AddStringCallback* add_licence;
-    AddStringCallback* add_test_output;
-    AddStringCallback* add_require;
-    AddTwoStringsCallback* add_provide;
-    AddTwoStringsCallback* add_named_src;
-    AddTwoStringsCallback* add_command;
-    AddTwoStringsCallback* set_container_setting;
-    GlobCallback* glob;
-    GetIncludeFileCallback* get_include_file;
-    GetIncludeFileCallback* get_subinclude_file;
-    GetLabelsCallback* get_labels;
-    SetBuildFunctionCallback* set_pre_build_function;
-    SetBuildFunctionCallback* set_post_build_function;
-    AddDependencyCallback* add_dependency;
-    AddOutputCallback* add_output;
-    AddTwoStringsCallback* add_licence_post;
-    AddThreeStringsCallback* set_command;
-    LogCallback* log;
-    ValidateCallback* is_valid_target_name;
-};
-
-extern void RegisterCallbacks(struct PleaseCallbacks*);
+extern void RegisterCallback(char*, char*, void*);
 extern char* ParseFile(char*, char*, size_t);
 extern char* ParseCode(char*, char*, size_t);
 extern void SetConfigValue(char*, char*);
--- src/parse/interpreter.c
+++ src/parse/interpreter.c
@@ -2,11 +2,13 @@
 
 #include "_cgo_export.h"
 
-static struct PleaseCallbacks callbacks;
-typedef void RegisterPypyCallbacks(struct PleaseCallbacks*);
-
 // Since we dlsym() the callbacks out of the parser .so, we have variables for them as
 // well as extern definitions which cffi uses. The two must match, of course.
+typedef void (RegisterPypyCallback)(char*, char*, void*);
+typedef char* (ParseFileCallback)(char*, char*, size_t);
+typedef void (SetConfigValueCallback)(char*, char*);
+typedef char* (PreBuildCallbackRunner)(void*, size_t, char*);
+typedef char* (PostBuildCallbackRunner)(void*, size_t, char*, char*);
 ParseFileCallback* parse_file;
 ParseFileCallback* parse_code;
 SetConfigValueCallback* set_config_value;
@@ -42,45 +44,47 @@ char* RunPostBuildFunction(size_t callback, size_t package, char* name, char* ou
 }
 
 int InitialiseInterpreter(char* parser_location) {
-  callbacks.add_target = (AddTargetCallback*)AddTarget;
-  callbacks.add_src = AddSource;
-  callbacks.add_data = AddData;
-  callbacks.add_out = AddOutput;
-  callbacks.add_dep = AddDep;
-  callbacks.add_exported_dep = AddExportedDep;
-  callbacks.add_tool = AddTool;
-  callbacks.add_vis = AddVis;
-  callbacks.add_label = AddLabel;
-  callbacks.add_hash = AddHash;
-  callbacks.add_licence = AddLicence;
-  callbacks.add_test_output = AddTestOutput;
-  callbacks.add_require = AddRequire;
-  callbacks.add_provide = AddProvide;
-  callbacks.add_named_src = AddNamedSource;
-  callbacks.add_command = AddCommand;
-  callbacks.set_container_setting = SetContainerSetting;
-  callbacks.glob = Glob;
-  callbacks.get_include_file = GetIncludeFile;
-  callbacks.get_subinclude_file = GetSubincludeFile;
-  callbacks.get_labels = GetLabels;
-  callbacks.set_pre_build_function = PreBuildFunctionSetter;
-  callbacks.set_post_build_function = PostBuildFunctionSetter;
-  callbacks.add_dependency = AddDependency;
-  callbacks.add_output = AddOutputPost;
-  callbacks.add_licence_post = AddLicencePost;
-  callbacks.set_command = SetCommand;
-  callbacks.log = Log;
-  callbacks.is_valid_target_name = IsValidTargetName;
-
   void* parser = dlopen(parser_location, RTLD_NOW | RTLD_GLOBAL);
   if (parser == NULL) {
     return 1;
   }
-  void* f = dlsym(parser, "RegisterCallbacks");
-  if (f == NULL) {
+  RegisterPypyCallback* reg = dlsym(parser, "RegisterCallback");
+  if (reg == NULL) {
     return 2;
   }
-  ((RegisterPypyCallbacks*)f)(&callbacks);
+  // TODO(pebers): it would be nicer if we could get rid of the explicit types here; something
+  //               like reg("_add_target", typeof(AddTarget), AddTarget) would be sweet.
+  //               As far as I know this is only possible in C++ using typeid though :(
+  reg("_add_target", "AddTargetCallback*", AddTarget);
+  reg("_add_src", "AddStringCallback*", AddSource);
+  reg("_add_data", "AddStringCallback*", AddData);
+  reg("_add_dep", "AddStringCallback*", AddDep);
+  reg("_add_exported_dep", "AddStringCallback*", AddExportedDep);
+  reg("_add_tool", "AddStringCallback*", AddTool);
+  reg("_add_out", "AddStringCallback*", AddOutput);
+  reg("_add_vis", "AddStringCallback*", AddVis);
+  reg("_add_label", "AddStringCallback*", AddLabel);
+  reg("_add_hash", "AddStringCallback*", AddHash);
+  reg("_add_licence", "AddStringCallback*", AddLicence);
+  reg("_add_test_output", "AddStringCallback*", AddTestOutput);
+  reg("_add_require", "AddStringCallback*", AddRequire);
+  reg("_add_provide", "AddTwoStringsCallback*", AddProvide);
+  reg("_add_named_src", "AddTwoStringsCallback*", AddNamedSource);
+  reg("_add_command", "AddTwoStringsCallback*", AddCommand);
+  reg("_set_container_setting", "AddTwoStringsCallback*", SetContainerSetting);
+  reg("_glob", "GlobCallback*", Glob);
+  reg("_get_include_file", "GetIncludeFileCallback*", GetIncludeFile);
+  reg("_get_subinclude_file", "GetIncludeFileCallback*", GetSubincludeFile);
+  reg("_get_labels", "GetLabelsCallback*", GetLabels);
+  reg("_set_pre_build_callback", "SetBuildFunctionCallback*", SetPreBuildFunction);
+  reg("_set_post_build_callback", "SetBuildFunctionCallback*", SetPostBuildFunction);
+  reg("_add_dependency", "AddDependencyCallback*", AddDependency);
+  reg("_add_output", "AddOutputCallback*", AddOutputPost);
+  reg("_add_licence_post", "AddTwoStringsCallback*", AddLicencePost);
+  reg("_set_command", "AddThreeStringsCallback*", SetCommand);
+  reg("_log", "LogCallback*", Log);
+  reg("_is_valid_target_name", "ValidateCallback*", IsValidTargetName);
+
   parse_file = dlsym(parser, "ParseFile");
   parse_code = dlsym(parser, "ParseCode");
   set_config_value = dlsym(parser, "SetConfigValue");
