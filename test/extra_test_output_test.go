bc0b6ed2301161cee93e1bb431c9a1c64405fd41
Peter Ebden <peter.ebden@gmail.com>
2016-05-24 12:23:36 +0100

    Remove testify from e2e test, it is not safe if the main plz process is building it for a different go_test somewhere else.

--- test/extra_test_output_test.go
+++ test/extra_test_output_test.go
@@ -4,15 +4,17 @@ import (
 	"encoding/base64"
 	"io/ioutil"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 const msg = "UmljY2FyZG8gbGlrZXMgcGluZWFwcGxlIHBpenphCg=="
 
 func TestWriteExtraOutput(t *testing.T) {
 	out, err := base64.StdEncoding.DecodeString(msg)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
 	err = ioutil.WriteFile("truth.txt", out, 0644)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
 }
