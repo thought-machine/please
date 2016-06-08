daab4cb0ba25ebb9a723818d820ef7e37f97d4e6
Peter Ebden <pebers@thoughtmachine.net>
2016-06-08 21:36:43 +0100

    Starting on type checks

---
+++ src/parse/type_checking.py
@@ -0,0 +1,58 @@
+"""Script invoked at build time to add type assertions to builtin rules.
+
+PEP3107 / PEP0484 style hints would be nicer, of course, but we need to
+retain python 2 compatibility for the foreseeable future.
+We could also write them manually, but I've chosen not to because
+of laziness.
+"""
+
+import ast
+import os
+import re
+import sys
+
+
+DOCSTRING_RE = re.compile(r' *([^ ]+) \(([^\)]+)\):')
+
+
+def read_functions(filename):
+    """Reads the given python file and yields the function arguments in it."""
+    with open(filename) as f:
+        tree = ast.parse(f.read(), f.name)
+        for i, node in enumerate(ast.iter_child_nodes(tree)):
+            if isinstance(node, ast.FunctionDef) and not node.name.startswith('_'):
+                yield node.body[0].value.lineno, list(arg_checks(node))
+
+
+def arg_checks(node):
+    """Yields a sequence of checks on the given ast function node."""
+    docs = {m.group(1): m.group(2) for m in DOCSTRING_RE.finditer(ast.get_docstring(node))}
+    min_default = len(node.args.args) - len(node.args.defaults)
+    for i, arg in enumerate(node.args.args):
+        doc = docs[arg.id]
+        if i > min_default:
+            if '|' in doc:
+                types = doc.split(' | ')
+                yield 'assert not %s or isinstance(%s, (%s)), "Argument %s to %s must be a %s"' % (
+                    arg.id, arg.id, ', '.join(types), arg.id, node.name, ' or '.join(types))
+            else:
+                yield 'assert not %s or isinstance(%s, %s), "Argument %s to %s must be a %s"' % (
+                    arg.id, arg.id, doc, arg.id, node.name, doc)
+        else:
+            yield'assert isinstance(%s, %s), "Argument %s to %s must be a %s"' % (
+                arg.id, doc, arg.id, node.name, doc)
+
+
+def process(filename):
+    checks = dict(read_functions(filename))
+    with open(filename) as f, open(os.path.basename(filename), 'w') as f2:
+        for i, line in enumerate(f):
+            if i in checks:
+                f2.write('\n'.join(indent * ' ' + check for check in checks[i]))
+            f2.write(line)
+            indent = len(line) - len(line.lstrip())
+
+
+if __name__ == '__main__':
+    for filename in sys.argv[1:]:
+        process(filename)
