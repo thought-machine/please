43ca08d55a1076fce560ae438f1069349273de93
Peter Ebden <peter.ebden@gmail.com>
2016-07-01 13:22:26 +0100

    Adding https & mutual auth support to RPC cache (#107)
    * Another C++ improvement; combine multiple .a files into one after each rule, which makes things a bit cleaner later on.
    
    * python 3 fix for type checking script
    
    * Starting on client auth code
    
    * Adding server code & CA cert & signed cert
    
    * Adding test
    
    * starting auth work in server
    
    * fixes
    
    * Adding tests
    
    * Revert "Another C++ improvement; combine multiple .a files into one after each rule, which makes things a bit cleaner later on."
    
    This reverts commit 4fea77338b6d4f02080f72121306a84caa509243.
    
    * Allow specifying that the cache should be secure for the case where there's no client auth and the CA is already implicitly trusted.
    
    * Add a little more timing information
    
    * Add auth on Delete rpc
    
    * Remove ssh key stuff, is likely impossible due to public key -> cert difference :(
    
    * Remove test files
    
    * remove very obsolete comment
    
    * make writable keys readable too

--- src/cache/server/rpc_server.go
+++ src/cache/server/rpc_server.go
@@ -1,26 +1,37 @@
-// Test only at this point to make sure we can build grpc correctly.
-// Later this will turn into a proper RPC cache server implementation.
 package server
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/pem"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
 	"path"
+	"path/filepath"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/peer"
 
 	pb "cache/proto/rpc_cache"
 )
 
 type RpcCacheServer struct {
-	cache *Cache
+	cache        *Cache
+	readonlyKeys map[string]*x509.Certificate
+	writableKeys map[string]*x509.Certificate
 }
 
 func (r *RpcCacheServer) Store(ctx context.Context, req *pb.StoreRequest) (*pb.StoreResponse, error) {
+	if err := r.authenticateClient(r.writableKeys, ctx); err != nil {
+		return nil, err
+	}
 	arch := req.Os + "_" + req.Arch
 	hash := base64.RawURLEncoding.EncodeToString(req.Hash)
 	for _, artifact := range req.Artifacts {
@@ -33,6 +44,9 @@ func (r *RpcCacheServer) Store(ctx context.Context, req *pb.StoreRequest) (*pb.S
 }
 
 func (r *RpcCacheServer) Retrieve(ctx context.Context, req *pb.RetrieveRequest) (*pb.RetrieveResponse, error) {
+	if err := r.authenticateClient(r.readonlyKeys, ctx); err != nil {
+		return nil, err
+	}
 	response := pb.RetrieveResponse{Success: true}
 	arch := req.Os + "_" + req.Arch
 	hash := base64.RawURLEncoding.EncodeToString(req.Hash)
@@ -57,6 +71,9 @@ func (r *RpcCacheServer) Retrieve(ctx context.Context, req *pb.RetrieveRequest) 
 }
 
 func (r *RpcCacheServer) Delete(ctx context.Context, req *pb.DeleteRequest) (*pb.DeleteResponse, error) {
+	if err := r.authenticateClient(r.writableKeys, ctx); err != nil {
+		return nil, err
+	}
 	if req.Everything {
 		return &pb.DeleteResponse{Success: r.cache.DeleteAllArtifacts() == nil}, nil
 	}
@@ -70,15 +87,78 @@ func (r *RpcCacheServer) Delete(ctx context.Context, req *pb.DeleteRequest) (*pb
 	return &pb.DeleteResponse{Success: success}, nil
 }
 
+func (r *RpcCacheServer) authenticateClient(certs map[string]*x509.Certificate, ctx context.Context) error {
+	if len(certs) == 0 {
+		return nil // Open to anyone.
+	}
+	p, ok := peer.FromContext(ctx)
+	if !ok {
+		return fmt.Errorf("Missing client certificate")
+	}
+	info, ok := p.AuthInfo.(credentials.TLSInfo)
+	if !ok {
+		return fmt.Errorf("Could not extract auth info")
+	}
+	if len(info.State.PeerCertificates) == 0 {
+		return fmt.Errorf("No peer certificate available")
+	}
+	cert := info.State.PeerCertificates[0]
+	if okCert := certs[string(cert.RawSubject)]; okCert != nil && okCert.Equal(cert) {
+		return nil
+	}
+	return fmt.Errorf("Invalid or unknown certificate")
+}
+
+func loadKeys(filename string) map[string]*x509.Certificate {
+	ret := map[string]*x509.Certificate{}
+	if err := filepath.Walk(filename, func(name string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		} else if !info.IsDir() {
+			data, err := ioutil.ReadFile(filename)
+			if err != nil {
+				log.Fatalf("Failed to read cert from %s: %s", filename, err)
+			}
+			p, _ := pem.Decode(data)
+			if p == nil {
+				log.Fatalf("Couldn't decode PEM data from %s: %s", filename, err)
+			}
+			cert, err := x509.ParseCertificate(p.Bytes)
+			if err != nil {
+				log.Fatalf("Couldn't parse certificate from %s: %s", filename, err)
+			}
+			ret[string(cert.RawSubject)] = cert
+		}
+		return nil
+	}); err != nil {
+		log.Fatalf("%s", err)
+	}
+	return ret
+}
+
 // BuildGrpcServer creates a new, unstarted grpc.Server and returns it.
 // It also returns a net.Listener to start it on.
-func BuildGrpcServer(port int, cache *Cache) (*grpc.Server, net.Listener) {
+func BuildGrpcServer(port int, cache *Cache, keyFile, certFile, caCertFile, readonlyKeys, writableKeys string) (*grpc.Server, net.Listener) {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("Failed to listen on port %d: %v", port, err)
 	}
-	s := grpc.NewServer()
+	s := serverWithAuth(keyFile, certFile, caCertFile)
 	r := &RpcCacheServer{cache: cache}
+	if writableKeys != "" {
+		r.writableKeys = loadKeys(writableKeys)
+	}
+	if readonlyKeys != "" {
+		r.readonlyKeys = loadKeys(readonlyKeys)
+		if len(r.readonlyKeys) > 0 {
+			// This saves duplication when checking later; writable keys are implicitly readable too.
+			for k, v := range r.writableKeys {
+				if _, present := r.readonlyKeys[k]; !present {
+					r.readonlyKeys[k] = v
+				}
+			}
+		}
+	}
 	pb.RegisterRpcCacheServer(s, r)
 	healthserver := health.NewHealthServer()
 	healthserver.SetServingStatus("plz-rpc-cache", healthpb.HealthCheckResponse_SERVING)
@@ -87,8 +167,35 @@ func BuildGrpcServer(port int, cache *Cache) (*grpc.Server, net.Listener) {
 }
 
 // ServeGrpcForever constructs a new server on the given port and serves until killed.
-func ServeGrpcForever(port int, cache *Cache) {
-	s, lis := BuildGrpcServer(port, cache)
+func ServeGrpcForever(port int, cache *Cache, keyFile, certFile, caCertFile, readonlyKeys, writableKeys string) {
+	s, lis := BuildGrpcServer(port, cache, keyFile, certFile, caCertFile, readonlyKeys, writableKeys)
 	log.Notice("Serving RPC cache on port %d", port)
 	s.Serve(lis)
+}
+
+// serverWithAuth builds a gRPC server, possibly with authentication if key / cert files are given.
+func serverWithAuth(keyFile, certFile, caCertFile string) *grpc.Server {
+	if keyFile == "" {
+		return grpc.NewServer() // No auth.
+	}
+	log.Debug("Loading x509 key pair from key: %s cert: %s", keyFile, certFile)
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		log.Fatalf("Failed to load x509 key pair: %s", err)
+	}
+	config := tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequestClientCert,
+	}
+	if caCertFile != "" {
+		cert, err := ioutil.ReadFile(caCertFile)
+		if err != nil {
+			log.Fatalf("Failed to read CA cert file: %s", err)
+		}
+		config.ClientCAs = x509.NewCertPool()
+		if !config.ClientCAs.AppendCertsFromPEM(cert) {
+			log.Fatalf("Failed to find any PEM certificates in CA cert")
+		}
+	}
+	return grpc.NewServer(grpc.Creds(credentials.NewTLS(&config)))
 }
