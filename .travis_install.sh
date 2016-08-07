5d48644ecd826bc752fe7c31653b9b9350c487ea
Peter Ebden <peter.ebden@gmail.com>
2016-08-07 10:14:52 +0100

    protoc fix

--- .travis_install.sh
+++ .travis_install.sh
@@ -8,8 +8,8 @@ if [ ! -f "$HOME/protoc" ]; then
     rm -rf "$HOME/protoc"
     echo 'Downloading protobuf...'
     curl -fsSLO https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
-    unzip protoc-3.0.0-linux-x86_64.zip protoc
-    mv protoc "$HOME/protoc"
+    unzip protoc-3.0.0-linux-x86_64.zip bin/protoc
+    mv bin/protoc "$HOME/protoc"
 else
     echo 'Using cached protobuf.';
 fi
