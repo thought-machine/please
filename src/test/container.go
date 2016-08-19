dcc2b60c6cc4a956ce457212fab1a60a0330d3f9
Peter Ebden <peter.ebden@gmail.com>
2016-08-19 13:52:31 +0100

    Remove support for PrintCommands, it's no longer useful and has been undocumented for some time

--- src/test/container.go
+++ src/test/container.go
@@ -43,11 +43,7 @@ func runContainerisedTest(state *core.BuildState, target *core.BuildTarget) ([]b
 	}
 	replacedCmd = "mkdir -p /tmp/test && cp -r /tmp/test_in/* /tmp/test && cd /tmp/test && " + replacedCmd
 	command = append(command, "-v", testDir+":/tmp/test_in", "-w", "/tmp/test_in", containerName, "bash", "-o", "pipefail", "-c", replacedCmd)
-	if state.PrintCommands {
-		log.Notice("Running containerised test %s: docker %s", target.Label, strings.Join(command, " "))
-	} else {
-		log.Debug("Running containerised test %s: docker %s", target.Label, strings.Join(command, " "))
-	}
+	log.Debug("Running containerised test %s: docker %s", target.Label, strings.Join(command, " "))
 	cmd := exec.Command("docker", command...)
 	cmd.Dir = target.TestDir()
 	out, err := core.ExecWithTimeout(cmd, target.TestTimeout, state.Config.Test.Timeout)
