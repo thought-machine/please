6a63c5da0867cc551d1454380320f031466a228b
Peter Ebden <peter.ebden@gmail.com>
2016-09-19 11:06:17 +0100

    Adding a post-build stress test to help check incrementality.

---
+++ src/misc/bootstrap_stress_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Used to stress test caching via bootstrap. Essentially we should bootstrap once to
+# build everything, then rerunning it a bunch of times in a loop should be very fast
+# and retrieve everything from the cache.
+# This script assumes the initial bootstrap has already run.
+
+set -eu
+
+N="${1:-10}"
+
+for i in `seq 1 $N`; do
+    echo "Round $i"
+    ./bootstrap.sh --plain_output
+    BUILDING="$(grep "Building target" plz-out/log/*.log)"
+    if [ -n "$BUILDING" ]; then
+        echo "Found building targets:"
+        echo "$BUILDING"
+        exit 1
+    fi
+    RUNNING="$(grep "Running test" plz-out/log/test_build.log)"
+    if [ -n "$RUNNING" ]; then
+        echo "Found running tests:"
+        echo "$RUNNING"
+        exit 1
+    fi
+done
