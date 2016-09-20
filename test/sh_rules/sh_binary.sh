1d1759b8d9a0a422fe886eaa5d393dcd21dde346
dimitar <dimitar@thoughtmachine.net>
2016-09-20 11:40:07 +0100

    add sh_rules tests

---
+++ test/sh_rules/sh_binary.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eu
+
+# test /bin/bash can be used
+[ ! -z "$BASH_VERSION" ] || (echo "Expected a bash shell" >&2 && exit 11)
+
+source test/sh_rules/sh_lib.sh
+[ "$TEST_DEPS_VAR" = "123" ] || (echo "Could not source variable from dependency" && exit 12)
