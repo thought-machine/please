286769a9146076d1778e6449aeaf9e3401ff75ac
Peter Ebden <peter.ebden@gmail.com>
2016-09-21 09:44:12 +0100

    Sort a copy

--- src/core/build_target.go
+++ src/core/build_target.go
@@ -298,8 +298,9 @@ func (target *BuildTarget) DependenciesFor(label BuildLabel) []*BuildTarget {
 // DeclaredOutputs returns the outputs from this target's original declaration.
 // Hence it's similar to Outputs() but without the resolving of other rule names.
 func (target *BuildTarget) DeclaredOutputs() []string {
-	sort.Strings(target.outputs)
-	return target.outputs
+	ret := target.outputs[:]
+	sort.Strings(ret)
+	return ret
 }
 
 // Outputs returns a slice of all the outputs of this rule.
