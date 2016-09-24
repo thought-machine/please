20fc0bed9f207a9a4492995520f381846ba9b8ed
Peter Ebden <peter.ebden@gmail.com>
2016-09-24 11:34:18 +0100

    Change install script to install into ~/.please

--- install.sh
+++ install.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 #
-# Installs Please and its relevant binaries into /opt/please.
+# Installs Please and its relevant binaries into ~/.please.
 # You must run ./bootstrap.sh before running this.
 
 set -eu
 
-[[ -w /opt && -w /usr/local ]] || sudo sh -c "mkdir -p /opt && chown -R $USER /opt /usr/local"
-
 if [ ! -f plz-out/bin/src/please ]; then
     echo "It looks like Please hasn't been built yet."
     echo "Try running ./bootstrap.sh first."
-else
-    if [ ! -d /opt/please ]; then
-        sudo mkdir -p /opt/please
-        sudo chown `whoami` /opt/please
-    fi
-    rm -f /opt/please/please /opt/please/please_pex /opt/please/junit_runner.jar /opt/please/jarcat /opt/please/please_maven /opt/please/cache_cleaner
-    cp plz-out/bin/src/please /opt/please/please
-    chmod 0775 /opt/please/please
-    cp plz-out/bin/src/libplease_parser_*.so /opt/please
-    chmod 0664 /opt/please/libplease_parser_*.so
-    cp plz-out/bin/src/build/python/please_pex /opt/please/please_pex
-    chmod 0775 /opt/please/please_pex
-    cp plz-out/bin/src/build/java/junit_runner.jar /opt/please/junit_runner.jar
-    chmod 0664 /opt/please/junit_runner.jar
-    cp plz-out/bin/src/build/java/jarcat /opt/please/jarcat
-    chmod 0775 /opt/please/jarcat
-    cp plz-out/bin/src/build/java/please_maven /opt/please/please_maven
-    chmod 0775 /opt/please/please_maven
-    cp plz-out/bin/src/cache/main/cache_cleaner /opt/please/cache_cleaner
-    chmod 0775 /opt/please/cache_cleaner
-    cp plz-out/bin/src/misc/please_diff_graphs /opt/please/please_diff_graphs
-    chmod 0775 /opt/please/please_diff_graphs
-    cp plz-out/bin/src/build/go/please_go_test /opt/please/please_go_test
-    chmod 0775 /opt/please/please_go_test
-    cp plz-out/bin/src/lint/please_build_linter /opt/please/please_build_linter
-    chmod 0775 /opt/please/please_build_linter
-    ln -sf /opt/please/please /usr/local/bin/plz
-    echo "Please installed"
+    exit 1
+fi
+
+DEST="${HOME}/.please"
+mkdir -p ${DEST}
+rm -f ${DEST}/please ${DEST}/please_pex ${DEST}/junit_runner.jar ${DEST}/jarcat ${DEST}/please_maven ${DEST}/cache_cleaner ${DEST}/*.so
+cp plz-out/bin/src/please ${DEST}/please
+chmod 0775 ${DEST}/please
+ln -sf ${DEST}/please ${DEST}/plz
+cp plz-out/bin/src/libplease_parser_*.so ${DEST}
+chmod 0664 ${DEST}/libplease_parser_*.so
+cp plz-out/bin/src/build/python/please_pex ${DEST}/please_pex
+chmod 0775 ${DEST}/please_pex
+cp plz-out/bin/src/build/java/junit_runner.jar ${DEST}/junit_runner.jar
+chmod 0664 ${DEST}/junit_runner.jar
+cp plz-out/bin/src/build/java/jarcat ${DEST}/jarcat
+chmod 0775 ${DEST}/jarcat
+cp plz-out/bin/src/build/java/please_maven ${DEST}/please_maven
+chmod 0775 ${DEST}/please_maven
+cp plz-out/bin/src/cache/main/cache_cleaner ${DEST}/cache_cleaner
+chmod 0775 ${DEST}/cache_cleaner
+cp plz-out/bin/src/misc/please_diff_graphs ${DEST}/please_diff_graphs
+chmod 0775 ${DEST}/please_diff_graphs
+cp plz-out/bin/src/build/go/please_go_test ${DEST}/please_go_test
+chmod 0775 ${DEST}/please_go_test
+cp plz-out/bin/src/lint/please_build_linter ${DEST}/please_build_linter
+chmod 0775 ${DEST}/please_build_linter
+echo "Please installed"
+
+if [ ! -f /usr/local/bin/plz ]; then
+    echo "You might want to run ln -s ~/.please/please /usr/local/bin/plz or add ~/.please to your PATH."
 fi
