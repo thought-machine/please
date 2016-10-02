a8ec58ed299ad2eab7a39f70e3700c44eecddd2e
Peter Ebden <peter.ebden@gmail.com>
2016-10-02 10:52:09 +0100

    add test

--- src/core/build_label_test.go
+++ src/core/build_label_test.go
@@ -25,3 +25,12 @@ func TestIncludesSubpackages(t *testing.T) {
 	label2 := BuildLabel{PackageName: "third_party/python3", Name: "six"}
 	assert.True(t, label1.includes(label2))
 }
+
+func TestParent(t *testing.T) {
+	label := BuildLabel{PackageName: "src/core", Name: "core"}
+	assert.Equal(t, label, label.Parent())
+	label2 := BuildLabel{PackageName: "src/core", Name: "_core#src"}
+	assert.Equal(t, label, label2.Parent())
+	label3 := BuildLabel{PackageName: "src/core", Name: "_core_something"}
+	assert.Equal(t, label3, label3.Parent())
+}
