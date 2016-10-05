beb13d340cf9467f494bd2a821dae3e061e5f4d0
Peter Ebden <peter.ebden@gmail.com>
2016-10-05 14:34:29 +0100

    Add a --unique flag on query deps to only output each target once (#148)

--- src/query/deps.go
+++ src/query/deps.go
@@ -4,17 +4,25 @@ import "core"
 import "fmt"
 
 // QueryDeps prints all transitive dependencies of a set of targets.
-func QueryDeps(state *core.BuildState, labels []core.BuildLabel) {
+func QueryDeps(state *core.BuildState, labels []core.BuildLabel, unique bool) {
+	targets := map[*core.BuildTarget]bool{}
 	for _, label := range labels {
-		printTarget(state, state.Graph.TargetOrDie(label), "")
+		printTarget(state, state.Graph.TargetOrDie(label), "", targets, unique)
 	}
 }
 
-func printTarget(state *core.BuildState, target *core.BuildTarget, indent string) {
+func printTarget(state *core.BuildState, target *core.BuildTarget, indent string, targets map[*core.BuildTarget]bool, unique bool) {
+	if unique && targets[target] {
+		return
+	}
+	targets[target] = true
 	if target.ShouldInclude(state.Include, state.Exclude) {
 		fmt.Printf("%s%s\n", indent, target.Label)
 	}
+	if !unique {
+		indent = indent + "  "
+	}
 	for _, dep := range target.Dependencies() {
-		printTarget(state, dep, indent+"  ")
+		printTarget(state, dep, indent, targets, unique)
 	}
 }
