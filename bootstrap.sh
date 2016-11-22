d6c072d137580f375d90b857b5a6706438da7139
Peter Ebden <peter.ebden@gmail.com>
2016-11-22 16:29:13 +0000

    More tweaks to bootstrap to give clearer output

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,23 +2,33 @@
 
 set -eu
 
+function notice {
+    >&2 echo -e "\033[32m$1\033[0m"
+}
+function warn {
+    >&2 echo -e "\033[33m$1\033[0m"
+}
+function error {
+    >&2 echo -e "\033[31m$1\033[0m"
+}
+
 function interpreter_target {
     if hash $1 2>/dev/null ; then
         $1 -c 'import cffi' 2> /dev/null
         if [ $? -eq 0 ]; then
             echo "//src:please_parser_$1"
         else
-            >&2 echo "$1 doesn't have cffi installed, won't build parser engine for it."
-            >&2 echo "You won't be able to build Please packages unless all parsers are present."
+            warn "$1 doesn't have cffi installed, won't build parser engine for it."
+            warn "You won't be able to build Please packages unless all parsers are present."
         fi
     else
-        >&2 echo "$1 not found; won't build parser engine for it."
-        >&2 echo "You won't be able to build Please packages unless all parsers are present."
+        warn "$1 not found; won't build parser engine for it."
+        warn "You won't be able to build Please packages unless all parsers are present."
     fi
 }
 
 # Fetch the Go dependencies manually
-echo "Installing Go dependencies..."
+notice "Installing Go dependencies..."
 export GOPATH="${PWD}"
 go get golang.org/x/crypto/ssh/terminal
 go get golang.org/x/tools/cover
@@ -39,7 +49,7 @@ PYTHON2="$(interpreter_target python2)"
 PYTHON3="$(interpreter_target python3)"
 INTERPRETERS="$PYPY $PYTHON2"
 if [ -z "${INTERPRETERS// }" ]; then
-    echo "No known Python interpreters found, can't build parser engine"
+    error "No known Python interpreters found, can't build parser engine"
     exit 1
 fi
 # Choose one to build with during bootstrap
@@ -58,14 +68,14 @@ bin/go-bindata -o src/parse/builtin_rules.go -pkg parse -prefix src/parse/rules/
 bin/go-bindata -o src/utils/wrapper_script.go -pkg utils -prefix src/misc src/misc
 
 # Now invoke Go to run Please to build itself.
-echo "Building Please..."
+notice "Building Please..."
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 ENGINE="${SCRIPT_DIR}/src/parse/cffi/libplease_parser_${INTERPRETER}.*"
 go run src/please.go --engine $ENGINE --plain_output build //src:please $INTERPRETERS --log_file plz-out/log/bootstrap_build.log --log_file_level 4
 # Use it to build the rest of the tools that come with it.
 # NB. We can't do the tarballs here because they depend on all the interpreters, which some
 #     users might not have installed.
-echo "Building the tools..."
+notice "Building the tools..."
 plz-out/bin/src/please --plain_output build //src:please //:all_tools --log_file plz-out/log/tools_build.log --log_file_level 4
 
 if [ $# -gt 0 ] && [ "$1" == "--skip_tests" ]; then
@@ -73,7 +83,7 @@ if [ $# -gt 0 ] && [ "$1" == "--skip_tests" ]; then
 fi
 
 # Run the tests to make sure they still work
-echo "Running tests..."
+notice "Running tests..."
 
 # Run the set of tests that will work on this machine.
 # We assume the user has Java and Python installed or the build will have already failed,
@@ -81,13 +91,13 @@ echo "Running tests..."
 EXCLUDES=""
 
 HAVE_UNITTEST=false
-for path in `echo | cpp -xc++ -Wp,-v 2>&1 | grep "^ "`; do
+for path in `echo -e | cpp -xc++ -Wp,-v 2>&1 | grep "^ "`; do
     if [ -f "${path}/unittest++/UnitTest++.h" ]; then
         HAVE_UNITTEST=true
     fi
 done
 if ! $HAVE_UNITTEST ; then
-    echo "UnitTest++.h not found, excluding C++ tests"
+    warn "UnitTest++.h not found, excluding C++ tests"
     EXCLUDES="${EXCLUDES} --exclude=cc"
 else
     if [ "`uname`" = "Darwin" ]; then
@@ -95,31 +105,31 @@ else
             # OSX comes with an ancient version of nasm that can't target
             # 64-bit Mach-O binaries (?!!). Ensure we've got the Brew one.
             if [ -n "`nasm -v | grep 'version 2'`" ]; then
-                echo "nasm 2.x not found, excluding C++ tests"
+                warn "nasm 2.x not found, excluding C++ tests"
                 EXCLUDES="${EXCLUDES} --exclude=cc"
             fi
         fi
     fi
 fi
 if ! hash docker 2>/dev/null ; then
-    echo "Docker not found, excluding containerised tests"
+    warn "Docker not found, excluding containerised tests"
     EXCLUDES="${EXCLUDES} --exclude=container"
 fi
 if ! hash python3 2>/dev/null ; then
-    echo "python3 not found, excluding python3 tests"
+    warn "python3 not found, excluding python3 tests"
     EXCLUDES="${EXCLUDES} --exclude=py3"
 fi
 if ! hash clang++ 2>/dev/null ; then
-    echo "Clang not found, excluding Clang tests"
+    warn "Clang not found, excluding Clang tests"
     EXCLUDES="${EXCLUDES} --exclude=clang"
 fi
 if ! hash gold 2>/dev/null ; then
-    echo "Gold not found, excluding Gold tests"
+    warn "Gold not found, excluding Gold tests"
     EXCLUDES="${EXCLUDES} --exclude=gold"
 fi
 # If the proto files are installed in a different location, their tests won't work.
 if [ ! -d "/usr/include/google/protobuf" ]; then
-    echo "google/protobuf not found, excluding relevant tests"
+    warn "google/protobuf not found, excluding relevant tests"
     EXCLUDES="${EXCLUDES} --exclude=proto"
 fi
 
