93390b978c929690a4cb0beeba34c6ae29ebb6e8
Peter Ebden <peter.ebden@gmail.com>
2016-12-10 16:23:02 +0000

    Limit the ldflag change to Linux / PyPy only.

--- src/parse/cffi/cffi_compiler.py
+++ src/parse/cffi/cffi_compiler.py
@@ -1,6 +1,7 @@
 """Compiler script to build the CFFI modules."""
 
 import os
+import platform
 import sys
 from cffi import FFI
 
@@ -8,8 +9,12 @@ def main(defs_file, parser_file, verbose):
     ffi = FFI()
     with open(defs_file) as f:
         ffi.embedding_api(f.read())
+    ldflags = None
+    # Support downloading portable PyPy on Linux.
+    if platform.python_implementation() == 'PyPy' and platform.system() == 'Linux':
+        ldflags = ['-Wl,-rpath=./plz-out/gen/_remote/_pypy/bin']
     ffi.set_source('parser_interface', '#include "%s"' % defs_file,
-                   extra_link_args=['-Wl,-rpath=./plz-out/gen/_remote/_pypy/bin'])
+                   extra_link_args=ldflags)
     with open(parser_file) as f:
         ffi.embedding_init_code(f.read())
     interpreter, _, _ = os.path.basename(sys.executable).partition('.')
