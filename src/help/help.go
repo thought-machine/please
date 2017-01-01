b931c56b890491a3cb7a17f8d7dd8e1e3ece9ba6
Peter Ebden <peter.ebden@gmail.com>
2017-01-01 14:34:55 +0000

    back to help

--- src/help/help.go
+++ src/help/help.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 
 	"gopkg.in/op/go-logging.v1"
+
+	"utils"
 )
 
 var log = logging.MustGetLogger("help")
@@ -18,6 +20,12 @@ const rulesPreamble = `
 
 `
 
+var allHelpFiles = []string{"rule_defs.json"}
+var allHelpPreambles = []string{rulesPreamble}
+
+// maxSuggestionDistance is the maximum Levenshtein edit distance we'll suggest help topics at.
+const maxSuggestionDistance = 5
+
 // Help prints help on a particular topic.
 // It returns true if the topic is known or false if it isn't.
 func Help(topic string) bool {
@@ -26,27 +34,50 @@ func Help(topic string) bool {
 		return true
 	}
 	fmt.Printf("Sorry OP, can't halp you with %s\n", topic)
-	// TODO(pebers): do some fuzzy matching here
-	fmt.Printf("\nMaybe have a look on the website: https://please.build\n")
+	if message := suggest(topic); message != "" {
+		fmt.Println(message)
+		fmt.Printf("Or have a look on the website: https://please.build\n")
+	} else {
+		fmt.Printf("\nMaybe have a look on the website? https://please.build\n")
+	}
 	return false
 }
 
 func help(topic string) string {
-	if message, found := halp(topic, "rule_defs.json", rulesPreamble); found {
-		return message
+	for i, filename := range allHelpFiles {
+		if message, found := halp(topic, filename, allHelpPreambles[i]); found {
+			return message
+		}
 	}
 	return ""
 }
 
 func halp(topic, filename, preamble string) (string, bool) {
-	data := MustAsset(filename)
-	m := map[string]string{}
-	if err := json.Unmarshal(data, &m); err != nil {
-		log.Fatalf("Failed to load help data: %s\n", err)
-	}
+	m := loadData(filename)
 	message, found := m[topic]
 	if !found {
 		return "", false
 	}
 	return fmt.Sprintf(preamble, topic) + message, true
+}
+
+func loadData(filename string) map[string]string {
+	log.Debug("Opening help file %s", filename)
+	data := MustAsset(filename)
+	m := map[string]string{}
+	if err := json.Unmarshal(data, &m); err != nil {
+		log.Fatalf("Failed to load help data: %s\n", err)
+	}
+	return m
+}
+
+// suggest looks through all known help topics and tries to make a suggestion about what the user might have meant.
+func suggest(topic string) string {
+	topics := []string{}
+	for _, filename := range allHelpFiles {
+		for t := range loadData(filename) {
+			topics = append(topics, t)
+		}
+	}
+	return utils.SuggestMessage(topic, topics, maxSuggestionDistance)
 }
--- src/help/help_test.go
+++ src/help/help_test.go
@@ -10,6 +10,7 @@ func TestPublicInterface(t *testing.T) {
 	// Quick test on the main Help function; it doesn't expose much information so other
 	// tests use non-exported functions to get a bit more detail.
 	assert.True(t, Help("go_binary"))
+	assert.False(t, Help("go_binaryz"))
 	assert.False(t, Help("wibble"))
 }
 
@@ -18,4 +19,9 @@ func TestHelpDescription(t *testing.T) {
 	assert.Contains(t, help("go_binary"), "built-in build rule")
 	// And what its name was
 	assert.Contains(t, help("go_binary"), "go_binary")
+}
+
+func TestSuggestion(t *testing.T) {
+	assert.Equal(t, "\nMaybe you meant cc_embed_binary ?", suggest("cc_emdbed_binary"))
+	assert.Equal(t, "", suggest("blahdiblahdiblah"))
 }
