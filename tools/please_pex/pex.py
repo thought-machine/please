fdd1b47e24be77afd684a5d712961d46e6a5741d
Peter Ebden <peter.ebden@gmail.com>
2017-01-23 18:06:33 +0000

    Rewriting please_pex to not use pkg_resources

--- tools/please_pex/pex.py
+++ tools/please_pex/pex.py
@@ -2,26 +2,21 @@
 """Pex building script for Please."""
 
 import argparse
-import contextlib
-import json
 import os
 import py_compile
 import shutil
 import sys
-import tempfile
 import zipfile
 
-# We need to load pkg_resources and pex; they must be shipped in .bootstrap
-# for pex to work, but that's already been scrubbed from the path so we must
-# re-add it here.
-# TODO(pebers): remove use of pkg_resources in this file and use zipfile
-#               for all operations.
+# We need to load pex; it must be shipped in .bootstrap so it works when it loads,
+# but that's already been scrubbed from the path so we must re-add it here.
 sys.path.insert(0, os.path.abspath(os.path.join(sys.argv[0], '.bootstrap')))
 
-import pkg_resources
-from _pex.compatibility import to_bytes
 from _pex.pex_builder import PEXBuilder
 from _pex.interpreter import PythonInterpreter
+
+# Global reference to our zipfile, which we extract various bits from.
+please_pex_zipfile = None
 
 
 def dereference_symlinks(src):
@@ -35,26 +30,12 @@ def dereference_symlinks(src):
     return src
 
 
-def write_file(f, contents):
-    """Writes contents to given file object.
-
-    TODO(pebers):  This is mostly an effort to get around various encoding issues. Would be
-                   nice to solve this in a less brutal way but not sure how best to resolve
-                   python2 / python3 / pkg_resources :(
-    """
-    try:
-        f.write(contents)
-    except (UnicodeDecodeError, UnicodeEncodeError, TypeError):
-        try:
-            f.write(contents.decode('utf-8'))
-        except (UnicodeDecodeError, UnicodeEncodeError):
-            f.write(contents.decode('latin_1'))  # This is desperate peasantry of course.
-
-
-def add_directory(root_path, path, pex_builder):
+def add_directory(root_path, path, pex_builder, exclude=None):
     """Recursively adds the contents of a directory to the pex."""
     base = os.path.join(root_path, path)
     for filename in os.listdir(base):
+        if filename == exclude:
+            continue
         src = dereference_symlinks(os.path.join(base, filename))
         dst = os.path.join(path, filename)
         if os.path.isdir(src):
@@ -67,63 +48,24 @@ def add_directory(root_path, path, pex_builder):
             pex_builder.add_resource(src, dst)
 
 
-def add_test_files(test_sources, out_dir):
-    """Add files required for running a test pex."""
-    contents = pkg_resources.resource_string('tools.please_pex', 'test_main.py').decode('utf-8')
-    remove_ext = lambda x: x[:-3] if x.endswith('.py') else x
-    test_modules = ','.join(remove_ext(src.replace('/', '.')) for src in test_sources.split(','))
-    contents = contents.replace('__TEST_NAMES__', test_modules)
-    with open(os.path.join(out_dir, 'test_main.py'), 'w') as f:
-        write_file(f, contents)
-    # Extract the xmlrunner and coverage files too
-    extract_directory('third_party.python', 'xmlrunner', out_dir, '.bootstrap/xmlrunner', True)
-    extract_file('third_party.python', os.path.join(out_dir, '.bootstrap'), 'six.py', True)
-    extract_directory('third_party.python', 'coverage', out_dir, '.bootstrap/coverage', True)
-
-
-def add_main(module_dir, entry_point, out_dir, zip_safe):
-    """Add pex_main.py as the entry point to a pex."""
-    contents = pkg_resources.resource_string('tools.please_pex', 'pex_main.py').decode('utf-8')
-    contents = contents.replace('__MODULE_DIR__', module_dir).replace('__ENTRY_POINT__', entry_point)
-    contents = contents.replace('__ZIP_SAFE__', str(zip_safe))
-    with open(os.path.join(out_dir, 'pex_main.py'), 'w') as f:
-        write_file(f, contents)
-    return 'pex_main'
-
-
-def extract_directory(in_pkg, in_req, out_dir, out_path, duplicates_allowed=False):
-    """Extracts a directory from the pex builder and adds it to the staging directory."""
-    target_dir = os.path.join(out_dir, out_path)
-    if os.path.exists(target_dir) and duplicates_allowed:
-        return
-    os.makedirs(target_dir)
-    full_dir = '%s.%s' % (in_pkg, in_req)
-    for filename in pkg_resources.resource_listdir(in_pkg, in_req):
-        if not pkg_resources.resource_isdir(full_dir, filename):
-            extract_file(full_dir, target_dir, filename)
-
-
 def extract_recursive_dir(dirname, out_dir):
     """Extracts an entire directory and copies it to the output dir."""
-    try:
-        zf = zipfile.ZipFile(sys.argv[0])
-        for info in zf.infolist():
-            if info.filename.startswith(dirname):
-                zf.extract(info, path=out_dir)
-    except zipfile.BadZipfile:
-        # This happens during bootstrapping when we're not actually in a zipfile.
-        # In that case we might not need to do anything because .bootstrap is already there.
-        if not os.path.isdir(dirname):
-            raise
+    for info in please_pex_zipfile.infolist():
+        if info.filename.startswith(dirname) and not info.filename.endswith('.pyc'):
+            please_pex_zipfile.extract(info, path=out_dir)
 
 
-def extract_file(in_pkg, out_dir, filename, duplicates_allowed=False):
-    """Extracts a single file from the pex builder and adds it to the staging directory."""
-    target = os.path.join(out_dir, filename)
-    if os.path.exists(target) and duplicates_allowed:
+def extract_file(filename, out_dir, out_file=None, replacements=None):
+    """Extracts a single file from the pex and writes it into the out dir."""
+    if not replacements:
+        please_pex_zipfile.extract(filename, path=out_dir)
         return
-    with open(target, 'w') as f:
-        write_file(f, pkg_resources.resource_string(in_pkg, filename))
+    # More elaborate version if we need to replace the contents of the file.
+    contents = please_pex_zipfile.read(filename)
+    for k, v in replacements.items():
+        contents = contents.replace(k.encode('utf-8'), v.encode('utf-8'))
+    with open(os.path.join(out_dir, out_file or filename), 'wb') as f:
+        f.write(contents)
 
 
 def compile_bytecode():
@@ -152,11 +94,20 @@ def compile_bytecode():
                     f.write(b'\\x80Cm8')
 
 
+def remove_ext(x):
+    """Strip a .py file extension if present."""
+    return x[:-3] if x.endswith('.py') else x
+
+
 def main(args):
     # If --compile is given, we just need to precompile bytecode.
     if args.compile:
         compile_bytecode()
         sys.exit(0)
+
+    if not args.bootstrap:
+        global please_pex_zipfile
+        please_pex_zipfile = zipfile.ZipFile(sys.argv[0])
 
     # Pex doesn't support relative interpreter paths.
     if not args.interpreter.startswith('/'):
@@ -167,67 +118,63 @@ def main(args):
             from distutils import spawn
             args.interpreter = spawn.find_executable(args.interpreter)
 
-    # Add pkg_resources and the bootstrapper
-    extract_recursive_dir('.bootstrap', args.src_dir)
+    if not args.bootstrap:
+        # Add pkg_resources, the bootstrapper and the required main files.
+        # If we're creating a test we can just extract the entire bootstrap directory.
+        if args.test_srcs:
+            extract_recursive_dir('.bootstrap', args.src_dir)
+            test_modules = ','.join(remove_ext(src.replace('/', '.'))
+                                    for src in args.test_srcs.split(','))
+            extract_file('tools/please_pex/test_main.py', args.src_dir, 'test_main.py', {
+                '__TEST_NAMES__': test_modules,
+            })
+        else:
+            extract_recursive_dir('.bootstrap/_pex', args.src_dir)
+            extract_recursive_dir('.bootstrap/pkg_resources', args.src_dir)
+            extract_file('.bootstrap/six.py', args.src_dir)
+        extract_file('tools/please_pex/pex_main.py', args.src_dir, 'pex_main.py', {
+            '__MODULE_DIR__': args.module_dir,
+            '__ENTRY_POINT__': remove_ext(args.entry_point.replace('/', '.')),
+            '__ZIP_SAFE__': str(args.zip_safe),
+        })
 
     # Setup a temp dir that the PEX builder will use as its scratch dir.
-    tmp_dir = tempfile.mkdtemp()
-    tmp_file = '_temp.pex'
-    try:
-        if os.path.islink(args.interpreter) and os.readlink(args.interpreter) == 'python-exec2c':
-            # Some distros have this intermediate binary; it messes things up for
-            # pex which derefences it to this binary which can't be invoked directly.
-            print('Can\'t determine Python interpreter; you should set the \n'
-                  'default_interpreter property in the [python] section of \n'
-                  'plzconfig.local to a specific version (e.g. /usr/bin/python3.4)')
-            sys.exit(1)
-        interpreter = PythonInterpreter.from_binary(args.interpreter)
-        pex_builder = PEXBuilder(path=tmp_dir, interpreter=interpreter)
-        pex_builder.info.zip_safe = args.zip_safe
+    tmp_dir = '_pex_tmp'
+    os.mkdir(tmp_dir)
+    if os.path.islink(args.interpreter) and os.readlink(args.interpreter) == 'python-exec2c':
+        # Some distros have this intermediate binary; it messes things up for
+        # pex which derefences it to this binary which can't be invoked directly.
+        print('Can\'t determine Python interpreter; you should set the \n'
+              'default_interpreter property in the [python] section of \n'
+              'plzconfig.local to a specific version (e.g. /usr/bin/python3.4)')
+        sys.exit(1)
+    interpreter = PythonInterpreter.from_binary(args.interpreter)
+    pex_builder = PEXBuilder(path=tmp_dir, interpreter=interpreter)
+    pex_builder.info.zip_safe = args.zip_safe
+    if args.shebang:
+        pex_builder.set_shebang(args.shebang)
+    # Override the entry point so the module import override works.
+    pex_builder.info.entry_point = 'test_main' if args.test_srcs else 'pex_main'
 
-        if args.shebang:
-            pex_builder.set_shebang(args.shebang)
+    if args.scan:
+        # Add everything under the input dir.
+        add_directory(args.src_dir, '.', pex_builder, exclude=tmp_dir)
+    else:
+        # Just add bootstrap dir and main.
+        add_directory(args.src_dir, '.bootstrap', pex_builder)
+        pex_builder.add_source('pex_main.py', 'pex_main.py')
 
-        # Set the entry point.
-        args.entry_point = args.entry_point.replace('/', '.')
-        if args.entry_point.endswith('.py'):
-            args.entry_point = args.entry_point[:-3]
-        pex_main = add_main(args.module_dir, args.entry_point, args.src_dir, args.zip_safe)
-        if args.test_package:
-            # Stick with the test main, it knows what to do.
-            pex_builder.info.entry_point = args.entry_point
-        else:
-            # Override the entry point so the module import override works.
-            pex_builder.info.entry_point = pex_main
+    # This function does some setuptools malarkey which is vexing me, so
+    # I'm just gonna cheekily disable it for now.
+    pex_builder._prepare_bootstrap = lambda: None
+    # Similarly I don't want it to write __init__.py files, we will take care
+    # of that later ourselves. This is important because we fail if we try to
+    # add different versions of the same file to the final pex, but because we
+    # only have a partial filetree here it may add some which don't appear until later.
+    pex_builder._prepare_inits = lambda: None
 
-        if args.test_package and args.test_srcs:
-            # Temp hack to make this work from an unexpected module name
-            sys.path.append(os.path.join(sys.argv[0], 'third_party/python'))
-            add_test_files(args.test_srcs, args.src_dir)
-
-        if args.scan:
-            # Add everything under the input dir.
-            add_directory(args.src_dir, '.', pex_builder)
-        else:
-            # Just add bootstrap dir and main.
-            add_directory(args.src_dir, '.bootstrap', pex_builder)
-            pex_builder.add_source(pex_main + '.py', pex_main + '.py')
-
-        # This function does some setuptools malarkey which is vexing me, so
-        # I'm just gonna cheekily disable it for now.
-        pex_builder._prepare_bootstrap = lambda: None
-        # Similarly I don't want it to write __init__.py files, we will take care
-        # of that later ourselves. This is important because we fail if we try to
-        # add different versions of the same file to the final pex, but because we
-        # only have a partial filetree here it may add some which don't appear until later.
-        pex_builder._prepare_inits = lambda: None
-
-        # Generate the PEX file.
-        pex_builder.build(args.out)
-
-    # Always try cleaning up the scratch dir, ignoring failures.
-    finally:
-        shutil.rmtree(tmp_dir, True)
+    # Generate the PEX file.
+    pex_builder.build(args.out)
 
 
 if __name__ == '__main__':
@@ -245,5 +192,6 @@ if __name__ == '__main__':
     parser.add_argument('--scan', dest='scan', action='store_true')
     parser.add_argument('--noscan', dest='scan', action='store_false')
     parser.add_argument('--compile', dest='compile', action='store_true')
-    parser.set_defaults(zip_safe=True, compile=False, scan=True)
+    parser.add_argument('--bootstrap', dest='bootstrap', action='store_true')
+    parser.set_defaults(zip_safe=True, compile=False, scan=True, bootstrap=False)
     main(parser.parse_args())
