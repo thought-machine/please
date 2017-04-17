1d7d427c170f356225775194954f1fa8da754fba
Peter Ebden <peter.ebden@gmail.com>
2017-04-17 16:48:03 +0100

    Breaking up Go system dep test; it seems a little unhappy about some changes in protobuf 3.2.0

--- test/system_deps/go/system_dep_test.go
+++ test/system_deps/go/system_dep_test.go
@@ -1,9 +1,13 @@
-package google_protobuf
+package system_deps_go
 
-import "testing"
+import (
+	"testing"
+
+	google_protobuf "test/system_deps/source_context"
+)
 
 func TestSourceContextProto(t *testing.T) {
-	sc := SourceContext{}
+	sc := google_protobuf.SourceContext{}
 	// These assertions are a bit pointless, essentially compiling this test is
 	// sufficient to ensure things are working OK. Just want to ensure that we
 	// actually use the type.
