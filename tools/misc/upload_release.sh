fb0c5b485770d46f7b0ff1bdcae1ee2f9d5df0cb
Peter Ebden <peter.ebden@gmail.com>
2017-04-26 09:47:28 +0100

    Change upload script to not use tput which fails in some headless scenarios ($TERM isn't set)

--- tools/misc/upload_release.sh
+++ tools/misc/upload_release.sh
@@ -4,20 +4,22 @@
 # Should be run from the root of the repo, and only by a CI system.
 
 set -euo pipefail
-RED="$(tput setaf 1)"
-GREEN="$(tput setaf 2)"
-YELLOW="$(tput setaf 3)"
-RESET="$(tput sgr0)"
+RED="\x1B[31m"
+GREEN="\x1B[32m"
+YELLOW="\x1B[33m"
+RESET="\x1B[0m"
+
 BUCKET="s3://get.please.build"
+PLZ="plz-out/bin/src/please"
 
 VERSION="$(cat VERSION)"
 eval $(go env)
-echo "${GREEN}Identifying outputs...${RESET}"
-FILES="$(plz query outputs -p //package:tarballs)"
+echo -e "${GREEN}Identifying outputs...${RESET}"
+FILES="$($PLZ query outputs -p //package:tarballs)"
 
 if [ "$GOOS" == "linux" ]; then
     # For Linux we upload debs as well.
-    DEBS="$(plz query alltargets -p //package:all --include deb | plz query outputs -p -)"
+    DEBS="$($PLZ query alltargets -p //package:all --include deb | $PLZ query outputs -p -)"
     FILES="$FILES $DEBS"
 fi
 
@@ -25,21 +27,21 @@ UPLOADED=''
 for FILE in $FILES; do
     BN="$(basename $FILE)"
     DEST="${BUCKET}/${GOOS}_${GOARCH}/${VERSION}/${BN}"
-    echo "${GREEN}Checking ${DEST}...${RESET}"
+    echo -e "${GREEN}Checking ${DEST}...${RESET}"
     if aws s3 ls $DEST > /dev/null ; then
-        echo "${YELLOW}${DEST} already exists, will not update${RESET}"
+        echo -e "${YELLOW}${DEST} already exists, will not update${RESET}"
     else
-        echo "${GREEN}Uploading ${FILE} to ${DEST}...${RESET}"
+        echo -e "${GREEN}Uploading ${FILE} to ${DEST}...${RESET}"
         aws s3 cp $FILE $DEST
         UPLOADED=true
     fi
 done
 
 if [[ "$VERSION" =~ .*(alpha|beta|pre|rc).* ]]; then
-    echo "${YELLOW}Pre-release version found, will not update latest_version.${RESET}"
+    echo -e "${YELLOW}Pre-release version found, will not update latest_version.${RESET}"
 elif [ "$UPLOADED" = true ]; then
-    echo "${GREEN}Uploaded at least one file, updating latest_version...${RESET}"
+    echo -e "${GREEN}Uploaded at least one file, updating latest_version...${RESET}"
     aws s3 cp "VERSION" "${BUCKET}/latest_version"
 else
-    echo "${YELLOW}Didn't upload anything, will not update latest_version${RESET}"
+    echo -e "${YELLOW}Didn't upload anything, will not update latest_version${RESET}"
 fi
