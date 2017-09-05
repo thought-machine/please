fc29d0bd6b1c3abde0f248168f6e5e6eb678e4c7
Peter Ebden <peter.ebden@gmail.com>
2017-09-05 18:10:56 +0100

    Adding script to update the website

---
+++ tools/misc/upload_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to automatically update the Please website.
+# Should be run from the root of the repo, and only by a CI system.
+
+set -eu
+
+BUCKET="s3://please.build"
+
+DIR="`mktemp -d`"
+plz-out/bin/src/please export outputs -o "$DIR" //docs
+aws s3 cp --recursive "$DIR" "$BUCKET"
+rm -rf "$DIR"
