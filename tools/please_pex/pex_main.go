e3182988855709106aa2512fb12d42ede02282ed
Peter Ebden <peter.ebden@gmail.com>
2017-09-13 17:33:34 +0100

    Improve incrementality for python_test / python_binary rules

--- tools/please_pex/pex_main.go
+++ tools/please_pex/pex_main.go
@@ -2,6 +2,9 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
+
 	"gopkg.in/op/go-logging.v1"
 
 	"cli"
@@ -22,7 +25,7 @@ var opts = struct {
 	Shebang     string   `short:"s" long:"shebang" description:"Explicitly set shebang to this"`
 	ZipSafe     bool     `long:"zip_safe" description:"Marks this pex as zip-safe"`
 	NoZipSafe   bool     `long:"nozip_safe" description:"Marks this pex as zip-unsafe"`
-	CodeHash    string   `long:"code_hash" env:"STAMP" description:"Hash to embed into the pex file"`
+	CodeHash    string   `long:"code_hash" description:"Hash to embed into the pex file"`
 }{
 	Usage: `
 please_pex is a tool to create .pex files for Python.
@@ -36,8 +39,16 @@ See https://github.com/pantsbuild/pex for more info about pex.
 }
 
 func main() {
-	cli.ParseFlagsOrDie("please_pex", "8.1.2", &opts)
+	cli.ParseFlagsOrDie("please_pex", "9.0.1", &opts)
 	cli.InitLogging(opts.Verbosity)
+	if opts.CodeHash == "" {
+		// It is difficult to know what the code hash should be. We want to have one to keep
+		// pex happy, but it is not "correct" since we don't have the entire pex to write at this
+		// point. It doesn't really matter in the sense that when run within plz pex will never
+		// reuse its code hashes anyway (because $HOME is overridden).
+		sum := sha1.Sum([]byte(opts.Out))
+		opts.CodeHash = base64.RawStdEncoding.EncodeToString(sum[:])
+	}
 	w := pex.NewPexWriter(opts.EntryPoint, opts.Interpreter, opts.CodeHash, !opts.NoZipSafe)
 	if opts.Shebang != "" {
 		w.SetShebang(opts.Shebang)
