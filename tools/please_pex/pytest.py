3f20dac3441abc514f8e955808fc23ff094d8baf
Peter Ebden <peter.ebden@gmail.com>
2017-12-03 13:21:48 +0000

    make it actually do something

--- tools/please_pex/pytest.py
+++ tools/please_pex/pytest.py
@@ -1,6 +1,8 @@
 def run_tests(test_names):
     """Runs tests using pytest, returns the number of failures."""
     # N.B. import must be deferred until we have set up import paths.
-    import pytest
-
-    return 1
+    from pytest import main
+    args = ['--junitxml', 'test.results'] + TEST_NAMES
+    if test_names:
+        args += ['-k', ' '.join(test_names)]
+    return main(args)
