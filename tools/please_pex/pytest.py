ce4ccd80f27c03e910892b01fad34f39f99cecb6
Peter Ebden <peter.ebden@gmail.com>
2017-12-03 13:59:38 +0000

    Revert "interesting rethink / hack: make test srcs data files for pytest so it can discover them"
    This reverts commit 1385fe5297ec26f8cb9f5fa5e2b762f46015aede.

--- tools/please_pex/pytest.py
+++ tools/please_pex/pytest.py
@@ -2,7 +2,7 @@ def run_tests(test_names):
     """Runs tests using pytest, returns the number of failures."""
     # N.B. import must be deferred until we have set up import paths.
     from pytest import main
-    args = ['--junitxml', 'test.results'] + TEST_NAMES
+    args = ['--junitxml', 'test.results', '--pyargs'] + TEST_NAMES
     if test_names:
         args += ['-k', ' '.join(test_names)]
     return main(args)
