a997600f4571363f69c4a92a687c4c27210b8480
Peter Ebden <peter.ebden@gmail.com>
2017-12-11 11:23:16 +0000

    attempt better OSX search
    weirdly --static doesn't seem to have the correct effect on pkg-config - but am guessing a bit with the machine I've got available

--- src/parse/cffi/find_python.sh
+++ src/parse/cffi/find_python.sh
@@ -2,20 +2,14 @@
 # Script to locate the static libpython.
 PKG_CONFIG_NAME="$1"
 LIB_FILENAME="$2"
-for i in `gcc -print-search-dirs | grep libraries: | cut -c 13- | tr ":" " "` `pkg-config --libs-only-L $PKG_CONFIG_NAME`; do
+SEARCH_DIRS="`gcc -print-search-dirs | grep libraries: | cut -c 13- | tr ':' ' '`"
+PC_DIR="`pkg-config --static --libs-only-L $PKG_CONFIG_NAME | sed -e 's|^-L||g'`"
+for i in $SEARCH_DIRS $PC_DIR "${PC_DIR}/${PKG_CONFIG_NAME/-/}/config"; do
     if [ -f "${i}/${LIB_FILENAME}" ]; then
         cp "${i}/${LIB_FILENAME}" $OUT
         exit 0
     fi
 done
-
-# Maybe we are on OSX. I am unsure how best to locate system frameworks so for now
-# just hardcoding this (which presumably will be there most of the time...)
-FRAMEWORK="/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config/${LIB_FILENAME}"
-if [ -f "$FRAMEWORK" ] ; then
-    cp "$FRAMEWORK" $OUT
-    exit 0
-fi
 
 echo "Couldn't find $LIB_FILENAME"
 exit 1
