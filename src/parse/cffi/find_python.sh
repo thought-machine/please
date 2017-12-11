e014cb796b26637d509a4082f036d072b7a0a873
Peter Ebden <peter.ebden@gmail.com>
2017-12-11 10:07:48 +0000

    Add some special casing for OSX which *might* help?

--- src/parse/cffi/find_python.sh
+++ src/parse/cffi/find_python.sh
@@ -4,10 +4,18 @@ PKG_CONFIG_NAME="$1"
 LIB_FILENAME="$2"
 for i in `gcc -print-search-dirs | grep libraries: | cut -c 13- | tr ":" " "` `pkg-config --libs-only-L $PKG_CONFIG_NAME`; do
     if [ -f "${i}/${LIB_FILENAME}" ]; then
-	cp "${i}/${LIB_FILENAME}" $OUT
-	exit 0
+        cp "${i}/${LIB_FILENAME}" $OUT
+        exit 0
     fi
 done
+
+# Maybe we are on OSX. I am unsure how best to locate system frameworks so for now
+# just hardcoding this (which presumably will be there most of the time...)
+FRAMEWORK="/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config/${LIB_FILENAME}"
+if [ -f "$FRAMEWORK" ] ; then
+    cp "$FRAMEWORK" $OUT
+    exit 0
+fi
 
 echo "Couldn't find $LIB_FILENAME"
 exit 1
