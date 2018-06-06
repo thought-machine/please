3c41265d25d2a9b466b05a1cf6faf9289c360d4b
Peter Ebden <peter.ebden@gmail.com>
2018-06-06 16:59:38 +0100

    Allow customising threadpool executor, but stick with fixed threadpool.

--- tools/javac_worker/src/build/please/compile/JavaCompiler.java
+++ tools/javac_worker/src/build/please/compile/JavaCompiler.java
@@ -26,7 +26,7 @@ public class JavaCompiler {
      * run reads requests from stdin and sends them to stdout until they are closed.
      */
     public void run() {
-        ExecutorService executor = Executors.newFixedThreadPool(8);
+        ExecutorService executor = createExecutor();
         final Gson gson = new GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
             .create();
@@ -47,6 +47,13 @@ public class JavaCompiler {
                     }
                 });
         }
+    }
+
+    /**
+     * Overriding this allows customising the threadpool that is used.
+     */
+    protected ExecutorService createExecutor() {
+        return Executors.newFixedThreadPool(8);
     }
 
     /**
