238acdca3c11bd6482d7466eedc2d20f7d8cd29e
Luna Chen <luna@bnmetrics.com>
2018-06-15 21:31:30 +0100

    experiment: pytest filters

--- tools/please_pex/pytest.py
+++ tools/please_pex/pytest.py
@@ -6,6 +6,11 @@ def run_tests(args):
     # N.B. import must be deferred until we have set up import paths.
     from pytest import main
     args = args or []
+
+    # Added this so the original filter functionality by name does not change
+    if args and not args[0].startswith('-'):
+        args += ['-k', ' '.join(args)]
+
     args += ['--junitxml', 'test.results'] + TEST_NAMES
 
     if os.environ.get('DEBUG'):
