6fec2ef0132d268c2f45001c43320de2321240b6
luna <luna@thoughtmachine.com>
2018-06-18 17:39:05 +0100

    changed behave.py to find features dir

--- tools/please_pex/behave.py
+++ tools/please_pex/behave.py
@@ -1,10 +1,18 @@
 import os
 
 
+def get_features_dir():
+    for i in TEST_NAMES:
+        file_name = i.split('/')[-1]
+        if '.feature' in file_name:
+            return os.path.dirname(i)
+
+
 def run_tests(args=None):
     from behave.__main__ import main
-    args = args or []
 
-    args += [os.environ.get('PKG_DIR') + '/features', '--junit',
+    args = args or []
+    features_dir = get_features_dir()
+    args += [features_dir, '--junit',
              '--junit-directory', 'test.results']
     main(args)
