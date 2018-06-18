54a34f9ffb58dff74281285618d437879656aa05
luna <luna@thoughtmachine.com>
2018-06-18 15:57:04 +0100

    Made changes to behave test, and behave dir

--- tools/please_pex/behave.py
+++ tools/please_pex/behave.py
@@ -5,6 +5,6 @@ def run_tests(args=None):
     from behave.__main__ import main
     args = args or []
 
-    args += [os.environ.get('PKG_DIR'), '--junit',
+    args += [os.environ.get('PKG_DIR') + '/features', '--junit',
              '--junit-directory', 'test.results']
     main(args)
