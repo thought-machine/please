551ea38ff8e73594efa3c9c6c5ebd070c900b1d5
BNMetrics <luna@bnmetrics.com>
2018-06-19 14:27:15 +0100

    Small changes to behave test directory (#336)
    * Made changes to behave test, and behave dir
    
    * changed behave.py to find features dir
    
    * fixed the bug in pytest.py, as currently it won't work with filters such as 'not test_foo'
    
    * removed feature_dir var

--- tools/please_pex/behave.py
+++ tools/please_pex/behave.py
@@ -1,10 +1,17 @@
 import os
 
 
+def get_features_dir():
+    for i in TEST_NAMES:
+        file_name = i.split('/')[-1]
+        if '.feature' in file_name:
+            return os.path.dirname(i)
+
+
 def run_tests(args=None):
     from behave.__main__ import main
-    args = args or []
 
-    args += [os.environ.get('PKG_DIR'), '--junit',
+    args = args or []
+    args += [get_features_dir(), '--junit',
              '--junit-directory', 'test.results']
     main(args)
