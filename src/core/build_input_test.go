7db0b0811a1723a72060b0b009d0d1f70fb97e5d
Peter Ebden <peter.ebden@gmail.com>
2018-06-25 15:46:09 +0100

    fixes

--- src/core/build_input_test.go
+++ src/core/build_input_test.go
@@ -43,3 +43,27 @@ func TestParseNamedOutputLabelEmptyOutput(t *testing.T) {
 		MustParseNamedOutputLabel("//src/core:target1|", pkg)
 	})
 }
+
+func TestParseNamedOutputLabelSubrepo(t *testing.T) {
+	pkg := NewPackage("")
+	input := MustParseNamedOutputLabel("@test_x86//src/core:target1", pkg)
+	_, ok := input.(NamedOutputLabel)
+	assert.False(t, ok)
+	label, ok := input.(BuildLabel)
+	assert.True(t, ok)
+	assert.Equal(t, "src/core", label.PackageName)
+	assert.Equal(t, "target1", label.Name)
+	assert.Equal(t, "test_x86", label.Subrepo)
+}
+
+func TestParseNamedOutputLabelRelativeSubrepo(t *testing.T) {
+	pkg := NewPackage("src/core")
+	input := MustParseNamedOutputLabel("@test_x86:target1", pkg)
+	_, ok := input.(NamedOutputLabel)
+	assert.False(t, ok)
+	label, ok := input.(BuildLabel)
+	assert.True(t, ok)
+	assert.Equal(t, "src/core", label.PackageName)
+	assert.Equal(t, "target1", label.Name)
+	assert.Equal(t, "test_x86", label.Subrepo)
+}
