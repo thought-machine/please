88df1dcbc55460274ce517aeddc745c2e47b9c5a
Peter Ebden <peter.ebden@gmail.com>
2018-08-02 12:39:09 +0100

    Better completion for multi-level subcommands

--- src/core/config_flags.go
+++ src/core/config_flags.go
@@ -12,27 +12,36 @@ import (
 // It returns true if any modifications were made.
 func (config *Configuration) AttachAliasFlags(parser *flags.Parser) bool {
 	for name, alias := range config.AllAliases() {
-		cmd, _ := parser.AddCommand(name, alias.Desc, alias.Desc, &struct{}{})
-		for _, subcommand := range alias.Subcommand {
-			addSubcommands(cmd, strings.Fields(subcommand))
-		}
-		for _, flag := range alias.Flag {
-			// This is unavailable during bootstrap due to being a local modification.
-			cmd.AddOption(getOption(flag))
+		cmd := parser.Command
+		for _, namePart := range strings.Fields(name) {
+			cmd = addSubcommand(cmd, namePart, alias.Desc)
+			for _, subcommand := range alias.Subcommand {
+				addSubcommands(cmd, strings.Fields(subcommand))
+			}
+			for _, flag := range alias.Flag {
+				// This is unavailable during bootstrap due to being a local modification.
+				cmd.AddOption(getOption(flag))
+			}
 		}
 	}
 	return len(config.Aliases) > 0 || len(config.Alias) > 0
 }
 
 // addSubcommands attaches a series of subcommands to the given command.
-func addSubcommands(cmd *flags.Command, subcommand []string) {
-	if len(subcommand) > 0 && cmd != nil {
-		cmd2 := cmd.Find(subcommand[0])
-		if cmd2 == nil {
-			cmd2, _ = cmd.AddCommand(subcommand[0], "", "", &struct{}{})
-		}
-		addSubcommands(cmd2, subcommand[1:])
+func addSubcommands(cmd *flags.Command, subcommands []string) {
+	if len(subcommands) > 0 && cmd != nil {
+		addSubcommands(addSubcommand(cmd, subcommands[0], ""), subcommands[1:])
 	}
+}
+
+// addSubcommand adds a single subcommand to the given command.
+// If one by that name already exists, it is returned.
+func addSubcommand(cmd *flags.Command, subcommand, desc string) *flags.Command {
+	if existing := cmd.Find(subcommand); existing != nil {
+		return existing
+	}
+	newCmd, _ := cmd.AddCommand(subcommand, desc, desc, &struct{}{})
+	return newCmd
 }
 
 // getOption creates a new flags.Option.
