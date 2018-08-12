e5cdfe8b51620a07d72f072922e3a8c0b06bbeaf
Peter Ebden <peter.ebden@gmail.com>
2018-08-12 14:43:51 +0100

    Adding a gtest example

---
+++ test/cc_rules/gtest/gtest_test.cc
@@ -0,0 +1,18 @@
+// Trivial test to illustrate gtest working.
+
+#include "gtest/gtest.h"
+
+#include "test/cc_rules/lib1.h"
+#include "test/cc_rules/lib2.h"
+
+namespace thought_machine {
+
+TEST(GTest, Number1) {
+    EXPECT_EQ(107, get_number_1());
+}
+
+TEST(GTest, Number2) {
+    EXPECT_EQ(215, get_number_2());
+}
+
+}
