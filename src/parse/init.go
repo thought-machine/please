3889822a8a5ac955fd39b90b8151e5d207afbe0f
Peter Ebden <peter.ebden@gmail.com>
2018-08-14 09:02:54 +0100

    working on this test_x86 issue

--- src/parse/init.go
+++ src/parse/init.go
@@ -16,9 +16,8 @@ import (
 const pleasings = `
 http_archive(
     name = "pleasings",
-    hashes = ["388baebf9381c619f13507915f16d0165a5dc13e"],
-    strip_prefix = "pleasings-f0c549b375067802400699247106e4907de917c2",
-    urls = ["https://github.com/thought-machine/pleasings/archive/f0c549b375067802400699247106e4907de917c2.zip"],
+    strip_prefix = "pleasings-master",
+    urls = ["https://github.com/thought-machine/pleasings/archive/master.zip"],
 )
 `
 
