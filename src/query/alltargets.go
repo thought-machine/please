617d9fc170d174b2348fee7cedd0abf78375b79f
luna <luna@thoughtmachine.net>
2018-08-14 15:06:18 +0100

    reverted the change

--- src/query/alltargets.go
+++ src/query/alltargets.go
@@ -1,20 +1,17 @@
 package query
 
 import (
-		"strings"
+	"fmt"
+	"strings"
 
 	"core"
-	"fmt"
 )
 
 // AllTargets simply prints all the targets according to some expression.
-func AllTargets(state *core.BuildState, labels core.BuildLabels, showHidden bool) {
+func AllTargets(graph *core.BuildGraph, labels core.BuildLabels, showHidden bool) {
 	for _, label := range labels {
 		if showHidden || !strings.HasPrefix(label.Name, "_") {
-			target := state.Graph.TargetOrDie(label)
-			if state.ShouldInclude(target) {
-				fmt.Printf("%s\n", target.Label)
-			}
+			fmt.Printf("%s\n", label)
 		}
 	}
 }
