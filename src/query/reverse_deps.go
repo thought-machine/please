1f2c914512e658e64af59452042bdc699160eb17
luna <luna@thoughtmachine.net>
2018-08-14 16:45:09 +0100

    commit

--- src/query/reverse_deps.go
+++ src/query/reverse_deps.go
@@ -8,16 +8,18 @@ import (
 
 // ReverseDeps For each input label, finds all targets which depend upon it.
 func ReverseDeps(state *core.BuildState, labels []core.BuildLabel) {
-	uniqueTargets := make(map[core.BuildLabel]*core.BuildTarget)
+
+	uniqueTargets := make(map[*core.BuildTarget]struct{})
 
 	graph := state.Graph
 	for _, label := range labels {
+		//labelTarget := graph.TargetOrDie(label)
 		for _, child := range graph.PackageOrDie(label).AllChildren(graph.TargetOrDie(label)) {
 			for _, target := range graph.ReverseDependencies(child) {
 				if parent := target.Parent(graph); parent != nil {
-					uniqueTargets[parent.Label] = parent
+					uniqueTargets[parent] = struct{}{}
 				} else {
-					uniqueTargets[target.Label] = target
+					uniqueTargets[target] = struct{}{}
 				}
 			}
 		}
@@ -27,7 +29,7 @@ func ReverseDeps(state *core.BuildState, labels []core.BuildLabel) {
 		for _, label := range labels {
 			if pkg.HasSubinclude(label) {
 				for _, target := range pkg.AllTargets() {
-					uniqueTargets[target.Label] = target
+					uniqueTargets[target] = struct{}{}
 				}
 			}
 		}
@@ -35,9 +37,9 @@ func ReverseDeps(state *core.BuildState, labels []core.BuildLabel) {
 
 	targets := make(core.BuildLabels, 0, len(uniqueTargets))
 	for _, label := range labels {
-		delete(uniqueTargets, label)
+		delete(uniqueTargets, graph.TargetOrDie(label))
 	}
-	for _, target := range uniqueTargets {
+	for target := range uniqueTargets {
 		if state.ShouldInclude(target) {
 			targets = append(targets, target.Label)
 		}
