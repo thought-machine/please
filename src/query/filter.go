8e66c306d590c904e271331ca187f6001a7431aa
mike <mike@thoughtmachine.net>
2018-08-16 17:26:38 +0100

    Add comment

--- src/query/filter.go
+++ src/query/filter.go
@@ -6,6 +6,8 @@ import (
 	"cli"
 )
 
+// Filter takes the list of BuildLabels and checks which ones match the label selectors passed in.
+// If no includeLabels are provided, the default behaviour is to include all labels.
 func Filter(graph *core.BuildGraph, labels core.BuildLabels, includeLabels []string, excludeLabels []string) {
 	matcher := func(target *core.BuildTarget) bool {
 		include := len(includeLabels) == 0
