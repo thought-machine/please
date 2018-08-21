23e0667daf9fcdd89b45f19e7e8c1d34c89497e8
luna <luna@thoughtmachine.net>
2018-08-21 15:05:40 +0100

    clean up

--- src/watch/stub.go
+++ src/watch/stub.go
@@ -7,4 +7,4 @@ package watch
 import "core"
 
 // Watch is a stub implementation of the real function in watch.go, this one does nothing.
-func Watch(state *core.BuildState, labels core.BuildLabels, runWatchedBuild func(state *core.BuildState, labels []core.BuildLabel)) {}
+func Watch(state *core.BuildState, labels core.BuildLabels, runWatchedBuild func(watchedProcessName string)) {}
