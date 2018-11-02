ff6ef4bd3414da5421425b2d53bd7970ddc0c408
Peter Ebden <peter.ebden@gmail.com>
2018-11-02 15:10:11 +0000

    Support for running Go example tests.

---
+++ test/go_rules/example_test.go
@@ -0,0 +1,18 @@
+package whatever
+
+import "fmt"
+
+// These are straight from the docs.
+
+func ExampleHello() {
+	fmt.Println("hello")
+	// Output: hello
+}
+
+func ExampleSalutations() {
+	fmt.Println("hello, and")
+	fmt.Println("goodbye")
+	// Output:
+	// hello, and
+	// goodbye
+}
