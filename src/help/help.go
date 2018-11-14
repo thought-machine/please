214da60f9f382b81a5f4606625ee8775e9b6b8f5
Sean Chittenden <sean@chittenden.org>
2018-11-14 18:26:53 +0900

    Ensure all help topics end with a newline. (#459)
    Previously it was required to encode a `\n` at the end of every misc
    help topic.  With this change all help topics will end with a newline.

--- src/help/help.go
+++ src/help/help.go
@@ -121,5 +121,5 @@ func printMessage(msg string) {
 		})
 	}
 	// Replace % to %% when not followed by anything so it doesn't become a replacement.
-	cli.Fprintf(os.Stdout, strings.Replace(msg, "% ", "%% ", -1))
+	cli.Fprintf(os.Stdout, strings.Replace(msg, "% ", "%% ", -1)+"\n")
 }
