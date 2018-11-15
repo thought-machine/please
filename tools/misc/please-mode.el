729a6ae6a38ec09f1b7e7a24f3f34a8d29e05d6c
Peter Ebden <peter.ebden@gmail.com>
2018-11-15 15:25:14 +0000

    update please-mode to use lsp-mode

--- tools/misc/please-mode.el
+++ tools/misc/please-mode.el
@@ -1,11 +1,16 @@
 ; Definition of Please mode for Emacs.
-; Mostly inherits from python-mode and has that do all the hard work, the
-; main advantage is automatic invocation of buildifier.
+; Inherits from python-mode for formatting but uses lsp-mode for
+; autocompletion etc.
 ;
 ; Add this to .emacs to make this load automatically.
-;(add-to-list 'auto-mode-alist '("BUILD" . please-mode))
+; (add-to-list 'auto-mode-alist '("/BUILD\\'" . please-mode))
+; (add-to-list 'auto-mode-alist '("/BUILD.plz\\'" . please-mode))
 ; And add this to run buildifier on the current buffer when saving:
 ; (add-hook 'after-save-hook 'please-buildify-on-save).
+; TODO(peterebden): get rid of please-buildify-on-save once plz tool langserver
+;                   supports the textDocument/formatting method
+
+(require 'lsp-mode)
 
 (defun please-buildify ()
   "Format the current buffer according to the buildifier tool,
@@ -19,6 +24,24 @@
   (interactive)
   (when (eq major-mode 'please-mode) (please-buildify)))
 
+(require 'lsp-imenu)
+(add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
+(use-package lsp-ui
+  :ensure t
+  :config
+  (setq lsp-ui-sideline-ignore-duplicate t)
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(lsp-define-stdio-client
+ please-mode
+ "Please"
+ (lambda () default-directory) ; TODO(peterebden): what should we do here?
+ '("plz" "tool" "langserver"))
+
 (define-derived-mode please-mode python-mode "Please mode")
+
+(add-hook 'please-mode-hook
+          (lambda ()
+           (please-mode-enable)))
 
 (provide 'please-mode)
