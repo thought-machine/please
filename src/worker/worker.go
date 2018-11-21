4c3cafeba08570d45e2738dbb67d4eafb729aa2c
Peter Ebden <peter.ebden@gmail.com>
2018-11-21 15:13:45 +0000

    correct message

--- src/worker/worker.go
+++ src/worker/worker.go
@@ -217,7 +217,7 @@ func (l *stderrLogger) Write(msg []byte) (int, error) {
 			if msg := strings.TrimSpace(string(l.buffer)); strings.HasPrefix(msg, "WARNING") {
 				log.Warning("Warning from remote worker: %s", msg)
 			} else {
-				log.Error("Error from remote worker: %s")
+				log.Error("Error from remote worker: %s", msg)
 			}
 		}
 		l.History = append(l.History, l.buffer...)
