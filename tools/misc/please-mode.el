13780e8f3e6bba5158f5b5e50b1639c97814bcf7
Peter Ebden <peter.ebden@gmail.com>
2018-11-24 11:20:43 +0000

    Add autoformatting to please-mode now LPS supports it

--- tools/misc/please-mode.el
+++ tools/misc/please-mode.el
@@ -5,25 +5,8 @@
 ; Add this to .emacs to make this load automatically.
 ; (add-to-list 'auto-mode-alist '("/BUILD\\'" . please-mode))
 ; (add-to-list 'auto-mode-alist '("/BUILD.plz\\'" . please-mode))
-; And add this to run buildifier on the current buffer when saving:
-; (add-hook 'after-save-hook 'please-buildify-on-save).
-; TODO(peterebden): get rid of please-buildify-on-save once plz tool langserver
-;                   supports the textDocument/formatting method
 
 (require 'lsp-mode)
-
-(defun please-buildify ()
-  "Format the current buffer according to the buildifier tool,
-  in a pretty quick and dirty way."
-  (interactive)
-  (call-process "buildifier" nil nil nil "-mode=fix" (buffer-file-name))
-  (revert-buffer t t))
-
-;;;###autoload
-(defun please-buildify-on-save ()
-  (interactive)
-  (when (eq major-mode 'please-mode) (please-buildify)))
-
 (require 'lsp-imenu)
 (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
 (use-package lsp-ui
@@ -43,5 +26,11 @@
 (add-hook 'please-mode-hook
           (lambda ()
            (please-mode-enable)))
+
+(defun please-mode-fmt-on-save ()
+  (interactive)
+  (when (eq major-mode 'please-mode) (lsp-format-buffer)))
+
+(add-hook 'before-save-hook 'please-mode-fmt-on-save)
 
 (provide 'please-mode)
