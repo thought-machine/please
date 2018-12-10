7caa3bf3f1c51f381fa159f30a8b732d0c6c5151
Sean Chittenden <sean@chittenden.org>
2018-12-10 18:34:53 +0900

    src/parse/asp: backout publicly usable `exec()` (#502)
    Fix part 1 of #501

--- test/go_rules/test/test.go
+++ test/go_rules/test/test.go
@@ -3,11 +3,6 @@ package test
 var (
 	Var  string = "missing var"
 	Var2 string = "missing var2"
-
-	ExecList        string = "missing exec list"
-	ExecStr         string = "missing exec str"
-	ExecStdErr      string = "missing exec stderr"
-	ExecCombinedOut string = "missing exec combined out"
 )
 
 func GetAnswer() int {
@@ -20,20 +15,4 @@ func GetVar() string {
 
 func GetVar2() string {
 	return Var2
-}
-
-func GetExecList() string {
-	return ExecList
-}
-
-func GetExecStr() string {
-	return ExecStr
-}
-
-func GetExecStdErr() string {
-	return ExecStdErr
-}
-
-func GetExecCombinedOut() string {
-	return ExecCombinedOut
 }
