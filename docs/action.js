93ccaafffcd8176f686ba6bba565daa6bd8c6994
Peter Ebden <peter.ebden@gmail.com>
2018-12-13 14:17:37 +0000

    Adding some breakpoints

--- docs/action.js
+++ docs/action.js
@@ -17,8 +17,30 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     }
-    if (found) {
-        window.addEventListener('scroll', function(e) {
+
+    var angle = function(el) {
+        var tr = window.getComputedStyle(el, null).getPropertyValue("transform");
+        if (tr === "none") {
+            return 0;
+        }
+        var values = tr.split('(')[1];
+        values = values.split(')')[0];
+        values = values.split(',');
+        var a = values[0];
+        var b = values[1];
+        var c = values[2];
+        var d = values[3];
+        return Math.round(Math.atan2(b, a) * (180/Math.PI));
+    }
+
+    var pics = document.querySelectorAll('.menu-graphic img, .sideimg img');
+    var rots = new Array(pics.length);
+    for (var i = 0; i < pics.length; ++i) {
+        rots[i] = angle(pics[i]);
+    }
+
+    window.addEventListener('scroll', function(e) {
+        if (found) {
             var maxY = 0;
             var winner = "";
             for (var key in menuItems) {
@@ -32,6 +54,29 @@ document.addEventListener('DOMContentLoaded', function() {
             if (winner) {
                 menuItems[winner].className = "selected";
             }
-        });
+        }
+        var rot = window.scrollY / 100;
+        for (var i = 0; i < pics.length; ++i) {
+            pics[i].style.transform = 'rotate(' + (rot + rots[i]) + 'deg)';
+        }
+    });
+
+    var toggleMenu = function() {
+        var elem = document.querySelector('nav > ul');
+        if (elem.classList.contains('hide')) {
+            elem.classList.remove('hide');
+        } else {
+            elem.classList.add('hide');
+        }
+    };
+
+    document.querySelector('nav > p a').addEventListener('click', function(e) {
+        toggleMenu();
+        e.preventDefault();
+        return false;
+    });
+
+    if (window.innerWidth < 800) {
+        toggleMenu();
     }
 }, false);
