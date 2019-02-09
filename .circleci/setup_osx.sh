1ca8b50457c1b5a56975996b0a1c89dbbf3301c7
Peter Ebden <peter.ebden@gmail.com>
2019-02-09 14:30:47 +0000

    Build a version of xz.
    Not super happy about this; might try to upload it somewhere when I can.

--- .circleci/setup_osx.sh
+++ .circleci/setup_osx.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+set -eu
+
 # /usr/local/go might get cached.
 if [ ! -d "/usr/local/go" ]; then
     curl -fsSL https://dl.google.com/go/go1.11.5.darwin-amd64.tar.gz | sudo tar -xz -C /usr/local
 fi
 sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
+
+# xz might also.
+if [ ! -f "/usr/local/bin/xz" ]; then
+    curl -fsSL https://tukaani.org/xz/xz-5.2.4.tar.gz | tar -xz
+    cd xz-5.2.4
+    ./configure --disable-shared
+    make
+    make install
+fi
