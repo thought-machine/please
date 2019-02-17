78c64babdeff0a94850b75fdffb28aea3c764ec4
Peter Ebden <peter.ebden@gmail.com>
2019-02-17 10:58:32 +0000

    buildify

--- tools/misc/buildify.sh
+++ tools/misc/buildify.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec plz-out/bin/src/please run -p -- //third_party/go:buildifier $@ `git ls-files | grep BUILD`
+exec plz-out/bin/src/please run -p -- //third_party/go:buildifier $@ `git ls-files | grep BUILD$`
