0637eeab1f2f0a94e4742d8c32fd0a04dee6306d
Peter Ebden <peter.ebden@gmail.com>
2019-02-24 13:40:24 +0000

    Fix final linter issues

--- tools/misc/lint.sh
+++ tools/misc/lint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 alias plz="plz-out/bin/src/please"
-BLACKLIST="src/parse/asp/main|tools/cache|//test"
+BLACKLIST="src/parse/asp/main|tools/cache|//test|tools/please_pex"
 
 for TARGET in `plz query alltargets --include go_src --hidden | grep -v "_test#lib" | grep -v proto | grep -Ev $BLACKLIST`; do
     DIR=`echo $TARGET | cut -f 1 -d ':' | cut -c 3-`
-    SRCS=`plz query input $TARGET | grep -E "$DIR/[^/]+\.go" | grep -v plz-out`
+    SRCS=`plz query print $TARGET -f srcs | grep -v // | while read SRC; do echo $DIR/$SRC; done`
     FILTERED=`plz-out/bin/tools/please_go_filter/please_go_filter -tags bootstrap $SRCS`
     if [ "$FILTERED" != "" ]; then
         go vet -tags bootstrap $FILTERED || {
