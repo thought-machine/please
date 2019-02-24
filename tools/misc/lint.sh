eb6fb8c507d4868829da9357d9885f154cb64d79
Peter Ebden <peter.ebden@gmail.com>
2019-02-24 12:52:41 +0000

    Strip these struct tags, we don't need them any more

--- tools/misc/lint.sh
+++ tools/misc/lint.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-set -eu
-
 alias plz="plz-out/bin/src/please"
+BLACKLIST="src/parse/asp/main"
 
-for TARGET in `plz query alltargets --include go_src --hidden | grep -v "_test#lib" | grep -v proto`; do
+for TARGET in `plz query alltargets --include go_src --hidden | grep -v "_test#lib" | grep -v proto | grep -v $BLACKLIST`; do
     DIR=`echo $TARGET | cut -f 1 -d ':' | cut -c 3-`
     SRCS=`plz query input $TARGET | grep -E "$DIR/[^/]+\.go" | grep -v plz-out`
     FILTERED=`plz-out/bin/tools/please_go_filter/please_go_filter -tags bootstrap $SRCS`
     if [ "$FILTERED" != "" ]; then
         echo go vet -tags bootstrap $FILTERED
-        go vet -tags bootstrap $FILTERED
+        go vet -tags bootstrap $FILTERED || exit 1
     fi
 done
 
