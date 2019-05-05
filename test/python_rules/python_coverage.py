1beea59916de495ec8f5c01b6f336d4b16eb09b9
Peter Ebden <peter.ebden@gmail.com>
2019-05-05 16:18:21 +0100

    Add a test case for coverage.

---
+++ test/python_rules/python_coverage.py
@@ -0,0 +1,12 @@
+"""This exists only as a test harness for examining slightly non-trivial coverage output."""
+
+
+def calculate_answer(a, b):
+    return a * b
+
+
+def the_answer():
+    return calculate_answer(
+        6,
+        7,
+    )
