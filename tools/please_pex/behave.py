760d7037d70abfe8be4e3b54d0b1eb1554a4b494
Geonaut <oliverbazely@gmail.com>
2019-05-22 15:11:12 +0100

    Explicit feature passing in the behave test runner integration (#621)
    * Behave test runner can now handle feature files in sibling folders
    
    * Typo
    
    * Better description

--- tools/please_pex/behave.py
+++ tools/please_pex/behave.py
@@ -1,17 +1,33 @@
 import os
+import logging
+from pathlib import Path
 
 
 def get_features_dir():
     for i in TEST_NAMES:
         file_name = i.split('/')[-1]
         if '.feature' in file_name:
-            return os.path.dirname(i)
+            return [os.path.dirname(i)]
+
+
+def get_all_feature_files():
+    files = []
+    for feature in Path('.').glob('**/*.feature'):
+        files.append(str(feature))
+    return files
 
 
 def run_tests(args=None):
     from behave.__main__ import main
 
     args = args or []
-    args += [get_features_dir(), '--junit',
-             '--junit-directory', 'test.results']
+
+    if 'all_features=True' in args:
+        logging.info('Getting all feature files')
+        features = get_all_feature_files()
+    else:
+        logging.info('Getting a single feature dir')
+        features = get_features_dir()
+
+    args += features + [ '--junit', '--junit-directory', 'test.results']
     main(args)
