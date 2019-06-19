1d95c411bcc411a8dce599273ad1b549779ad897
Peter Ebden <peter.ebden@gmail.com>
2019-06-19 09:35:15 +0100

    Extend exclude

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,7 +53,7 @@ fi
 # TODO(peterebden): Once Go 1.13 is out we will need to update (or remove) this.
 if [ ! "`go version | grep 1.12`" ]; then
     warn "Go version != 1.12 detected, excluding assembly tests"
-    EXCLUDES="${EXCLUDES} --exclude=asm"
+    EXCLUDES="${EXCLUDES} --exclude=asm --exclude=//test/go_rules/asm_binary/..."
 fi
 if ! hash python2 2>/dev/null ; then
     warn "python2 not found, excluding python2 tests"
