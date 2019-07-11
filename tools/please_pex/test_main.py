337e9daa650e1ce0b529397773973eaa072ef901
Gytis Raciukaitis <gytis.raciukaitis@gmail.com>
2019-07-11 15:24:07 +0300

    fix: updated python coverage paths to new cache location (#678)

--- tools/please_pex/test_main.py
+++ tools/please_pex/test_main.py
@@ -14,9 +14,11 @@ def initialise_coverage():
     _original_xml_file = coverage_control.XmlReporter.xml_file
     # Fix up paths in coverage output which are absolute; we want paths relative to
     # the repository root. Also skip empty __init__.py files.
+    pex_path = pex_paths()[0]
+
     def _xml_file(self, fr, analysis):
-        if '.pex' in fr.filename:
-            fr.filename = fr.filename[fr.filename.index('.pex') + 5:]  # +5 to take off .pex/
+        if pex_path in fr.filename:
+            fr.filename = fr.filename[len(pex_path) + 1:]  # +1 to remove the remaining /
         if fr.filename == '__main__.py':
             return  # Don't calculate coverage for the synthetic entrypoint.
         if not (fr.filename.endswith('__init__.py') and len(analysis.statements) <= 1):
