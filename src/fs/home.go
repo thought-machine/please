22bcc365c19e5ac1712d6c615bb6f6289affc1e3
Peter Ebden <peter.ebden@gmail.com>
2019-09-19 10:53:40 +0100

    Don't use local home dir for remote (#737)

--- src/fs/home.go
+++ src/fs/home.go
@@ -11,7 +11,12 @@ var homeRex = regexp.MustCompile("(?:^|:)(~(?:[/:]|$))")
 
 // ExpandHomePath expands all prefixes of ~ without a user specifier to $HOME.
 func ExpandHomePath(path string) string {
+	return ExpandHomePathTo(path, home)
+}
+
+// ExpandHomePathTo expands all prefixes of ~ without a user specifier to the given string.
+func ExpandHomePathTo(path, to string) string {
 	return homeRex.ReplaceAllStringFunc(path, func(subpath string) string {
-		return strings.Replace(subpath, "~", home, -1)
+		return strings.Replace(subpath, "~", to, -1)
 	})
 }
