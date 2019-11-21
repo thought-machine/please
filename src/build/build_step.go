6b95a4516b3ff292936d69293c2d106227dd39ff
Peter Ebden <peter.ebden@gmail.com>
2019-11-21 16:52:03 +0000

    Force download of targets needed for subinclude()

--- src/build/build_step.go
+++ src/build/build_step.go
@@ -253,7 +253,7 @@ func buildTarget(tid int, state *core.BuildState, target *core.BuildTarget, runR
 	checkLicences(state, target)
 
 	if runRemotely {
-		if state.IsOriginalTarget(target.Label) {
+		if state.IsOriginalTarget(target.Label) || target.NeededForSubinclude {
 			state.LogBuildResult(tid, target.Label, core.TargetBuilding, "Downloading")
 			if _, err := state.RemoteClient.Retrieve(target); err != nil {
 				return fmt.Errorf("Failed to retrieve outputs for %s: %s", target.Label, err)
