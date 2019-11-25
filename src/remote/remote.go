8aa37dbf015a9640d97a70117a5b96f51e61d743
Peter Ebden <peter.ebden@gmail.com>
2019-11-25 14:57:56 +0000

    revert change

--- src/remote/remote.go
+++ src/remote/remote.go
@@ -104,8 +104,7 @@ func (c *Client) init() {
 		conn, err := grpc.Dial(c.state.Config.Remote.URL,
 			grpc.WithTimeout(dialTimeout),
 			grpc.WithInsecure(),
-			grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(grpc_retry.WithMax(maxRetries))),
-			grpc.WithStreamInterceptor(grpc_retry.StreamClientInterceptor(grpc_retry.WithMax(maxRetries))))
+			grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(grpc_retry.WithMax(maxRetries))))
 		if err != nil {
 			return err
 		}
