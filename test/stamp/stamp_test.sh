0cd2c6cb983f6415ef848631b4dfac804d299d0c
vj <vj@thoughtmachine.net>
2019-12-09 13:32:56 +0000

    add human-readable scm identifier

--- test/stamp/stamp_test.sh
+++ test/stamp/stamp_test.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-if [ "`$DATA`" = "12345" ]; then
+git_revision=$(git rev-parse HEAD)
+git_describe=$(git describe --always)
+
+expected="${git_revision}
+${git_describe}"
+
+if [ "`$DATA`" != "${expected}" ]; then
+    echo "`${DATA}` is not equal to ${expected}"
     echo "Stamped variable has not been replaced correctly."
     exit 1
 fi
