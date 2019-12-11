c69ca86c2804b3f1e470f6006da427c06cdcf752
VJ Patel <VJftw@users.noreply.github.com>
2019-12-11 10:51:45 +0000

    add human-readable scm identifier (#789)
    * add human-readable scm identifier
    
    * use 'revision' instead of 'sha' and do not execute 'git' in end-to-end tests for stamp

--- test/stamp/stamp_test.sh
+++ test/stamp/stamp_test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-if [ "`$DATA`" = "12345" ]; then
+unexpected="12345-revision
+12345-describe"
+
+if [ "`$DATA`" == "${unexpected}" ]; then
     echo "Stamped variable has not been replaced correctly."
     exit 1
 fi
