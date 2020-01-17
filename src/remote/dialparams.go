3eae116b1d2a9950ad5b39b5cae3ef2cb23bcff7
Peter Ebden <peter.ebden@gmail.com>
2020-01-17 10:16:11 +0000

    set max message size

--- src/remote/dialparams.go
+++ src/remote/dialparams.go
@@ -14,6 +14,10 @@ func (c *Client) dialParams() client.DialParams {
 		CASService:         c.state.Config.Remote.CASURL,
 		NoSecurity:         !c.state.Config.Remote.Secure,
 		TransportCredsOnly: c.state.Config.Remote.Secure,
-		DialOpts:           []grpc.DialOption{grpc.WithStatsHandler(newStatsHandler(c))},
+		DialOpts: []grpc.DialOption{
+			grpc.WithStatsHandler(newStatsHandler(c)),
+			// Set an arbitrarily large (400MB) max message size so it isn't a limitation.
+			grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(419430400)),
+		},
 	}
 }
