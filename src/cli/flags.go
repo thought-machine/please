1ad365f48623600f273beb9090ecb7ee8daab06c
Peter Ebden <peter.ebden@gmail.com>
2020-01-31 14:35:49 +0000

    update upstream cli lib

--- src/cli/flags.go
+++ src/cli/flags.go
@@ -7,9 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"strconv"
 	"strings"
-	"time"
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/dustin/go-humanize"
@@ -77,26 +75,7 @@ func (b *ByteSize) UnmarshalText(text []byte) error {
 // A Duration is used for flags that represent a time duration; it's just a wrapper
 // around time.Duration that implements the flags.Unmarshaler and
 // encoding.TextUnmarshaler interfaces.
-type Duration time.Duration
-
-// UnmarshalFlag implements the flags.Unmarshaler interface.
-func (d *Duration) UnmarshalFlag(in string) error {
-	d2, err := time.ParseDuration(in)
-	// For backwards compatibility, treat missing units as seconds.
-	if err != nil {
-		if d3, err := strconv.Atoi(in); err == nil {
-			*d = Duration(time.Duration(d3) * time.Second)
-			return nil
-		}
-	}
-	*d = Duration(d2)
-	return flagsError(err)
-}
-
-// UnmarshalText implements the encoding.TextUnmarshaler interface
-func (d *Duration) UnmarshalText(text []byte) error {
-	return d.UnmarshalFlag(string(text))
-}
+type Duration = cli.Duration
 
 // A URL is used for flags or config fields that represent a URL.
 // It's just a string because it's more convenient that way; we haven't needed them as a net.URL so far.
--- src/cli/flags_test.go
+++ src/cli/flags_test.go
@@ -3,7 +3,6 @@ package cli
 import (
 	"runtime"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -16,31 +15,6 @@ func TestByteSize(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, len(extraArgs))
 	assert.EqualValues(t, 15000000, opts.Size)
-}
-
-func TestDuration(t *testing.T) {
-	opts := struct {
-		D Duration `short:"d"`
-	}{}
-	_, extraArgs, err := ParseFlags("test", &opts, []string{"test", "-d=3h"}, 0, nil)
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(extraArgs))
-	assert.EqualValues(t, 3*time.Hour, opts.D)
-
-	_, extraArgs, err = ParseFlags("test", &opts, []string{"test", "-d=3"}, 0, nil)
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(extraArgs))
-	assert.EqualValues(t, 3*time.Second, opts.D)
-}
-
-func TestDurationDefault(t *testing.T) {
-	opts := struct {
-		D Duration `short:"d" default:"3h"`
-	}{}
-	_, extraArgs, err := ParseFlags("test", &opts, []string{"test"}, 0, nil)
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(extraArgs))
-	assert.EqualValues(t, 3*time.Hour, opts.D)
 }
 
 func TestURL(t *testing.T) {
