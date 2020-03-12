a96fbf1c7d4821d17a1d43deadbc839d643cf0a7
Peter Ebden <peter.ebden@gmail.com>
2020-03-12 09:55:16 +0000

    Check if module is callable

--- tools/please_pex/custom.py
+++ tools/please_pex/custom.py
@@ -3,6 +3,10 @@ import importlib
 
 def run_tests(args):
     """Runs tests using a custom test runner that is selected by the user."""
-    runner, _, name = "__TEST_RUNNER__".rpartition('.')
-    mod = importlib.import_module(runner)
-    return getattr(mod, name)(TEST_NAMES, args)
+    runner = "__TEST_RUNNER__"
+    mod_name, _, name = runner.rpartition('.')
+    mod = importlib.import_module(mod_name)
+    f = getattr(mod, name)
+    if not callable(f):
+        raise TypeError('Specified test runner %s is not callable, should be a function taking two arguments' % runner)
+    return f(TEST_NAMES, args)
