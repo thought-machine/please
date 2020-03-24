a3ed38ee26b1176073adb9246a4d56182d353880
Peter Ebden <peter.ebden@gmail.com>
2020-03-24 15:49:49 +0000

    Fix test

--- src/build/build_step_stress_test.go
+++ src/build/build_step_stress_test.go
@@ -65,7 +65,7 @@ func addTarget(state *core.BuildState, i int) *core.BuildTarget {
 				dep := label(i*10 + j)
 				log.Info("Adding dependency %s -> %s", target.Label, dep)
 				target.AddDependency(dep)
-				state.Graph.AddDependency(target, dep)
+				go state.Graph.AddDependency(target, dep)
 			}
 		} else {
 			// These are buildable now
