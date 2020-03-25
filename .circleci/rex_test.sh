d5c4ada9e0199aea50002122e8cbb6d32a095124
Peter Ebden <peter.ebden@gmail.com>
2020-03-25 18:39:58 +0000

    Make it not interactive

--- .circleci/rex_test.sh
+++ .circleci/rex_test.sh
@@ -12,9 +12,9 @@ tar -xzf /tmp/workspace/please_*.tar.gz --strip-components=1 -C $DIR
 export PATH="$DIR:$PATH"
 
 # Start the servers in the background
-plz run parallel //test/remote:run_elan //test/remote:run_zeal //test/remote:run_mettle &
+plz run parallel -p -v 2 --colour //test/remote:run_elan //test/remote:run_zeal //test/remote:run_mettle &
 
 # Test we can rebuild plz itself.
-plz build --profile ci_remote //src:please
+plz build --profile ci_remote -p -v 2 --colour //src:please
 # Check we can actually run some tests
-plz test --profile ci_remote //src/core:all
+plz test --profile ci_remote -p -v 2 --colour //src/core:all
