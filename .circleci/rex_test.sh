b0496e60c5869f02a4767a89866357fd89744d98
Peter Ebden <peter.ebden@gmail.com>
2020-03-26 13:08:57 +0000

    Fix up script a bit

--- .circleci/rex_test.sh
+++ .circleci/rex_test.sh
@@ -4,12 +4,12 @@ set -eu
 
 trap 'killall elan mettle zeal' SIGINT SIGTERM EXIT
 
-DIR="${1:-~/.please}"
+DIR="${1:-/tmp/please}"
 
 # Extract the plz installation from earlier step
 rm -rf "$DIR"
 mkdir "$DIR"
-tar -xzf /tmp/workspace/please_*.tar.gz --strip-components=1 -C "$DIR"
+tar -xzf /tmp/workspace/linux_amd64/please_*.tar.gz --strip-components=1 -C "$DIR"
 ln -s "${DIR}/please" "${DIR}/plz"
 export PATH="$DIR:$PATH"
 
