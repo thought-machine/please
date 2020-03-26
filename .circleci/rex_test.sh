568b478af80e0557d3b1a5f3560cc452309b279a
Peter Ebden <peter.ebden@gmail.com>
2020-03-26 12:19:16 +0000

    Tweak script a bit

--- .circleci/rex_test.sh
+++ .circleci/rex_test.sh
@@ -7,15 +7,16 @@ trap 'killall elan mettle zeal' SIGINT SIGTERM EXIT
 DIR="${1:-~/.please}"
 
 # Extract the plz installation from earlier step
-mkdir -p $DIR
-tar -xzf /tmp/workspace/please_*.tar.gz --strip-components=1 -C $DIR
+rm -rf "$DIR"
+mkdir "$DIR"
+tar -xzf /tmp/workspace/please_*.tar.gz --strip-components=1 -C "$DIR"
 ln -s "${DIR}/please" "${DIR}/plz"
 export PATH="$DIR:$PATH"
 
 # Start the servers in the background
-plz run parallel -p -v 2 --colour --detach //test/remote:run_elan //test/remote:run_zeal //test/remote:run_mettle
+plz run parallel -p -v notice --colour --detach //test/remote:run_elan //test/remote:run_zeal //test/remote:run_mettle
 
 # Test we can rebuild plz itself.
-plz build --profile ci_remote -p -v 2 --colour //src:please
+plz build --profile ci_remote -p -v notice --colour //src:please
 # Check we can actually run some tests
-plz test --profile ci_remote -p -v 2 --colour //src/core:all
+plz test --profile ci_remote -p -v notice --colour //src/core:all
