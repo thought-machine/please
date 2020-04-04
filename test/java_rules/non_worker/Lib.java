5f68923027c0c996996efbc7023bef1f6cba75e9
Peter Ebden <peter.ebden@gmail.com>
2020-04-04 14:41:04 +0100

    Add test related to #860

--- test/java_rules/non_worker/Lib.java
+++ test/java_rules/non_worker/Lib.java
@@ -4,8 +4,10 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class Lib {
-  public String readData() throws Exception {
-    InputStream stream = getClass().getClassLoader().getResourceAsStream("test/java_rules/non_worker/data.txt");
+  public static final String DEFAULT_FILENAME = "test/java_rules/non_worker/data.txt";
+
+  public String readData(String filename) throws Exception {
+    InputStream stream = getClass().getClassLoader().getResourceAsStream(filename);
     java.util.Scanner scanner = new java.util.Scanner(stream).useDelimiter("\\A");
     return scanner.next().trim();
   }
