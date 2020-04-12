0fee965c1d66465b0a030f0cae7376725ccd309e
Peter Ebden <peter.ebden@gmail.com>
2020-04-12 11:50:46 +0100

    Tidy up protoc (#905)
    * Redo docs slightly to be clearer that Go and Git are the only requirements now.
    
    * Simplify bootstrap for hashes
    
    * Should have done this one in the last PR
    
    * Remove from watch
    
    * Remove build constraints from gc

--- src/hashes/stub.go
+++
@@ -1,8 +0,0 @@
-// +build bootstrap
-
-package hashes
-
-import "github.com/thought-machine/please/src/core"
-
-// RewriteHashes is a stub used during initial bootstrap.
-func RewriteHashes(state *core.BuildState, labels []core.BuildLabel) {}
