910a5aa28eebf07d306557e75d313cbf1aef9089
Peter Ebden <peter.ebden@gmail.com>
2020-04-12 13:47:56 +0100

    Remove build directives

--- src/update/stub.go
+++
@@ -1,9 +0,0 @@
-// +build bootstrap
-
-package update
-
-import "github.com/thought-machine/please/src/core"
-
-// CheckAndUpdate is a stub implementation that does nothing.
-func CheckAndUpdate(config *core.Configuration, updatesEnabled, updateCommand, forceUpdate, verify bool) {
-}
