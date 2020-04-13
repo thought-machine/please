d239758cc25891dd3d07e1530eb72c6b4e594a2a
Peter Ebden <peter.ebden@gmail.com>
2020-04-13 11:09:49 +0100

    fix query quickly

--- src/query/whatoutputs_test.go
+++ src/query/whatoutputs_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/thought-machine/please/src/core"
 )
 
-func makeTarget(g *core.BuildGraph, label string, filegroup bool, outputs ...string) *core.BuildTarget {
+func makeTarget2(g *core.BuildGraph, label string, filegroup bool, outputs ...string) *core.BuildTarget {
 	l := core.ParseBuildLabel(label, "")
 	t := core.NewBuildTarget(l)
 
@@ -35,7 +35,7 @@ func makeTarget(g *core.BuildGraph, label string, filegroup bool, outputs ...str
 
 func TestDetectsOutputs(t *testing.T) {
 	graph := core.NewGraph()
-	makeTarget(graph, "//package1:target1", false, "out1", "out2")
+	makeTarget2(graph, "//package1:target1", false, "out1", "out2")
 	targets := whatOutputs(graph.AllTargets(), "plz-out/gen/package1/out1")
 	assert.Equal(t, []core.BuildLabel{{PackageName: "package1", Name: "target1"}}, targets)
 }
@@ -44,8 +44,8 @@ func TestDetectOutputsFilegroup(t *testing.T) {
 	// Slightly different because filegroups register outputs in different ways and
 	// there can be multiple outputting one file.
 	graph := core.NewGraph()
-	makeTarget(graph, "//package1:target1", true, "out1", "out2")
-	makeTarget(graph, "//package1:target2", true, "out1")
+	makeTarget2(graph, "//package1:target1", true, "out1", "out2")
+	makeTarget2(graph, "//package1:target2", true, "out1")
 	targets := whatOutputs(graph.AllTargets(), "plz-out/gen/package1/out1")
 	assert.Equal(t, []core.BuildLabel{
 		{PackageName: "package1", Name: "target1"},
