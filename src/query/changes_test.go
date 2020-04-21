2c3a8295c1be6a87acc82ba5c648851b73089100
Peter Ebden <peter.ebden@gmail.com>
2020-04-21 14:22:12 +0100

    Refactor of query affectedtargets, changes and changed

--- src/query/changes_test.go
+++ src/query/changes_test.go
@@ -8,21 +8,21 @@ import (
 	"github.com/thought-machine/please/src/core"
 )
 
-func TestTargetsChanged(t *testing.T) {
+func TestDiffGraphs(t *testing.T) {
 	s1 := core.NewDefaultBuildState()
 	s2 := core.NewDefaultBuildState()
 	t1 := addTarget(s1, "//src/query:changes", nil, "src/query/changes.go")
 	t2 := addTarget(s2, "//src/query:changes", nil, "src/query/changes.go")
 	addTarget(s1, "//src/query:changes_test", t1, "src/query/changes_test.go")
 	t4 := addTarget(s2, "//src/query:changes_test", t2, "src/query/changes_test.go")
-	assert.EqualValues(t, []core.BuildLabel{}, DiffGraphs(s1, s2, nil))
+	assert.EqualValues(t, []core.BuildLabel{}, DiffGraphs(s1, s2, nil, true, true))
 
 	t2.Command = "nope nope nope"
-	assert.EqualValues(t, []core.BuildLabel{t2.Label, t4.Label}, DiffGraphs(s1, s2, nil))
+	assert.EqualValues(t, []core.BuildLabel{t2.Label, t4.Label}, DiffGraphs(s1, s2, nil, true, true))
 
 	t2.AddLabel("nope")
 	s2.SetIncludeAndExclude(nil, []string{"nope"})
-	assert.EqualValues(t, []core.BuildLabel{}, DiffGraphs(s1, s2, nil))
+	assert.EqualValues(t, []core.BuildLabel{}, DiffGraphs(s1, s2, nil, true, true))
 }
 
 func addTarget(state *core.BuildState, label string, dep *core.BuildTarget, sources ...string) *core.BuildTarget {
