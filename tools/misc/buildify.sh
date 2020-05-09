cc210200e287dc4c8298e4168f2c99c5c932cdb6
Peter Ebden <peter.ebden@gmail.com>
2020-05-09 13:18:21 +0100

    Fix buildify script too

--- tools/misc/buildify.sh
+++ tools/misc/buildify.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-exec plz-out/bin/src/please run -p -- //third_party/go:buildifier $@ `git ls-files | grep BUILD$`
+#!/usr/bin/env bash
+
+plz="plz-out/bin/src/please"
+if [ ! -f $plz ]; then
+    plz="./pleasew"
+fi
+
+exec $plz run -p -- //third_party/go:buildifier $@ `git ls-files | grep BUILD$`
