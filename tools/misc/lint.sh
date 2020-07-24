12188725a7e4b7fb34e3784c2c6f6889a6ccbfe9
Peter Ebden <peter.ebden@gmail.com>
2020-07-24 16:14:40 +0100

    Configure it for CI

--- tools/misc/lint.sh
+++ tools/misc/lint.sh
@@ -7,6 +7,9 @@ if [ "`find src/ tools/ -name '*.go' | xargs gofmt -s -l `" != "" ]; then
     echo "Files are not gofmt'd: find src/ tools/ -name '*.go' | xargs gofmt -s -w"
     exit 1
 fi
+# SA4006 is too unreliable with too many false positives (sad, it seems useful)
+# TODO(peterebden,tatskaari): fix the errcheck issues and enable it.
+golangci-lint run -D errcheck -e SA4006 src/... tools/...
 
 [ -f "plz-out/bin/src/please" ] && PLZ="plz-out/bin/src/please" || PLZ="./pleasew"
 $PLZ run //tools/misc:buildify -p -- --mode=check || {
