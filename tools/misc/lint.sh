79ed7b816029799cb94c0a24af00d2b0940362ad
Peter Ebden <peter.ebden@gmail.com>
2020-07-25 09:56:10 +0100

    Download golangci-lint ourselves

--- tools/misc/lint.sh
+++ tools/misc/lint.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
+[ -f "plz-out/bin/src/please" ] && PLZ="plz-out/bin/src/please" || PLZ="./pleasew"
+
 # SA4006 is too unreliable with too many false positives (sad, it seems useful)
 # TODO(peterebden,tatskaari): fix the errcheck issues and enable it.
-golangci-lint run -D errcheck -e SA4006 src/... tools/...
+$PLZ run //third_party/binary:golangci-lint -p -- run -D errcheck -e SA4006 src/... tools/...
 
-[ -f "plz-out/bin/src/please" ] && PLZ="plz-out/bin/src/please" || PLZ="./pleasew"
 $PLZ run //tools/misc:buildify -p -- --mode=check || {
     echo "BUILD files are not correctly formatted; run plz buildify to fix."
     exit 1
