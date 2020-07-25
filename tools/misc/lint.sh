60f49ea6578c20e3f04c0bb9c137d0fea37bdeaf
Peter Ebden <peter.ebden@gmail.com>
2020-07-25 09:38:13 +0100

    just use golangci-lint

--- tools/misc/lint.sh
+++ tools/misc/lint.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-go vet github.com/thought-machine/please/src/... github.com/thought-machine/please/tools/...
-golint src/... tools/...
-if [ "`find src/ tools/ -name '*.go' | xargs gofmt -s -l `" != "" ]; then
-    echo "Files are not gofmt'd: find src/ tools/ -name '*.go' | xargs gofmt -s -w"
-    exit 1
-fi
 # SA4006 is too unreliable with too many false positives (sad, it seems useful)
 # TODO(peterebden,tatskaari): fix the errcheck issues and enable it.
 golangci-lint run -D errcheck -e SA4006 src/... tools/...
