ea9357d7338ed72e3e755426dd474fc8bc0aa848
Jonathan Poole <jonfpoole@gmail.com>
2020-09-03 13:46:45 +0100

    Make go tool compile use the -importcfg flag for package resolution (#1158)
    * Draft: Attempting to use go tool compile/link -importcfg
    
    * Fix up test import paths
    
    * Cleaned up some . and / problems with find and sed
    
    * Fix missing import config problem for ASM test
    
    * Fixed up how we generate the test package import config
    
    * Fix up benchmark, cgo, and external tests
    
    * Fix binary go-get
    
    * Remove unused pre build from go_library
    
    * Fix up remote exection
    
    * Made import path public
    
    Co-authored-by: jpoole <jpoole@thoughtmachine.net>

--- rules/up_to_date_test.sh
+++ rules/up_to_date_test.sh
@@ -4,6 +4,6 @@ if cmp -s $DATA_BIN_DATA $DATA_GENERATED; then
     echo "Files have the same content"
     exit 0
 else
-    echo "rules/rules.go is out of date, please run //rules:generate"
+    echo "rules/rules.go is out of date, plz run //rules:generate"
     exit 1
 fi
