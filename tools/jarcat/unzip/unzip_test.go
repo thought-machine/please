0a4af9c823b0a6f2d8f42536abdcc1b67076e347
Chris Novakovic <chris@chrisn.me.uk>
2020-09-09 18:04:44 +0100

    jarcat/unzip: create entire path when creating directory Tar members (#1203)
    Following #1200, jarcat/unzip no longer explicitly creates directories
    when processing members of Tar archives. This is problematic when
    extracting an archive containing an empty directory, since there is no
    future action that will cause that directory to be created (e.g. the
    creation of intermediate directories for a child file, since there are
    none).
    
    Mostly revert #1200, and explicitly create directory Tar members using
    os.MkdirAll(), which avoids raising an error when the target path
    already exists (which is the problem #1200 intended to solve).

--- tools/jarcat/unzip/unzip_test.go
+++ tools/jarcat/unzip/unzip_test.go
@@ -7,7 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var files = []string{
+var edgecasesFiles = []string{
+	"top_level_dir/",
+	"top_level_dir/empty_dir/",
+}
+
+var xmlrunnerFiles = []string{
 	"third_party/python/xmlrunner/__init__.py",
 	"third_party/python/xmlrunner/__main__.py",
 	"third_party/python/xmlrunner/builder.py",
@@ -20,17 +25,33 @@ var files = []string{
 	"third_party/python/xmlrunner/version.py",
 }
 
-func TestExtractZip(t *testing.T) {
-	assert.NoError(t, Extract("tools/jarcat/unzip/test_data/xmlrunner.whl", ".", "", ""))
-	for _, file := range files {
+func TestExtractEdgeCasesTar(t *testing.T) {
+	assert.NoError(t, Extract("tools/jarcat/unzip/test_data/edgecases.tar", ".", "", ""))
+	for _, file := range edgecasesFiles {
 		_, err := os.Stat(file)
 		assert.NoError(t, err)
 	}
 }
 
-func TestExtractTar(t *testing.T) {
+func TestExtractEdgeCasesZip(t *testing.T) {
+	assert.NoError(t, Extract("tools/jarcat/unzip/test_data/edgecases.zip", ".", "", ""))
+	for _, file := range edgecasesFiles {
+		_, err := os.Stat(file)
+		assert.NoError(t, err)
+	}
+}
+
+func TestExtractXMLRunnerTar(t *testing.T) {
 	assert.NoError(t, Extract("tools/jarcat/unzip/test_data/xmlrunner.tar", ".", "", ""))
-	for _, file := range files {
+	for _, file := range xmlrunnerFiles {
+		_, err := os.Stat(file)
+		assert.NoError(t, err)
+	}
+}
+
+func TestExtractXMLRunnerZip(t *testing.T) {
+	assert.NoError(t, Extract("tools/jarcat/unzip/test_data/xmlrunner.whl", ".", "", ""))
+	for _, file := range xmlrunnerFiles {
 		_, err := os.Stat(file)
 		assert.NoError(t, err)
 	}
@@ -39,7 +60,7 @@ func TestExtractTar(t *testing.T) {
 func TestPrefix(t *testing.T) {
 	prefix := "third_party/python"
 	assert.NoError(t, Extract("tools/jarcat/unzip/test_data/xmlrunner.whl", ".", "", prefix))
-	for _, file := range files {
+	for _, file := range xmlrunnerFiles {
 		_, err := os.Stat(file[len(prefix)+1:])
 		assert.NoError(t, err)
 	}
