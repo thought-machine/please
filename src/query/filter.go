d72c84e44697c89b558778e02bc54c0d31c4f2b3
jpoole <jpoole@thoughtmachine.net>
2020-09-23 11:20:09 +0100

    Added hidden flag to some of the query subcommands

--- src/query/filter.go
+++ src/query/filter.go
@@ -2,18 +2,21 @@ package query
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/thought-machine/please/src/core"
 )
 
 // Filter takes the list of BuildLabels and checks which ones match the label selectors passed in.
-func Filter(state *core.BuildState, labels core.BuildLabels) {
+func Filter(state *core.BuildState, labels core.BuildLabels, showHidden bool) {
 	// Eventually this could be more clever...
 	matcher := state.ShouldInclude
 
 	for _, label := range labels {
-		if matcher(state.Graph.TargetOrDie(label)) {
-			fmt.Println(label)
+		if showHidden || !strings.HasPrefix(label.Name, "_") {
+			if matcher(state.Graph.TargetOrDie(label)) {
+				fmt.Println(label)
+			}
 		}
 	}
 }
