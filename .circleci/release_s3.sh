104e57258f5c1c329a55012991ec30fd78671fb9
Jonathan Poole <jonfpoole@gmail.com>
2020-10-01 15:34:31 +0100

    Small fixes to release script (#1266)
    * Remove ._* files from OSX build
    
    * Add get please to release
    
    * Update release content types

--- .circleci/release_s3.sh
+++ .circleci/release_s3.sh
@@ -10,16 +10,19 @@ if s3 ls s3://please-releases/linux_amd64/$VERSION; then
 fi
 echo "Releasing Please $VERSION"
 
+find /tmp/workspace/darwin_amd64 -name "._*" | xargs rm -rf
 find /tmp/workspace/*_amd64 -type f | xargs /tmp/workspace/release_signer
 
 aws s3 sync /tmp/workspace/darwin_amd64 s3://please-releases/darwin_amd64/$VERSION
 aws s3 sync /tmp/workspace/linux_amd64 s3://please-releases/linux_amd64/$VERSION
 aws s3 sync /tmp/workspace/freebsd_amd64 s3://please-releases/freebsd_amd64/$VERSION
 
+aws s3 cp tools/misc/get_plz.sh s3://please-releases/get_plz.sh --content-type text/x-shellscript
+
 if [[ "$VERSION" == *"beta"* ]] || [[ "$VERSION" == *"alpha"* ]]; then
   echo "$VERSION is a prerelease, only setting latest_prerelease_version"
 else
   echo "$VERSION is not a prerelease, setting latest_version and latest_prerelease_version"
-  aws s3 cp VERSION s3://please-releases/latest_version
+  aws s3 cp VERSION s3://please-releases/latest_version  --content-type text/plain
 fi
-aws s3 cp VERSION s3://please-releases/latest_prerelease_version
+aws s3 cp VERSION s3://please-releases/latest_prerelease_version  --content-type text/plain
