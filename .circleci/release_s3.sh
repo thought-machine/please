93bab04cccda6afd01b96f8a9edbcd9b80195e0e
tatskaari <jpoole@thoughtmachine.net>
2020-10-01 12:33:39 +0100

    Updated to set latest version and prerelease version

--- .circleci/release_s3.sh
+++ .circleci/release_s3.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 VERSION=$(cat VERSION)
+
 if s3 ls s3://please-releases/linux_amd64/$VERSION; then
   echo "Please $VERSION has already been released, nothing to do."
   exit 0
@@ -12,3 +15,11 @@ find /tmp/workspace/*_amd64 -type f | xargs /tmp/workspace/release_signer
 aws s3 sync /tmp/workspace/darwin_amd64 s3://please-releases/darwin_amd64/$VERSION
 aws s3 sync /tmp/workspace/linux_amd64 s3://please-releases/linux_amd64/$VERSION
 aws s3 sync /tmp/workspace/freebsd_amd64 s3://please-releases/freebsd_amd64/$VERSION
+
+if [[ "$VERSION" == *"beta"* ]] || [[ "$VERSION" == *"alpha"* ]]; then
+  echo "$VERSION is a prerelease, only setting latest_prerelease_version"
+else
+  echo "$VERSION is not a prerelease, setting latest_version and latest_prerelease_version"
+  aws s3 cp VERSION s3://please-releases/latest_version
+fi
+aws s3 cp VERSION s3://please-releases/latest_prerelease_version
