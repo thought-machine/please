88f6eb983e4d140581c9d6bb553adcec54b4aa62
Peter Ebden <peter.ebden@gmail.com>
2020-10-05 15:05:22 +0100

    Make window resizing a no-op on Windows (#1278)
    * Make window resizing a no-op on Windows
    
    * always forget to leave a space here

---
+++ src/cli/winch_other.go
@@ -0,0 +1,19 @@
+// +build !windows
+
+package cli
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// notifyOnWindowResize calls the given function whenever the size of the terminal window changes
+// (when receiving a SIGWINCH on most platforms).
+func notifyOnWindowResize(f func()) {
+	sig := make(chan os.Signal, 10)
+	signal.Notify(sig, syscall.SIGWINCH)
+	for range sig {
+		f()
+	}
+}
