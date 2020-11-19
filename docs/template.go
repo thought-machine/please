670d8e41d4fcb9ecff26154837f2a49ba2dbd134
jpoole <jpoole@thoughtmachine.net>
2020-11-19 15:07:25 +0000

    Added plz query codelab

--- docs/template.go
+++ docs/template.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/peterebden/go-cli-init"
 	"hash/adler32"
 	"io/ioutil"
 	"os"
@@ -47,6 +48,7 @@ var pageTitles = map[string]string{
 	"cache.html":              "Please caching system",
 	"commands.html":           "Please commands",
 	"config.html":             "Please config file reference",
+	"codelabs.html":           "Codelabs",
 	"cross_compiling.html":    "Cross-compiling",
 	"dependencies.html":       "Third-party dependencies",
 	"quickstart_dropoff.html": "What's next?",
@@ -62,7 +64,6 @@ var pageTitles = map[string]string{
 	"quickstart.html":         "Please quickstart",
 	"tests.html":              "Testing with Please",
 	"workers.html":            "Persistent worker processes",
-	"codelabs/codelabs.html":  "Codelabs",
 }
 
 func mustRead(filename string) string {
@@ -96,9 +97,20 @@ func mustHighlight(contents string) string {
 	})
 }
 
+var opts struct{
+	In string `long:"in" description:"The file to template"`
+	Filename string `short:"f" long:"file" description:"The final file name relative to the web root" default:""`
+	Template string `short:"t" long:"template" description:"The golang template to use"`
+}
+
 func main() {
-	filename := os.Args[2]
-	basename := strings.TrimPrefix(filename, "docs/")
+	cli.ParseFlagsOrDie("Docs template", &opts)
+
+	if opts.Filename == "" {
+		opts.Filename = strings.TrimPrefix(opts.In, "docs/")
+	}
+
+	basename := strings.TrimPrefix(opts.Filename, "docs/")
 	basenameIndex := int(adler32.Checksum([]byte(basename)))
 	modulo := func(s []string, i int) string { return s[(basenameIndex+i)%len(s)] }
 	random := func(x, min, max int) int { return (x*basenameIndex+min)%(max-min) + min }
@@ -125,12 +137,12 @@ func main() {
 		},
 	}
 	var title string
-	if filepath.Dir(filename) == "docs/milestones" {
+	if filepath.Dir(opts.Filename) == "milestones" {
 		title = fmt.Sprintf("Please v%v", strings.TrimSuffix(basename, ".html"))
 	} else {
-		t, present := pageTitles[basename]
+		t, present := pageTitles[opts.Filename]
 		if !present {
-			panic("missing title for " + basename)
+			panic("missing title for " + opts.Filename)
 		}
 		title = t
 	}
@@ -140,16 +152,16 @@ func main() {
 		SideImages                        []int
 	}{
 		Title:    title,
-		Header:   mustRead(os.Args[1]),
-		Contents: mustHighlight(mustRead(filename)),
-		Filename: basename,
+		Header:   mustRead(opts.Template),
+		Contents: mustHighlight(mustRead(opts.In)),
+		Filename: opts.Filename,
 	}
 	for i := 0; i <= strings.Count(data.Contents, "\n")/150; i++ {
 		// Awkwardly this seems to have to be a slice to range over in the template.
 		data.SideImages = append(data.SideImages, i+1)
 	}
 
-	tmpl := template.Must(template.New("tmpl").Funcs(funcs).Parse(mustRead(os.Args[1])))
+	tmpl := template.Must(template.New("tmpl").Funcs(funcs).Parse(mustRead(opts.Template)))
 	err := tmpl.Execute(os.Stdout, data)
 	if err != nil {
 		panic(err)
