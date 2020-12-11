9beb2dd1f6584970dd6192dc7f3d91b0d04a8153
jpoole <jpoole@thoughtmachine.net>
2020-12-11 11:43:03 +0000

    Updated to fix /home/jpoole issue with remote

--- src/remote/action.go
+++ src/remote/action.go
@@ -82,7 +82,7 @@ func (c *Client) buildCommand(target *core.BuildTarget, inputRoot *pb.Directory,
 	}
 	// We can't predict what variables like this should be so we sneakily bung something on
 	// the front of the command. It'd be nicer if there were a better way though...
-	var commandPrefix = "export TMP_DIR=\"`pwd`\" && "
+	var commandPrefix = "export TMP_DIR=\"`pwd`\" && export HOME=$TMP_DIR && "
 	// TODO(peterebden): Remove this nonsense once API v2.1 is released.
 	files, dirs := outputs(target)
 	if len(target.Outputs()) == 1 { // $OUT is relative when running remotely; make it absolute
