71e55dccaa39c672fa34edf56cc73eff24974377
tatskaari <jpoole@thoughtmachine.net>
2020-12-15 16:07:56 +0000

    Reinstating .tar.gz

--- .circleci/http_cache_test.sh
+++ .circleci/http_cache_test.sh
@@ -9,7 +9,7 @@ DIR="${1:-/tmp/please}"
 # Extract the plz installation from earlier step
 rm -rf "$DIR"
 mkdir "$DIR"
-tar -xzf /tmp/workspace/linux_amd64/please_*.tar.xz --strip-components=1 -C "$DIR"
+tar -xzf /tmp/workspace/linux_amd64/please_*.tar.gz --strip-components=1 -C "$DIR"
 ln -s "${DIR}/please" "${DIR}/plz"
 export PATH="$DIR:$PATH"
 
