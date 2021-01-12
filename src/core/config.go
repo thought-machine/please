3d2eb4d82482588ea09cda2943c9ece33d8290a5
Xinke Chen <xinkecf35@users.noreply.github.com>
2021-01-12 03:44:42 -0500

    Add Config Option to Disable Vendor Specific Flags for Pip in pip_library (#1470)
    * Added python config definition for disabling vendor flag
    
    * Modified check to include disable vendor flags
    
    * Updated docs with config flag
    
    * Updated rules.go with plz generation
    
    * Update DisableVendorFlags docs with new structure
    
    Co-authored-by: Xinke Chen <xinke@xinkechen.me>

--- src/core/config.go
+++ src/core/config.go
@@ -282,6 +282,7 @@ func DefaultConfiguration() *Configuration {
 	config.Go.InstallTool = "please_go_install"
 	config.Python.PexTool = "please_pex"
 	config.Python.DefaultInterpreter = "python3"
+	config.Python.DisableVendorFlags = false
 	config.Python.TestRunner = "unittest"
 	config.Python.TestRunnerBootstrap = ""
 	config.Python.UsePyPI = true
@@ -459,6 +460,7 @@ type Configuration struct {
 		UsePyPI             bool    `help:"Whether or not to use PyPI for pip_library rules or not. Defaults to true, if you disable this you will presumably want to set DefaultPipRepo to use one of your own.\nIs overridden by the use_pypi argument to pip_library." var:"USE_PYPI"`
 		WheelNameScheme     string  `help:"Defines a custom templatized wheel naming scheme. Templatized variables should be surrounded in curly braces, and the available options are: url_base, package_name, and version. The default search pattern is '{url_base}/{package_name}-{version}-${{OS}}-${{ARCH}}.whl' along with a few common variants." var:"PYTHON_WHEEL_NAME_SCHEME"`
 		InterpreterOptions  string  `help:"Options to pass to the python interpeter, when writing shebangs for pex executables." var:"PYTHON_INTERPRETER_OPTIONS"`
+		DisableVendorFlags  bool    `help:"Disables injection of vendor specific flags for pip while using pip_library. The option can be useful if you are using something like Pyenv, and the passing of additional flags or configuration that are vendor specific, e.g. --system, breaks your build." var:"DISABLE_VENDOR_FLAGS"`
 	} `help:"Please has built-in support for compiling Python.\nPlease's Python artifacts are pex files, which are essentially self-executable zip files containing all needed dependencies, bar the interpreter itself. This fits our aim of at least semi-static binaries for each language.\nSee https://github.com/pantsbuild/pex for more information.\nNote that due to differences between the environment inside a pex and outside some third-party code may not run unmodified (for example, it cannot simply open() files). It's possible to work around a lot of this, but if it all becomes too much it's possible to mark pexes as not zip-safe which typically resolves most of it at a modest speed penalty."`
 	Java struct {
 		JavacTool          string    `help:"Defines the tool used for the Java compiler. Defaults to javac." var:"JAVAC_TOOL"`
