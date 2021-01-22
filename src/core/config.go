0226e7ae1c7bd4e9394eebe2533c5cafa5600124
linuxerwang <linuxerwang@gmail.com>
2021-01-22 10:26:46 -0800

    Demonstrate how to make grpc_library use google.golang.org/protobuf (#1488)
    * Demonstrate how to make grpc_library use google.golang.org/protobuf instead of github.com/golang/protubuf.
    
    Not aimed for merge, because it's not backward compatible.
    
    * Added feature flag and test
    
    * Fix test
    
    * Switch on config option rather than feature flag
    
    * Update config description
    
    Co-authored-by: work <work@jons-mbp.mynet>

--- src/core/config.go
+++ src/core/config.go
@@ -525,6 +525,7 @@ type Configuration struct {
 		ProtocGoPlugin   string   `help:"The binary passed to protoc as a plugin to generate Go code. Defaults to protoc-gen-go.\nWe've found this easier to manage with a go_get rule instead though, so you can also pass a build label here. See the Please repo for an example." var:"PROTOC_GO_PLUGIN"`
 		GrpcPythonPlugin string   `help:"The plugin invoked to compile Python code for grpc_library.\nDefaults to protoc-gen-grpc-python." var:"GRPC_PYTHON_PLUGIN"`
 		GrpcJavaPlugin   string   `help:"The plugin invoked to compile Java code for grpc_library.\nDefaults to protoc-gen-grpc-java." var:"GRPC_JAVA_PLUGIN"`
+		GrpcGoPlugin     string   `help:"The plugin invoked to compile Go code for grpc_library.\nIf not set, then the protoc plugin will be used instead." var:"GRPC_GO_PLUGIN"`
 		GrpcCCPlugin     string   `help:"The plugin invoked to compile C++ code for grpc_library.\nDefaults to grpc_cpp_plugin." var:"GRPC_CC_PLUGIN"`
 		Language         []string `help:"Sets the default set of languages that proto rules are built for.\nChosen from the set of {cc, java, go, py}.\nDefaults to all of them!" var:"PROTO_LANGUAGES"`
 		PythonDep        string   `help:"An in-repo dependency that's applied to any Python proto libraries." var:"PROTO_PYTHON_DEP"`
