4b96f74c5975983e8acf1e9380262db8871761b3
jpoole <jpoole@thoughtmachine.net>
2021-02-05 17:31:50 +0000

    Fix remote env

--- src/remote/action.go
+++ src/remote/action.go
@@ -83,6 +83,11 @@ func (c *Client) buildCommand(target *core.BuildTarget, inputRoot *pb.Directory,
 	// We can't predict what variables like this should be so we sneakily bung something on
 	// the front of the command. It'd be nicer if there were a better way though...
 	var commandPrefix = "export TMP_DIR=\"`pwd`\" && export HOME=$TMP_DIR && "
+
+	for k, v := range target.Env {
+		commandPrefix += fmt.Sprintf("export %s=%s && ", k, v)
+	}
+
 	// TODO(peterebden): Remove this nonsense once API v2.1 is released.
 	files, dirs := outputs(target)
 	if len(target.Outputs()) == 1 { // $OUT is relative when running remotely; make it absolute
