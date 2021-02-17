65a9a7d7cac71ab2d05ed86571dba460b853a9b5
tatskaari <jpoole@thoughtmachine.net>
2021-02-17 16:30:15 +0000

    Make install add pleasew to path instead of plz itself

--- tools/misc/get_plz.sh
+++ tools/misc/get_plz.sh
@@ -22,26 +22,31 @@ LOCATION="${HOME}/.please"
 DIR="${LOCATION}/${VERSION}"
 mkdir -p "$DIR"
 
-echo "Downloading Please ${VERSION} to ${DIR}..."
+echo "Downloading Please ${VERSION}..."
 curl -fsSL "${PLEASE_URL}" | tar -xzpf- --strip-components=1 -C "$DIR"
 # Link it all back up a dir
 for x in `ls "$DIR"`; do
     ln -sf "${DIR}/${x}" "$LOCATION"
 done
 ln -sf "${LOCATION}/please" "${LOCATION}/plz"
+mkdir "${LOCATION}/bin" && curl http://get.please.build/pleasew -s --output "${LOCATION}/bin/plz"
+
+echo
 
 if ! hash plz 2>/dev/null; then
-    echo "Adding plz to PATH..."
     if [ -d ~/.local/bin ]; then
+        echo "Adding plz to ~/.local/bin..."
         ln -s ~/.please/plz ~/.local/bin/plz
     elif [ -f ~/.profile ]; then
-        echo 'export PATH="${PATH}:${HOME}/.please"' >> ~/.profile
-        echo "You may need to run 'source ~/.profile' to pick up the new PATH."
+        echo 'export PATH="${PATH}:${HOME}/.please/bin"' >> ~/.profile
+        echo "Added Please to path. You may need to run 'source ~/.profile' to pick up the new PATH."
     else
-        echo "Unsure how to add to PATH, not modifying anything. If desired add '${HOME}/.please' manually to your PATH."
+        echo "Unsure how to add to PATH, not modifying anything. If desired add '~/.please/bin' to your PATH"
+        echo "or, install please system-wide with 'sudo cp ~/.please/bin/* /usr/bin' or similar"
     fi
 fi
 
-echo "Please installed."
+echo
+echo "Please has been installed under ${LOCATION}"
 echo "Run plz --help for more information about how to invoke it,"
 echo "or plz help for information on specific help topics."
