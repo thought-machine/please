aa97a128261dd496ed039a502b46a298ba2ad8d7
tatskaari <jpoole@thoughtmachine.net>
2021-02-19 13:07:59 +0000

    Lint fixes

--- tools/misc/lint.sh
+++ tools/misc/lint.sh
@@ -3,7 +3,7 @@ set -eu
 
 [ -f "plz-out/bin/src/please" ] && PLZ="plz-out/bin/src/please" || PLZ="./pleasew"
 
-$PLZ run //third_party/binary:golangci-lint -p -- run --sort-results src/... tools/...
+$PLZ run //third_party/binary:golangci-lint -p -- run --skip-dirs test_data --sort-results src/... tools/...
 $PLZ fmt -q || {
     echo "BUILD files are not correctly formatted; run plz fmt -w to fix."
     exit 1
