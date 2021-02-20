7d510b53c9d2d9cd338baf946a27d37a2e6ca1bc
Peter Bocan <me@pbocan.net>
2021-02-20 18:00:49 +0000

    Add support for strings in tools argument

--- src/parse/asp/targets.go
+++ src/parse/asp/targets.go
@@ -207,8 +207,8 @@ func populateTarget(s *scope, t *core.BuildTarget, args []pyObject) {
 	} else {
 		addMaybeNamed(s, "srcs", args[srcsBuildRuleArgIdx], t.AddSource, t.AddNamedSource, false, false)
 	}
-	addMaybeNamed(s, "tools", args[toolsBuildRuleArgIdx], t.AddTool, t.AddNamedTool, true, true)
-	addMaybeNamed(s, "test_tools", args[testToolsBuildRuleArgIdx], t.AddTestTool, t.AddNamedTestTool, true, true)
+	addMaybeNamedOrString(s, "tools", args[toolsBuildRuleArgIdx], t.AddTool, t.AddNamedTool, true, true)
+	addMaybeNamedOrString(s, "test_tools", args[testToolsBuildRuleArgIdx], t.AddTestTool, t.AddNamedTestTool, true, true)
 	addMaybeNamed(s, "system_srcs", args[systemSrcsBuildRuleArgIdx], t.AddSource, nil, true, false)
 	addMaybeNamed(s, "data", args[dataBuildRuleArgIdx], t.AddDatum, t.AddNamedDatum, false, false)
 	addMaybeNamedOutput(s, "outs", args[outsBuildRuleArgIdx], t.AddOutput, t.AddNamedOutput, t, false)
@@ -294,6 +294,20 @@ func addMaybeNamed(s *scope, name string, obj pyObject, anon func(core.BuildInpu
 	} else if obj != None {
 		s.Assert(false, "Argument %s must be a list or dict, not %s", name, obj.Type())
 	}
+}
+
+// addMaybeNamedOrString adds inputs to a target, possibly in named groups.
+func addMaybeNamedOrString(s *scope, name string, obj pyObject, anon func(core.BuildInput), named func(string, core.BuildInput), systemAllowed, tool bool) {
+	if obj == nil {
+		return
+	}
+	if str, ok := asString(obj); ok {
+		if bi := parseBuildInput(s, str, name, systemAllowed, tool); bi != nil {
+			anon(bi)
+		}
+		return
+	}
+	addMaybeNamed(s, name, obj, anon, named, systemAllowed, tool)
 }
 
 // addMaybeNamedOutput adds outputs to a target, possibly in a named group
@@ -557,4 +571,12 @@ func asDict(obj pyObject) (pyDict, bool) {
 		return d.pyDict, true
 	}
 	return nil, false
+}
+
+// asString converts an object to a pyString
+func asString(obj pyObject) (pyString, bool) {
+	if s, ok := obj.(pyString); ok {
+		return s, true
+	}
+	return "", false
 }
