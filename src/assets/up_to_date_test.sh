8c92fa7e0bf6f42c218319715e4df721a9a52c66
Jonathan Poole <jonfpoole@gmail.com>
2021-03-16 22:31:46 +0000

    Embed assets (#1662)
    * Embed assets
    
    * Lint fixes

--- src/assets/up_to_date_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if cmp -s $DATA_BIN_DATA $DATA_GENERATED; then
-    echo "Files have the same content"
-    exit 0
-else
-    echo "src/assets/bindata.go is out of date, plz run //src/assets:generate"
-    exit 1
-fi
