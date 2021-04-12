48afb61ee4c2b28c4ef28a8836e4f7ede722e0ae
Peter Ebden <peter.ebden@gmail.com>
2021-04-12 19:16:25 +0100

    correct field name here

--- src/build/incrementality_test.go
+++ src/build/incrementality_test.go
@@ -57,7 +57,7 @@ var KnownFields = map[string]bool{
 	"ExitOnError":                 true,
 	"EntryPoints":                 true,
 	"Env":                         true,
-	"Platform":                    true,
+	"RemotePlatform":              true,
 
 	// These only contribute to the runtime hash, not at build time.
 	"Data":              true,
