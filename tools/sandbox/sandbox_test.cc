869f8e083edcc191cbeed8be4696f42ed2a18a39
Peter Ebden <peter.ebden@gmail.com>
2021-04-16 20:53:40 +0100

    Rewrite paths in sandbox binary (#1747)
    * Rewrite paths in env vars
    
    * make this visible on OSX too

--- tools/sandbox/sandbox_test.cc
+++ tools/sandbox/sandbox_test.cc
@@ -1,5 +1,7 @@
 #include <UnitTest++/UnitTest++.h>
 
+#include <cstring>
+
 extern "C" {
 #include "tools/sandbox/sandbox.h"
 }
@@ -21,6 +23,37 @@ TEST(ExecNameShorterThanSandboxDir) {
 TEST(SameDir) {
   // We wouldn't normally do this but it should still work fine.
   CHECK_EQUAL("/tmp/plz_sandbox/test.bin", exec_name("/tmp/plz_sandbox/test.bin", "/tmp/plz_sandbox", "/tmp/plz_sandbox"));
+}
+
+TEST(ChangePath) {
+  CHECK_EQUAL("RESULTS_FILE=/tmp/plz_sandbox/test.results", change_path(
+      "RESULTS_FILE=/home/peter/git/please/plz-out/tmp/my_test/test.results",
+      "/home/peter/git/please/plz-out/tmp/my_test",
+      "/tmp/plz_sandbox",
+      strlen("RESULTS_FILE=")));
+}
+
+TEST(ChangeEnvVars) {
+  char* env[] = {
+    strdup("TMP_DIR=/home/peter/git/please/plz-out/tmp/my_test"),
+    strdup("RESULTS_FILE=/home/peter/git/please/plz-out/tmp/my_test/test.results"),
+    strdup("SOME_TOOL=/usr/local/bin/go"),
+    strdup("thirty-five ham and cheese sandwiches"),
+    NULL
+  };
+  char* expected[] = {
+    strdup("TMP_DIR=/tmp/plz_sandbox"),
+    strdup("RESULTS_FILE=/tmp/plz_sandbox/test.results"),
+    strdup("SOME_TOOL=/usr/local/bin/go"),
+    strdup("thirty-five ham and cheese sandwiches"),
+    NULL
+  };
+  change_env_vars(env, "/home/peter/git/please/plz-out/tmp/my_test", "/tmp/plz_sandbox");
+  CHECK_EQUAL(expected[0], env[0]);
+  CHECK_EQUAL(expected[1], env[1]);
+  CHECK_EQUAL(expected[2], env[2]);
+  CHECK_EQUAL(expected[3], env[3]);
+  CHECK_EQUAL(expected[4], env[4]);
 }
 
 }
