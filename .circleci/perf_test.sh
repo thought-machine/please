c269829bc4333ceb3423acb6e27a673f7734ce82
Peter Ebden <peter.ebden@gmail.com>
2021-05-05 17:47:38 +0100

    Hook it up in CircleCI

--- .circleci/perf_test.sh
+++ .circleci/perf_test.sh
@@ -6,13 +6,13 @@ PLZ="/tmp/workspace/linux_amd64/please"
 REV="`git rev-parse HEAD`"
 
 echo "Generating test file tree"
-"$PLZ" run //tools/misc:gen_parse_tree -- --plz "$PLZ"
+/tmp/workspace/gen_parse_tree.pex -- --plz "$PLZ"
 echo "Running parse performance test"
-"$PLZ" run //tools/performance:parse_perf_test -- --plz "$PLZ"
+/tmp/workspace/parse_perf_test.pex -- --plz "$PLZ"
 echo "Uploading results..."
 aws s3 cp plz.prof "s3://please-releases/performance/${REVISION}.prof"
 aws s3 cp results.json "s3://please-releases/performance/${REVISION}.json"
 aws s3 cp s3://please-releases/performance/all_results.json all_results.json
-cat all_results.json results.json > updated_results.json
+cat all_results.json results.json | tail -n 100 > updated_results.json
 aws s3 cp updated_results.json s3://please-releases/performance/all_results.json
 echo "Done!"
