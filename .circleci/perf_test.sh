d653f042e0119a6ca17c9f0b009162b5cf2cdd3f
Peter Ebden <peter.ebden@gmail.com>
2021-05-06 18:00:54 +0100

    Disable progress bar stuff for perftest in ci (#1799)
    * Make progress bars optional
    
    * Don't actually need git
    
    * Put the image back since we don't need git any more

--- .circleci/perf_test.sh
+++ .circleci/perf_test.sh
@@ -3,16 +3,15 @@
 set -eu
 
 PLZ="/tmp/workspace/linux_amd64/please"
-REV="`git rev-parse HEAD`"
 BUCKET="s3://please-releases/performance"
 
 echo "Generating test file tree"
-/tmp/workspace/gen_parse_tree.pex -- --plz "$PLZ"
+/tmp/workspace/gen_parse_tree.pex -- --plz "$PLZ" --noprogress
 echo "Running parse performance test"
 /tmp/workspace/parse_perf_test.pex -- --plz "$PLZ"
 echo "Uploading results..."
-aws s3 cp plz.prof "${BUCKET}/${REVISION}.prof"
-aws s3 cp results.json "${BUCKET}/${REVISION}.json"
+aws s3 cp plz.prof "${BUCKET}/${CIRCLE_SHA1}.prof"
+aws s3 cp results.json "${BUCKET}/${CIRCLE_SHA1}.json"
 if aws s3 ls "${BUCKET}/all_results.jsonl"; then
     aws s3 cp "${BUCKET}/all_results.jsonl" all_results.jsonl
     cat all_results.jsonl results.json | tail -n 100 > updated_results.jsonl
