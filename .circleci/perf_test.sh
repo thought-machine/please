b38a0cbd4044f10f2ed3f06813835a0feeb926dd
Peter Ebden <peter.ebden@gmail.com>
2021-05-06 17:50:59 +0100

    Don't actually need git

--- .circleci/perf_test.sh
+++ .circleci/perf_test.sh
@@ -3,7 +3,6 @@
 set -eu
 
 PLZ="/tmp/workspace/linux_amd64/please"
-REV="`git rev-parse HEAD`"
 BUCKET="s3://please-releases/performance"
 
 echo "Generating test file tree"
@@ -11,8 +10,8 @@ echo "Generating test file tree"
 echo "Running parse performance test"
 /tmp/workspace/parse_perf_test.pex -- --plz "$PLZ"
 echo "Uploading results..."
-aws s3 cp plz.prof "${BUCKET}/${REVISION}.prof"
-aws s3 cp results.json "${BUCKET}/${REVISION}.json"
+aws s3 cp plz.prof "${BUCKET}/${CIRCLE_SHA1}.prof"
+aws s3 cp results.json "${BUCKET}/${CIRCLE_SHA1}.json"
 if aws s3 ls "${BUCKET}/all_results.jsonl"; then
     aws s3 cp "${BUCKET}/all_results.jsonl" all_results.jsonl
     cat all_results.jsonl results.json | tail -n 100 > updated_results.jsonl
