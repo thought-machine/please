4df644ed69c21aa19d6b44399a4fb1a4a164946a
Peter Ebden <peter.ebden@gmail.com>
2021-05-06 18:07:10 +0100

    drop lingering --

--- .circleci/perf_test.sh
+++ .circleci/perf_test.sh
@@ -6,9 +6,9 @@ PLZ="/tmp/workspace/linux_amd64/please"
 BUCKET="s3://please-releases/performance"
 
 echo "Generating test file tree"
-/tmp/workspace/gen_parse_tree.pex -- --plz "$PLZ" --noprogress
+/tmp/workspace/gen_parse_tree.pex --plz "$PLZ" --noprogress
 echo "Running parse performance test"
-/tmp/workspace/parse_perf_test.pex -- --plz "$PLZ"
+/tmp/workspace/parse_perf_test.pex --plz "$PLZ"
 echo "Uploading results..."
 aws s3 cp plz.prof "${BUCKET}/${CIRCLE_SHA1}.prof"
 aws s3 cp results.json "${BUCKET}/${CIRCLE_SHA1}.json"
