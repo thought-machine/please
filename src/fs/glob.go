a6902b9c7fc180efd5260ed5c6ddb12397e13ea3
Peter Ebden <peter.ebden@gmail.com>
2021-05-07 15:54:32 +0100

    handle glob strings

--- src/fs/glob.go
+++ src/fs/glob.go
@@ -156,6 +156,11 @@ func (globber *Globber) Glob(rootPath string, includes, excludes []string, inclu
 	var filenames []string
 	for _, include := range includes {
 		mustBeValidGlobString(include)
+		// TODO(peterebden): Add this to mustBeValidGlobString as a fatal error in v17
+		if strings.HasPrefix(include, "/") {
+			log.Warning("Glob expression %s should not begin with a /", include)
+			include = strings.TrimPrefix(include, "/")
+		}
 
 		matches, err := globber.glob(rootPath, include, excludes, includeHidden)
 		if err != nil {
