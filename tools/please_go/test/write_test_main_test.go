7a3c271fb55353633638356c90313f8bfd38c6d1
Jonathan Poole <jpoole@thoughtmachine.net>
2021-06-28 18:24:21 +0100

    Fix up some more tests

--- tools/please_go/test/write_test_main_test.go
+++ tools/please_go/test/write_test_main_test.go
@@ -48,8 +48,8 @@ func TestParseTestSourcesFailsGracefully(t *testing.T) {
 
 func TestWriteTestMain(t *testing.T) {
 	err := WriteTestMain(
-		"tools/please_go/test/test_data",
 		"",
+		"test_pkg",
 		[]string{"tools/please_go/test/test_data/test/example_test.go"},
 		"test.go",
 		false,
@@ -66,8 +66,8 @@ func TestWriteTestMain(t *testing.T) {
 
 func TestWriteTestMainWithCoverage(t *testing.T) {
 	err := WriteTestMain(
-		"tools/please_go/test/test_data",
 		"",
+		"test_package",
 		[]string{"tools/please_go/test/test_data/test/example_test.go"},
 		"test.go",
 		true,
@@ -89,8 +89,8 @@ func TestWriteTestMainWithCoverage(t *testing.T) {
 
 func TestWriteTestMainWithBenchmark(t *testing.T) {
 	err := WriteTestMain(
-		"tools/please_go/test/test_data",
 		"",
+		"test_package",
 		[]string{"tools/please_go/test/test_data/bench/example_benchmark_test.go"},
 		"test.go",
 		true,
@@ -110,22 +110,22 @@ func TestWriteTestMainWithBenchmark(t *testing.T) {
 }
 
 func TestExtraImportPaths(t *testing.T) {
-	assert.Equal(t, extraImportPaths("core", "src/core", "", []CoverVar{
+	assert.Equal(t, extraImportPaths("core", "", []CoverVar{
 		{ImportPath: "core"},
 		{ImportPath: "output"},
 	}), []string{
-		"core \"core\"",
+		"\"core\"",
 		"_cover0 \"core\"",
 		"_cover1 \"output\"",
 	})
 }
 
 func TestExtraImportPathsWithImportPath(t *testing.T) {
-	assert.Equal(t, extraImportPaths("core", "src/core", "github.com/thought-machine/please", []CoverVar{
+	assert.Equal(t, extraImportPaths("core", "github.com/thought-machine/please", []CoverVar{
 		{ImportPath: "src/core"},
 		{ImportPath: "output"},
 	}), []string{
-		"core \"github.com/thought-machine/please/src/core\"",
+		"\"core\"",
 		"_cover0 \"github.com/thought-machine/please/src/core\"",
 		"_cover1 \"github.com/thought-machine/please/output\"",
 	})
