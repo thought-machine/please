fbc0258cf113b0f693b8b83612e785773454cb36
jpoole <jpoole@thoughtmachine.net>
2021-06-29 12:36:26 +0100

    Fix asm

--- test/go_rules/package_test.go
+++ test/go_rules/package_test.go
@@ -3,7 +3,7 @@ package package_lib_test
 import (
 	"testing"
 
-	"github.com/thought-machine/please/test/go_rules"
+	"github.com/thought-machine/please/test/go_rules/package_lib"
 )
 
 func TestFoo(t *testing.T) {
