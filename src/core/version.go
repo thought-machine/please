1b460e91d6ba119f34a905e2c80074231383133e
Peter Ebden <peter.ebden@gmail.com>
2021-07-01 16:02:46 +0100

    Use a definition

--- src/core/version.go
+++ src/core/version.go
@@ -2,4 +2,4 @@ package core
 
 // PleaseVersion is the current version of Please.
 // Note that non-bootstrap builds replace this interim version with a real one.
-const PleaseVersion = "1.0.9999"
+var PleaseVersion = "1.0.9999"
