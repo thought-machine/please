ba565c4696440c5f0e9226edfc6c0b4e5da0b29a
Tiago Tristao <tiago.v.tristao@gmail.com>
2021-07-13 18:12:42 +0100

    Clean up

--- test/plz_exec/test_repo/network_test.go
+++ test/plz_exec/test_repo/network_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	if _, err := net.DialTimeout("tcp", "google.com:80", 1*time.Second); err != nil {
+	if _, err := net.DialTimeout("tcp", "google.com:80", time.Second); err != nil {
 		log.Fatal(err)
 	}
 }
