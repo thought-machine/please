5f5d7e37130cc87c9359f1e13f60e385933f7029
Tiago Tristao <tiago.v.tristao@gmail.com>
2022-02-14 13:46:16 +0000

    Minimal plz shim that handles first installation and updates before executing main binary

---
+++ tools/please_wrapper/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
+
+	"github.com/thought-machine/please/src/core"
+	"github.com/thought-machine/please/src/fs"
+	"github.com/thought-machine/please/src/update"
+)
+
+func main() {
+	core.MustFindRepoRoot()
+	if err := os.Chdir(core.RepoRoot); err != nil {
+		log.Fatalf("Error moving to '%s' repo root: %s", core.RepoRoot, err)
+	}
+
+	config, err := core.ReadDefaultConfigFiles(nil)
+	if err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+
+	plzPath := filepath.Join(config.Please.Location, "please")
+
+	if fs.FileExists(plzPath) {
+		out, err := exec.Command(plzPath, "--version").Output()
+		if err != nil {
+			log.Fatalf("Error getting Please version: %s", err)
+		}
+
+		core.PleaseVersion = strings.TrimSpace(strings.TrimPrefix(string(out), "Please version "))
+	}
+
+	update.CheckAndUpdate(config, true, false, false, true, true, false)
+
+	if err := syscall.Exec(plzPath, append(os.Args, "--noupdate"), os.Environ()); err != nil {
+		log.Fatalf("Failed to execute Please: %s", err)
+	}
+}
