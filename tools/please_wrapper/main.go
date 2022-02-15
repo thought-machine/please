24c3809061ff60105ef7c9b31dc51d4b39f68cd6
Tiago Tristao <tiago.v.tristao@gmail.com>
2022-02-15 12:28:50 +0000

    Don't override default log file

--- tools/please_wrapper/main.go
+++ tools/please_wrapper/main.go
@@ -8,6 +8,9 @@ import (
 	"strings"
 	"syscall"
 
+	"gopkg.in/op/go-logging.v1"
+
+	"github.com/thought-machine/please/src/cli"
 	"github.com/thought-machine/please/src/core"
 	"github.com/thought-machine/please/src/fs"
 	"github.com/thought-machine/please/src/update"
@@ -18,6 +21,15 @@ func main() {
 	if err := os.Chdir(core.RepoRoot); err != nil {
 		log.Fatalf("Error moving to '%s' repo root: %s", core.RepoRoot, err)
 	}
+
+	tmpDir := os.TempDir()
+	if tmpDir == "" {
+		log.Fatal("No default directory to use for temporary files found")
+	}
+
+	// Prevent the repo's default `plz-out/log/build.log` file from being overriden
+	logFilePath := filepath.Join(tmpDir, "plz_build.log")
+	cli.InitFileLogging(logFilePath, cli.Verbosity(logging.DEBUG), false)
 
 	config, err := core.ReadDefaultConfigFiles(nil)
 	if err != nil {
