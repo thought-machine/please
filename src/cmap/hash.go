90ea68837b6bbbaa3fb53e2ff2d847a763295298
Peter Ebden <peter.ebden@gmail.com>
2022-03-16 21:13:10 +0000

    lil' comment

--- src/cmap/hash.go
+++ src/cmap/hash.go
@@ -4,6 +4,7 @@ const prime32 = 16777619
 const initial = uint32(2166136261)
 
 // Fnv32 returns a 32-bit FNV-1 hash of a string.
+// This is a convenient hash function for a Map.
 func Fnv32(s string) uint32 {
 	hash := initial
 	for i := 0; i < len(s); i++ {
@@ -14,6 +15,7 @@ func Fnv32(s string) uint32 {
 }
 
 // Fnv32s returns a 32-bit FNV-1 hash of a series of strings.
+// This is a convenient hash function for a Map based on a struct containing multiple strings.
 func Fnv32s(s ...string) uint32 {
 	hash := initial
 	for _, x := range s {
