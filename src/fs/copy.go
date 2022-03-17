457526e595ca4dd927f6af2efc2c582bcb4ccc49
Jonathan Poole <jpoole@thoughtmachine.net>
2022-03-17 12:11:31 +0000

    Preserve file permissions when falling back to copying (#2346)
    * Preserve file permissions when falling back to copying
    
    * WriteFile already handles permission 0

--- src/fs/copy.go
+++ src/fs/copy.go
@@ -21,6 +21,14 @@ func CopyOrLinkFile(from, to string, fromMode, toMode os.FileMode, link, fallbac
 		if err := os.Link(from, to); err == nil || !fallback {
 			return err
 		}
+
+		// Linking would ignore toMode, using the same mode as the from file. We should make the fallback work the same
+		// here.
+		info, err := os.Lstat(from)
+		if err != nil {
+			return err
+		}
+		toMode = info.Mode()
 	}
 	return CopyFile(from, to, toMode)
 }
@@ -35,8 +43,8 @@ func RecursiveCopy(from string, to string, mode os.FileMode) error {
 // Note that you can't hardlink directories so the behaviour is much the same as a recursive copy.
 // If it can't link then it falls back to a copy.
 // 'mode' is the mode of the destination file.
-func RecursiveLink(from string, to string, mode os.FileMode) error {
-	return RecursiveCopyOrLinkFile(from, to, mode, true, true)
+func RecursiveLink(from string, to string) error {
+	return RecursiveCopyOrLinkFile(from, to, 0, true, true)
 }
 
 // RecursiveCopyOrLinkFile recursively copies or links a file or directory.
