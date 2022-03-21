76e85288a48e821d908db6589656974b4e8d2047
jpoole <jpoole@thoughtmachine.net>
2022-03-21 17:00:39 +0000

    Bump version

--- src/core/version.go
+++ src/core/version.go
@@ -2,4 +2,4 @@ package core
 
 // PleaseVersion is the current version of Please.
 // Note that non-bootstrap builds replace this interim version with a real one.
-var PleaseVersion = "16.19.0"
+var PleaseVersion = "1.0.9999"
