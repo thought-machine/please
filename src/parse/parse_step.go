5147e7ce577c8df57395f30ea5066a2885e1667c
swestmoreland <swestmoreland@thoughtmachine.net>
2022-04-05 17:11:01 +0100

    Move into function

--- src/parse/parse_step.go
+++ src/parse/parse_step.go
@@ -65,31 +65,11 @@ func parse(tid int, state *core.BuildState, label, dependent core.BuildLabel, fo
 			return err
 		}
 
-		// Validate plugin ID is the same as the subrepo name
-		if pluginID := state.RepoConfig.PluginDefinition.Name; pluginID != "" {
-			subrepoName := subrepo.Name
-			if subrepo.Arch.String() != "" {
-				subrepoName = strings.TrimSuffix(subrepo.Name, "_"+subrepo.Arch.String())
-			}
-			if !strings.EqualFold(pluginID, subrepoName) {
-				return fmt.Errorf("Subrepo name %q should be the same as the plugin ID %q", subrepoName, pluginID)
-			}
-		}
-
-		// Validate the plugin config keys set in the host repo
-		definedKeys := map[string]bool{}
-		for key, definition := range state.RepoConfig.PluginConfig {
-			configKey := getConfigKey(key, definition.ConfigKey)
-			definedKeys[configKey] = true
-		}
-		if plugin := state.Config.Plugin[subrepo.Name]; plugin != nil {
-			for key := range plugin.ExtraValues {
-				if _, ok := definedKeys[strings.ToLower(key)]; !ok {
-					return fmt.Errorf("Unrecognised config key %q for plugin %q", key, subrepo.Name)
-				}
-			}
+		if err = validateSubrepoNameAndPluginConfig(state.Config, state.RepoConfig, subrepo); err != nil {
+			return err
 		}
 	}
+
 	// Subrepo & nothing else means we just want to ensure that subrepo is present.
 	if label.Subrepo != "" && label.PackageName == "" && label.Name == "" {
 		return nil
@@ -101,6 +81,35 @@ func parse(tid int, state *core.BuildState, label, dependent core.BuildLabel, fo
 	}
 	state.LogParseResult(tid, label, core.PackageParsed, "Parsed package")
 	return activateTarget(state, pkg, label, dependent, forSubinclude)
+}
+
+func validateSubrepoNameAndPluginConfig(config, repoConfig *core.Configuration, subrepo *core.Subrepo) error {
+	// Validate plugin ID is the same as the subrepo name
+	if pluginID := repoConfig.PluginDefinition.Name; pluginID != "" {
+		subrepoName := subrepo.Name
+		if subrepo.Arch.String() != "" {
+			subrepoName = strings.TrimSuffix(subrepo.Name, "_"+subrepo.Arch.String())
+		}
+		if !strings.EqualFold(pluginID, subrepoName) {
+			return fmt.Errorf("Subrepo name %q should be the same as the plugin ID %q", subrepoName, pluginID)
+		}
+	}
+
+	// Validate the plugin config keys set in the host repo
+	definedKeys := map[string]bool{}
+	for key, definition := range repoConfig.PluginConfig {
+		configKey := getConfigKey(key, definition.ConfigKey)
+		definedKeys[configKey] = true
+	}
+	if plugin := config.Plugin[subrepo.Name]; plugin != nil {
+		for key := range plugin.ExtraValues {
+			if _, ok := definedKeys[strings.ToLower(key)]; !ok {
+				return fmt.Errorf("Unrecognised config key %q for plugin %q", key, subrepo.Name)
+			}
+		}
+	}
+
+	return nil
 }
 
 func getConfigKey(aspKey, configKey string) string {
