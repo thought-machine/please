6e205c5c08c7b1560412c59e2ddbb58a0d224796
Peter Ebden <peter.ebden@gmail.com>
2022-05-31 10:12:02 +0100

    Add a test case for embedding a directory

--- test/go_rules/embed/embed_test.go
+++ test/go_rules/embed/embed_test.go
@@ -10,3 +10,9 @@ import (
 func TestLibEmbed(t *testing.T) {
 	assert.Equal(t, "hello", strings.TrimSpace(hello))
 }
+
+func TestLibEmbedDir(t *testing.T) {
+	b, err := testData.ReadFile("test_data/text.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, "hello world", strings.TrimSpace(string(b)))
+}
