97076ac7fefa3b14a7fbb63f266450b5dc0bbe37
Jonathan Poole <jpoole@thoughtmachine.net>
2022-12-19 12:12:22 +0000

    Allow subrepos to output into a package root to avoid collisions (#2605)
    * Allow subrepos to output into a package root to avoid collisions
    
    * Fix the package root truthy test
    
    * Fix a couple things

--- src/parse/asp/builtins.go
+++ src/parse/asp/builtins.go
@@ -1075,6 +1075,7 @@ func subrepo(s *scope, args []pyObject) pyObject {
 		BazelCompatArgIdx
 		ArchArgIdx
 		PluginArgIdx
+		PackageRootIdx
 	)
 
 	s.NAssert(s.pkg == nil, "Cannot create new subrepos in this scope")
@@ -1120,6 +1121,10 @@ func subrepo(s *scope, args []pyObject) pyObject {
 		state = state.ForArch(arch)
 		isCrossCompile = true
 	}
+	pkgRoot := ""
+	if args[PackageRootIdx].IsTruthy() {
+		pkgRoot = args[PackageRootIdx].String()
+	}
 
 	// Subrepo
 	sr := &core.Subrepo{
@@ -1129,6 +1134,7 @@ func subrepo(s *scope, args []pyObject) pyObject {
 		State:          state,
 		Arch:           arch,
 		IsCrossCompile: isCrossCompile,
+		PackageRoot:    pkgRoot,
 	}
 
 	// Typically this would be deferred until we have built the subrepo target and have its config available. As we
