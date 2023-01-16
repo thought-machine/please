081da6949c9019796d5bc1f6448f739e97fb18f4
Jonathan Poole <jpoole@thoughtmachine.net>
2023-01-16 14:36:53 +0000

    Set GOOGLE_APPLICATION_CREDENTIALS for release step (#2640)

--- .circleci/release_s3.sh
+++ .circleci/release_s3.sh
@@ -23,9 +23,11 @@ aws s3 sync /tmp/workspace/linux_amd64 s3://please-releases/linux_amd64/$VERSION
 aws s3 sync /tmp/workspace/freebsd_amd64 s3://please-releases/freebsd_amd64/$VERSION
 
 # Sign the download script with our release key
-/tmp/workspace/release_signer -o get_plz.sh.asc -i tools/misc/get_plz.sh
+/tmp/workspace/release_signer pgp -o get_plz.sh.asc -i tools/misc/get_plz.sh
+/tmp/workspace/release_signer kms -o get_plz.sh.sig -i tools/misc/get_plz.sh
 aws s3 cp tools/misc/get_plz.sh s3://please-releases/get_plz.sh --content-type text/x-shellscript
 aws s3 cp get_plz.sh.asc s3://please-releases/get_plz.sh.asc --content-type text/plain
+aws s3 cp get_plz.sh.sig s3://please-releases/get_plz.sh.sig --content-type application/octet-stream
 
 if [[ "$VERSION" == *"beta"* ]] || [[ "$VERSION" == *"alpha"* ]] || [[ "$VERSION" == *"prerelease"* ]]; then
   echo "$VERSION is a prerelease, only setting latest_prerelease_version"
