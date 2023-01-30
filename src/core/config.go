bcf89c7c018b895bfd4c267c4d155b088ea5b6e0
swestmoreland <swestmoreland@thoughtmachine.net>
2023-01-30 12:56:15 +0000

    Delete shell rules ff as well

--- src/core/config.go
+++ src/core/config.go
@@ -696,7 +696,6 @@ type Configuration struct {
 		ExcludeJavaRules         bool `help:"Whether to include the java rules or use the plugin"`
 		ExcludeCCRules           bool `help:"Whether to include the C and C++ rules or require use of the plugin"`
 		ExcludeGoRules           bool `help:"Whether to include the go rules rules or require use of the plugin"`
-		ExcludeShellRules        bool `help:"Whether to include the shell rules rules or require use of the plugin"`
 		ExcludeProtoRules        bool `help:"Whether to include the proto rules or require use of the plugin"`
 		ExcludeSymlinksInGlob    bool `help:"Whether to include symlinks in the glob" var:"FF_EXCLUDE_GLOB_SYMLINKS"`
 		GoDontCollapseImportPath bool `help:"If set, we will no longer collapse import paths that have repeat final parts e.g. foo/bar/bar -> foo/bar" var:"FF_GO_DONT_COLLAPSE_IMPORT_PATHS"`
--- src/core/state.go
+++ src/core/state.go
@@ -265,9 +265,6 @@ func (state *BuildState) ExcludedBuiltinRules() map[string]struct{} {
 	if state.Config.FeatureFlags.ExcludeGoRules {
 		ret["go_rules.build_defs"] = struct{}{}
 	}
-	if state.Config.FeatureFlags.ExcludeShellRules {
-		ret["sh_rules.build_defs"] = struct{}{}
-	}
 	if state.Config.FeatureFlags.ExcludeProtoRules {
 		ret["proto_rules.build_defs"] = struct{}{}
 	}
