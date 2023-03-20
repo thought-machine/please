6070643289260bbf4b9284c6aa0c085f5661c91b
Jonathan Poole <jpoole@thoughtmachine.net>
2023-03-20 12:06:33 +0000

    Support for go 1.20 (#2735)
    * Support for go 1.20
    
    * Version bump
    
    * Update release step
    
    * Downgrade go plugin

--- .circleci/release_s3.sh
+++ .circleci/release_s3.sh
@@ -4,10 +4,6 @@ set -eu
 
 VERSION=$(cat VERSION)
 
-echo "Releasing docs website"
-tar -xzf /tmp/workspace/deep-docs.tar.gz -C /tmp/workspace && aws s3 sync /tmp/workspace/docs s3://please-docs
-
-
 if aws s3 ls s3://please-releases/linux_arm64/$VERSION/; then
   echo "Please $VERSION has already been released, nothing to do."
   exit 0
@@ -35,9 +31,8 @@ aws s3 cp get_plz.sh.asc s3://please-releases/get_plz.sh.asc --content-type text
 aws s3 cp get_plz.sh.sig s3://please-releases/get_plz.sh.sig --content-type application/octet-stream
 
 if [[ "$VERSION" == *"beta"* ]] || [[ "$VERSION" == *"alpha"* ]] || [[ "$VERSION" == *"prerelease"* ]]; then
-  echo "$VERSION is a prerelease, only setting latest_prerelease_version"
 else
-  echo "$VERSION is not a prerelease, setting latest_version and latest_prerelease_version"
+  echo "$VERSION is not a prerelease, setting latest_version"
   aws s3 cp VERSION s3://please-releases/latest_version  --content-type text/plain
 fi
-aws s3 cp VERSION s3://please-releases/latest_prerelease_version  --content-type text/plain
+
