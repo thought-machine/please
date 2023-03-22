3462184e4c5b0b30668d5b2cd92a999d50852145
Jonathan Poole <jpoole@thoughtmachine.net>
2023-03-22 16:06:44 +0000

    Remove iter sources ff (#2738)
    * Remove iter sources ff
    
    * Fix test
    
    * Remove feature flag

--- src/core/config.go
+++ src/core/config.go
@@ -694,7 +694,6 @@ type Configuration struct {
 
 	FeatureFlags struct {
 		PackageOutputsStrictness bool `help:"Prevents certain combinations of target outputs within a package that result in nondeterminist behaviour"`
-		NoIterSourcesMarked      bool `help:"Don't mark sources as done when iterating inputs" var:"FF_NO_ITER_SOURCES_MARKED"`
 		ExcludeProtoRules        bool `help:"Whether to include the proto rules or require use of the plugin"`
 		ExcludeSymlinksInGlob    bool `help:"Whether to include symlinks in the glob" var:"FF_EXCLUDE_GLOB_SYMLINKS"`
 	} `help:"Flags controlling preview features for the next release. Typically these config options gate breaking changes and only have a lifetime of one major release."`
