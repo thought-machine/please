00c12ff06a79ee27b171e4a74943d8979bcc6a14
Hugues Morisset <morisset.hugues@gmail.com>
2023-05-25 17:34:41 +0200

    Can generate dot graph on query deps command
    Add an option --dot on query deps which
    generate a dot compatible graph.
    
    Graph nodes will have different shapes depending
    if it's a filegroup/remote_file/text_file/binary
    
    One can get an image with
    `plz query deps //src:please --dot | dot -Tpng > graph.png`
    
    issue #2777

--- src/query/deps.go
+++ src/query/deps.go
@@ -9,14 +9,30 @@ import (
 )
 
 // Deps prints all transitive dependencies of a set of targets.
-func Deps(state *core.BuildState, labels []core.BuildLabel, hidden bool, targetLevel int) {
-	deps(os.Stdout, state, labels, hidden, targetLevel)
+func Deps(state *core.BuildState, labels []core.BuildLabel, hidden bool, targetLevel int, formatdot bool) {
+	deps(os.Stdout, state, labels, hidden, targetLevel, formatdot)
 }
 
-func deps(out io.Writer, state *core.BuildState, labels []core.BuildLabel, hidden bool, targetLevel int) {
+func deps(out io.Writer, state *core.BuildState, labels []core.BuildLabel, hidden bool, targetLevel int, formatdot bool) {
+	if formatdot {
+		fmt.Fprintf(out, "digraph deps {\n")
+		fmt.Fprintf(out, "  fontname=\"Helvetica,Arial,sans-serif\"\n")
+		fmt.Fprintf(out, "  node [fontname=\"Helvetica,Arial,sans-serif\"]\n")
+		fmt.Fprintf(out, "  edge [fontname=\"Helvetica,Arial,sans-serif\"]\n")
+		fmt.Fprintf(out, "  rankdir=\"LR\"\n")
+	}
 	done := map[core.BuildLabel]bool{}
 	for _, label := range labels {
-		printTarget(out, state, state.Graph.TargetOrDie(label), "", done, hidden, 0, targetLevel)
+		if formatdot {
+			fmt.Fprintf(out, "  subgraph \"%s\" {\n", label)
+			printTargetDot(out, state, state.Graph.TargetOrDie(label), nil, done, hidden, 0, targetLevel)
+			fmt.Fprintf(out, "  }\n")
+		} else {
+			printTarget(out, state, state.Graph.TargetOrDie(label), "", done, hidden, 0, targetLevel)
+		}
+	}
+	if formatdot {
+		fmt.Fprintf(out, "}\n")
 	}
 }
 
@@ -35,5 +51,37 @@ func printTarget(out io.Writer, state *core.BuildState, target *core.BuildTarget
 
 	for _, dep := range target.Dependencies() {
 		printTarget(out, state, dep, indent, done, hidden, currentLevel, targetLevel)
+	}
+}
+
+func printTargetDot(out io.Writer, state *core.BuildState, target *core.BuildTarget, parent *core.BuildTarget, done map[core.BuildLabel]bool, hidden bool, currentLevel int, targetLevel int) {
+	levelLimitReached := targetLevel != -1 && currentLevel == targetLevel
+
+	if state.ShouldInclude(target) && (hidden || !target.HasParent()) {
+		if !done[target.Label] {
+			shape := "ellipse"
+			if target.IsFilegroup {
+				shape = "folder"
+			} else if target.IsRemoteFile {
+				shape = "octagon"
+			} else if target.IsTextFile {
+				shape = "note"
+			} else if target.IsBinary {
+				shape = "component"
+			}
+			fmt.Fprintf(out, "   node [shape=%s] \"%s\";\n", shape, target)
+		}
+		if parent != nil {
+			fmt.Fprintf(out, "   \"%s\" -> \"%s\";\n", parent, target)
+		}
+		currentLevel++
+	}
+	if done[target.Label] || levelLimitReached {
+		return
+	}
+	done[target.Label] = true
+
+	for _, dep := range target.Dependencies() {
+		printTargetDot(out, state, dep, target, done, hidden, currentLevel, targetLevel)
 	}
 }
