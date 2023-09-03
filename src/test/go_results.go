5de474b0e3decfe93df1608681376a991db86505
Peter Ebden <peter.ebden@gmail.com>
2023-09-03 13:19:08 +0100

    Fix linter

--- src/test/go_results.go
+++ src/test/go_results.go
@@ -22,42 +22,43 @@ func parseGoTestResults(data []byte) (core.TestSuite, error) {
 	if err != nil {
 		return core.TestSuite{}, err
 	}
-	// We should only get a single package here; because of the way we set up tests, we only get results from one at a time.
-	for _, pkg := range report.Packages {
-		suite := core.TestSuite{
-			Package:    pkg.Name,
-			Duration:   pkg.Duration,
-			Properties: pkg.Properties,
-		}
-		for _, test := range pkg.Tests {
-			execution := core.TestExecution{
-				Duration: &test.Duration,
-			}
-			output := strings.TrimSpace(strings.Join(test.Output, "\n"))
-			switch test.Result {
-			case gtr.Fail:
-				execution.Failure = &core.TestResultFailure{
-					Message: output,
-				}
-			case gtr.Skip:
-				execution.Skip = &core.TestResultSkip{
-					Message: output,
-				}
-			case gtr.Pass:
-				execution.Stdout = output
-			}
-			suite.TestCases = append(suite.TestCases, core.TestCase{
-				Name:       test.Name,
-				Executions: []core.TestExecution{execution},
-			})
-		}
-		// Attach any trailing output to the last failing test case. This happens if e.g. a case panics.
-		if len(suite.TestCases) > 0 {
-			if c := &suite.TestCases[len(suite.TestCases)-1]; c.Executions[0].Failure != nil {
-				c.Executions[0].Failure.Traceback = strings.Join(pkg.Output, "\n")
-			}
-		}
-		return suite, nil
+	if len(report.Packages) == 0 {
+		return core.TestSuite{}, nil
 	}
-	return core.TestSuite{}, nil
+	// We should only get a single package here; because of the way we set up tests, we only get results from one at a time.
+	pkg := report.Packages[0]
+	suite := core.TestSuite{
+		Package:    pkg.Name,
+		Duration:   pkg.Duration,
+		Properties: pkg.Properties,
+	}
+	for _, test := range pkg.Tests {
+		execution := core.TestExecution{
+			Duration: &test.Duration,
+		}
+		output := strings.TrimSpace(strings.Join(test.Output, "\n"))
+		switch test.Result {
+		case gtr.Fail:
+			execution.Failure = &core.TestResultFailure{
+				Message: output,
+			}
+		case gtr.Skip:
+			execution.Skip = &core.TestResultSkip{
+				Message: output,
+			}
+		case gtr.Pass:
+			execution.Stdout = output
+		}
+		suite.TestCases = append(suite.TestCases, core.TestCase{
+			Name:       test.Name,
+			Executions: []core.TestExecution{execution},
+		})
+	}
+	// Attach any trailing output to the last failing test case. This happens if e.g. a case panics.
+	if len(suite.TestCases) > 0 {
+		if c := &suite.TestCases[len(suite.TestCases)-1]; c.Executions[0].Failure != nil {
+			c.Executions[0].Failure.Traceback = strings.Join(pkg.Output, "\n")
+		}
+	}
+	return suite, nil
 }
