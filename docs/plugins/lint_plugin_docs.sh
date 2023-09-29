fdf0195101080eb73be7526e8edcc3a6055d0805
swestmoreland <swestmoreland@thoughtmachine.net>
2023-09-29 16:11:18 +0100

    Check all the plugins for up-to-date docs rather than exiting early

--- docs/plugins/lint_plugin_docs.sh
+++ docs/plugins/lint_plugin_docs.sh
@@ -3,12 +3,12 @@
 # This script gets the latest tags for each of our first class plugin repos
 # It is intended to be run by a github action
 
-plugins=("python" "java" "go" "cc" "shell" "go-proto" "proto")
+PLUGINS=("python" "java" "go" "cc" "shell" "go-proto" "proto")
 
 URLPREFIX="https://github.com/please-build/"
 
-# loop through plugins and check version
-for PLUGIN in "${plugins[@]}"; do
+FAILED=0
+for PLUGIN in "${PLUGINS[@]}"; do
     LATEST=$(git ls-remote --tags ${URLPREFIX}${PLUGIN}-rules.git | sed 's/.*\///' | sed '/^v[0-9]\+\.[0-9]\+\.[0-9]\+$/!d' | tail -n 1)
     if [ -z "$LATEST" ]; then
         echo "No tags found for ${PLUGIN}"
@@ -23,8 +23,13 @@ for PLUGIN in "${plugins[@]}"; do
 
     if [ "$LATEST" != "$DOCS_VERSION" ]; then
         echo "Latest version for ${PLUGIN} is ${LATEST}, update the plugin version in docs/BUILD from ${DOCS_VERSION} to ${LATEST}"
-        exit 1
+        FAILED=1
+        continue
     fi
 
     echo "Latest version for ${PLUGIN} is ${LATEST}. Docs are up to date"
 done
+
+if [ $FAILED -eq 1 ]; then
+    exit 1
+fi
