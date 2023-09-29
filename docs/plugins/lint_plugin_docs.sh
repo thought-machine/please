f7383b5161e525ab022e545688a2b4d1f1cedfe8
swestmoreland <swestmoreland@thoughtmachine.net>
2023-09-29 16:26:11 +0100

    Quote variables

--- docs/plugins/lint_plugin_docs.sh
+++ docs/plugins/lint_plugin_docs.sh
@@ -8,13 +8,13 @@ URLPREFIX="https://github.com/please-build/"
 
 FAILED=0
 for PLUGIN in "${PLUGINS[@]}"; do
-    LATEST=$(git ls-remote --tags ${URLPREFIX}${PLUGIN}-rules.git | sed 's/.*\///' | sed '/^v[0-9]\+\.[0-9]\+\.[0-9]\+$/!d' | tail -n 1)
+    LATEST=$(git ls-remote --tags "${URLPREFIX}""${PLUGIN}"-rules.git | sed 's/.*\///' | sed '/^v[0-9]\+\.[0-9]\+\.[0-9]\+$/!d' | tail -n 1)
     if [ -z "$LATEST" ]; then
         echo "No tags found for ${PLUGIN}"
         exit 1
     fi
 
-    DOCS_VERSION=$(./pleasew query print //docs/... --include ${PLUGIN}_plugin_docs* | grep labels | cut -d: -f2 | cut -d\' -f1)
+    DOCS_VERSION=$(./pleasew query print //docs/... --include "${PLUGIN}"_plugin_docs* | grep labels | cut -d: -f2 | cut -d\' -f1)
     if [ -z "$DOCS_VERSION" ]; then
         echo "No docs found for ${PLUGIN}"
         exit 1
