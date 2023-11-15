2ed5063b6fb7e846afd97e3b8f457278c82d925e
Jonathan Poole <jpoole@thoughtmachine.net>
2023-11-15 19:59:31 +0000

    Implement iofs.StatFS for the CAS FS (#2962)
    * Implement iofs.StatFS for the CAS FS
    
    * lint

--- src/remote/fs/info.go
+++ src/remote/fs/info.go
@@ -10,16 +10,55 @@ import (
 
 // info represents information about a file/directory
 type info struct {
-	name     string
-	isDir    bool
-	size     int64
-	modTime  time.Time
-	mode     os.FileMode
-	typeMode os.FileMode
+	name    string
+	size    int64
+	modTime time.Time
+	mode    os.FileMode
+}
+
+func newFileInfo(f *pb.FileNode) *info {
+	i := info{
+		size: f.Digest.SizeBytes,
+		name: f.Name,
+	}
+	return i.withProperties(f.NodeProperties)
+}
+func newDirInfo(name string, dir *pb.Directory) *info {
+	i := &info{
+		name: name,
+		mode: os.ModeDir,
+	}
+	return i.withProperties(dir.NodeProperties)
+}
+
+func newSymlinkInfo(node *pb.SymlinkNode) *info {
+	i := &info{
+		name: node.Name,
+		mode: os.ModeSymlink,
+	}
+	return i.withProperties(node.NodeProperties)
+}
+
+// withProperties safely sets the node info if it's available.
+func (i *info) withProperties(nodeProperties *pb.NodeProperties) *info {
+	if nodeProperties == nil {
+		return i
+	}
+
+	if nodeProperties.UnixMode != nil {
+		// This should in theory have the type mode set already but we bitwise or here to to make sure this is preserved
+		// from the constructors above in case the remote doesn't set this.
+		i.mode |= os.FileMode(nodeProperties.UnixMode.Value)
+	}
+
+	if nodeProperties.Mtime != nil {
+		i.modTime = nodeProperties.Mtime.AsTime()
+	}
+	return i
 }
 
 func (i *info) Type() iofs.FileMode {
-	return i.typeMode
+	return i.mode.Type()
 }
 
 func (i *info) Info() (iofs.FileInfo, error) {
@@ -43,25 +82,9 @@ func (i *info) ModTime() time.Time {
 }
 
 func (i *info) IsDir() bool {
-	return i.isDir
+	return i.mode.IsDir()
 }
 
 func (i *info) Sys() any {
 	return nil
-}
-
-// withProperties safely sets the node info if it's available.
-func (i *info) withProperties(nodeProperties *pb.NodeProperties) *info {
-	if nodeProperties == nil {
-		return i
-	}
-
-	if nodeProperties.UnixMode != nil {
-		i.mode = os.FileMode(nodeProperties.UnixMode.Value)
-	}
-
-	if nodeProperties.Mtime != nil {
-		i.modTime = nodeProperties.Mtime.AsTime()
-	}
-	return i
 }
