c8739486cc97b4abf574afffd6b4bda9315826b7
Jon Poole <jpoole@thoughtmachine.net>
2023-11-27 11:45:39 +0000

    Don't cross compile for osx

--- .circleci/setup_osx.sh
+++ .circleci/setup_osx.sh
@@ -4,12 +4,12 @@ set -eu
 
 # /usr/local/go might get cached.
 if [ ! -d "/usr/local/go" ]; then
-    curl -fsSL https://dl.google.com/go/go1.19.darwin-amd64.tar.gz | sudo tar -xz -C /usr/local
+    curl -fsSL https://dl.google.com/go/go1.21.3.darwin-$1.tar.gz | sudo tar -xz -C /usr/local
 fi
 sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
 
 # xz might also.
 if [ ! -f "/usr/local/bin/xz" ]; then
-    curl -fsSL https://get.please.build/third_party/binary/xz-5.2.4-darwin_amd64 -o /usr/local/bin/xz
+    curl -fsSL https://get.please.build/third_party/binary/xz-5.2.4-darwin_$1 -o /usr/local/bin/xz
     chmod +x /usr/local/bin/xz
 fi
