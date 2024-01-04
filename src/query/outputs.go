68854b7e58b9f6c4a9038a6457c9244443280e25
Samuel Littley <slittley@thoughtmachine.net>
2024-01-04 14:44:04 +0000

    Add a `--json` flag to `plz query outputs` (#3011)
    * Add a `--json` flag to `plz query outputs`
    
    Produces a map from target name to an array of output files.
    
    Tested using `plz-out/bin/src/please query outputs --json //src:please`
    
    * Fix lint
    
    * Switch to `json.Encoder` for indentation and streamed output

--- src/query/outputs.go
+++ src/query/outputs.go
@@ -1,18 +1,43 @@
 package query
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/thought-machine/please/src/core"
 )
 
 // TargetOutputs prints all output files for a set of targets.
-func TargetOutputs(graph *core.BuildGraph, labels []core.BuildLabel) {
+func TargetOutputs(graph *core.BuildGraph, labels []core.BuildLabel, useJSON bool) {
+	if useJSON {
+		targetOutputsJSON(graph, labels)
+	} else {
+		targetOutputsFlat(graph, labels)
+	}
+}
+
+func targetOutputsFlat(graph *core.BuildGraph, labels []core.BuildLabel) {
 	for _, label := range labels {
 		target := graph.TargetOrDie(label)
 		for _, out := range target.Outputs() {
 			fmt.Printf("%s\n", filepath.Join(target.OutDir(), out))
 		}
+	}
+}
+
+func targetOutputsJSON(graph *core.BuildGraph, labels []core.BuildLabel) {
+	data := map[string][]string{}
+	for _, label := range labels {
+		target := graph.TargetOrDie(label)
+		for _, out := range target.Outputs() {
+			data[label.String()] = append(data[label.String()], filepath.Join(target.OutDir(), out))
+		}
+	}
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(data); err != nil {
+		log.Fatalf("failed to write JSON: %v", err)
 	}
 }
