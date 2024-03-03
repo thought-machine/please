6ae2c3eecd11abe88bf957ac1c1e0fd7d92e1176
Peter Ebden <peter.ebden@gmail.com>
2024-03-03 11:58:05 +0000

    restructure test a bit

--- src/parse/asp/interpreter_test.go
+++ src/parse/asp/interpreter_test.go
@@ -671,7 +671,9 @@ func TestLogConfigVariable(t *testing.T) {
 func TestOperatorPrecedence(t *testing.T) {
 	s, err := parseFile("src/parse/asp/test_data/interpreter/operator_precedence.build")
 	assert.NoError(t, err)
-	assert.EqualValues(t, 15, s.Lookup("x"))
-	assert.EqualValues(t, 53, s.Lookup("y"))
-	assert.EqualValues(t, 32, s.Lookup("z"))
+	assert.EqualValues(t, 15, s.Lookup("a"))
+	assert.EqualValues(t, 53, s.Lookup("b"))
+	assert.EqualValues(t, 32, s.Lookup("c"))
+	assert.EqualValues(t, False, s.Lookup("d"))
+	assert.EqualValues(t, False, s.Lookup("e"))
 }
