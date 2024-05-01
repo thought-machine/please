dd93587469b963d0264d2f8133b55db14e6ba6a6
Peter Ebden <peter.ebden@gmail.com>
2024-05-01 12:07:51 +0100

    Attempt to switch circleci to apple silicon (#3142)
    * Attempt to switch circleci to apple silicon
    
    * change script, hopefully I don't need xz
    
    * make test work for arm64 as well
    
    * use fixed plugin
    
    * try installing xz via brew
    
    * Put it on the global path
    
    * try this
    
    * I think Clang spells it this way
    
    * try this
    
    * continue trial and error
    
    * this seems better

--- .circleci/setup_osx.sh
+++ .circleci/setup_osx.sh
@@ -4,12 +4,12 @@ set -eu
 
 # /usr/local/go might get cached.
 if [ ! -d "/usr/local/go" ]; then
-    curl -fsSL https://dl.google.com/go/go1.21.5.darwin-amd64.tar.gz | sudo tar -xz -C /usr/local
+    curl -fsSL https://dl.google.com/go/go1.21.5.darwin-arm64.tar.gz | sudo tar -xz -C /usr/local
 fi
 sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
 
 # xz might also.
-if [ ! -f "/usr/local/bin/xz" ]; then
-    curl -fsSL https://get.please.build/third_party/binary/xz-5.2.4-darwin_amd64 -o /usr/local/bin/xz
-    chmod +x /usr/local/bin/xz
+if ! command -v xz &> /dev/null; then
+    brew install xz
 fi
+sudo ln -s /opt/homebrew/bin/xz /usr/local/bin/xz
--- test/config_rules/config_test.py
+++ test/config_rules/config_test.py
@@ -5,5 +5,5 @@ import unittest
 class ConfigTest(unittest.TestCase):
 
     def test_flag_matches(self):
-        """Test the flag matches as expected. It's always x86_64 because we don't build Please for x86."""
-        self.assertEqual('--arch=x86_64', sys.argv[-1])
+        """Test the flag matches as expected."""
+        self.assertEqual('--word_size=64', sys.argv[-1])
