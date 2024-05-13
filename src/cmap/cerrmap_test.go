82e45458af547bcaebc062a81d6543610d6fa86a
Peter Ebden <peter.ebden@gmail.com>
2024-05-13 15:03:31 +0100

    Fix test

--- src/cmap/cerrmap_test.go
+++ src/cmap/cerrmap_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestErrMap(t *testing.T) {
-	m := NewErrMap[int, int](DefaultShardCount, hashInts)
+	m := NewErrMap[int, int](DefaultShardCount, hashInts, nil)
 	assert.True(t, m.Add(5, 7))
 	assert.True(t, m.Add(7, 5))
 	err := fmt.Errorf("it broke")
@@ -21,18 +21,14 @@ func TestErrMap(t *testing.T) {
 }
 
 func TestErrWait(t *testing.T) {
-	m := NewErrMap[int, int](DefaultShardCount, hashInts)
-	v, ch, first, err := m.GetOrWait(5)
-	assert.Equal(t, 0, v) // Should be the zero value
-	assert.True(t, first) // We're the first to request it
+	m := NewErrMap[int, int](DefaultShardCount, hashInts, nil)
+	v, err := m.GetOrSet(5, func() (int, error) {
+		return 5, nil
+	})
+	assert.Equal(t, 5, v)
 	assert.NoError(t, err)
-	go func() {
-		m.SetError(5, fmt.Errorf("it broke"))
-	}()
-	<-ch
-	v, ch, first, err = m.GetOrWait(5)
-	assert.Equal(t, 0, v)
-	assert.Nil(t, ch)
-	assert.False(t, first)
+	_, err = m.GetOrSet(7, func() (int, error) {
+		return 0, fmt.Errorf("it broke")
+	})
 	assert.Error(t, err)
 }
