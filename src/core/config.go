ffc312aed269caf3054d38bab0e01be62f08c1d3
Peter Ebden <peter.ebden@gmail.com>
2024-07-03 10:36:14 +0100

    Add config value to make optional outputs required for remote execution

--- src/core/config.go
+++ src/core/config.go
@@ -570,6 +570,7 @@ type Configuration struct {
 		Secure        bool         `help:"Whether to use TLS for communication or not."`
 		VerifyOutputs bool         `help:"Whether to verify all outputs are present after a cached remote execution action. Depending on your server implementation, you may require this to ensure files are really present."`
 		UploadDirs    bool         `help:"Uploads individual directory blobs after build actions. This might not be necessary with some servers, but if you aren't sure, you should leave it on."`
+		OptionalOutputsRequired bool `help:"Requires that any optional outputs of build actions (optional test outputs, coverage when not opted out of) are produced. By default this is a non-fatal failure, but the actions may not cache remotely."`
 		Shell         string       `help:"Path to the shell to use to execute actions in. Default is 'bash' which will be looked up by the server."`
 		Platform      []string     `help:"Platform properties to request from remote workers, in the format key=value."`
 		CacheDuration cli.Duration `help:"Length of time before we re-check locally cached build actions. Default is unlimited."`
