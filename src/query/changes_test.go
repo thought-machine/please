735c452dbc4b0acc486fd22882d810bac8c84c29
Peter Ebden <peter.ebden@gmail.com>
2024-07-03 17:47:01 +0100

    add a test

--- src/query/changes_test.go
+++ src/query/changes_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/thought-machine/please/src/core"
+	"github.com/thought-machine/please/src/cli"
 )
 
 func TestDiffGraphs(t *testing.T) {
@@ -77,6 +78,25 @@ func TestDiffGraphsIncludeTransitive(t *testing.T) {
 	assert.EqualValues(t, core.BuildLabels{t1.Label, t2.Label, t3.Label}, DiffGraphs(s1, s2, nil, -1, false))
 }
 
+func TestDiffGraphsVsSubrepos(t *testing.T) {
+	s1 := core.NewDefaultBuildState()
+	s2 := core.NewDefaultBuildState()
+	t1 := addTarget(s1, "//:modfile", nil, "go.mod")
+	t2 := addTarget(s1, "//third_party/go:mod", t1)
+	t3 := addTarget(s1, "///third_party/go/mod//:mod", nil)
+	t3.Subrepo = core.NewSubrepo(s1, "go_mod", "third_party/go", t2, cli.Arch{}, false)
+	addTarget(s1, "//src/core:core", t3)
+
+	t1 = addTarget(s2, "//:modfile", nil, "go.mod")
+	t2 = addTarget(s2, "//third_party/go:mod", t1)
+	t3 = addTarget(s2, "///third_party/go/mod//:mod", nil)
+	t3.Subrepo = core.NewSubrepo(s2, "go_mod", "third_party/go", t2, cli.Arch{}, false)
+	addTarget(s2, "//src/core:core", t3)
+
+	// t3 should not be changed here - its subrepo has but we should see that the targets generated in it are still identical
+	assert.EqualValues(t, []core.BuildLabel{t1.Label, t2.Label}, DiffGraphs(s1, s2, []string{"go.mod"}, -1, false))
+}
+
 func TestChangesIncludesDataDirs(t *testing.T) {
 	s := core.NewDefaultBuildState()
 	t1 := addTarget(s, "//src/core:core", nil, "src/core/core.go")
@@ -119,7 +139,7 @@ func addTarget(state *core.BuildState, label string, dep *core.BuildTarget, sour
 	}
 	pkg := state.Graph.PackageByLabel(t.Label)
 	if pkg == nil {
-		pkg = core.NewPackage(t.Label.PackageName)
+		pkg = core.NewPackageSubrepo(t.Label.PackageName, t.Label.Subrepo)
 		state.Graph.AddPackage(pkg)
 	}
 	pkg.AddTarget(t)
