18f624031f7335a33441b3753990d1935260e7e4
Peter Ebden <peter.ebden@gmail.com>
2024-07-03 17:24:37 +0100

    Prevent query changesfrom propagating through subrepos

--- src/query/changes.go
+++ src/query/changes.go
@@ -68,7 +68,7 @@ func changedTargets(state *core.BuildState, files []string, changed map[*core.Bu
 	}
 
 	if level != 0 {
-		revdeps := FindRevdeps(state, labels, true, false, level)
+		revdeps := FindRevdeps(state, labels, true, false, includeSubrepos, level)
 		for dep := range revdeps {
 			if _, present := changed[dep]; !present {
 				labels = append(labels, dep.Label)
