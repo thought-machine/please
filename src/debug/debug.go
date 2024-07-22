119ab1c1b0868d133f36d2aa13fff2a7099ea3eb
Alexander Garbett <agarbett@thoughtmachine.net>
2024-07-22 16:00:12 +0100

    Add sharing options to debug as well

--- src/debug/debug.go
+++ src/debug/debug.go
@@ -12,7 +12,7 @@ import (
 
 var log = logging.Log
 
-func Debug(state *core.BuildState, label core.BuildLabel, args, env []string) int {
+func Debug(state *core.BuildState, label core.BuildLabel, args, env []string, shareNetwork, shareMount bool) int {
 	target := state.Graph.TargetOrDie(label)
 	if len(target.Debug.Command) == 0 {
 		log.Fatalf("The build definition used by %s doesn't appear to support debugging yet", target.Label)
@@ -21,7 +21,7 @@ func Debug(state *core.BuildState, label core.BuildLabel, args, env []string) in
 	// Runtime directory.
 	dir := filepath.Join(core.OutDir, "debug", target.Label.Subrepo, target.Label.PackageName)
 
-	sandbox := target.Sandbox
+	sandbox := target.Sandbox || !shareMount
 	if target.IsTest() {
 		sandbox = target.Test.Sandbox
 		env = append(env, "TESTS="+strings.Join(args, " "))
@@ -31,7 +31,7 @@ func Debug(state *core.BuildState, label core.BuildLabel, args, env []string) in
 
 	// The value of `port` takes priority in deciding whether the network namespace should
 	// be shared or not, otherwise clients (i.e. IDEs) might not be able to connect to the debugger.
-	shareNetwork := state.DebugPort != 0 || !sandbox
+	shareNetwork = shareNetwork || state.DebugPort != 0 || !sandbox
 
 	return exec.Exec(state, core.AnnotatedOutputLabel{BuildLabel: label}, dir, env, cmd, nil, state.DebugPort == 0, process.NewSandboxConfig(!shareNetwork, sandbox))
 }
