a44eb2482d48246219257cd4cc97d6be4507d9ff
Alexander Garbett <agarbett@thoughtmachine.net>
2024-07-22 16:08:31 +0100

    Slightly clear up naming

--- src/debug/debug.go
+++ src/debug/debug.go
@@ -21,9 +21,9 @@ func Debug(state *core.BuildState, label core.BuildLabel, args, env []string, sh
 	// Runtime directory.
 	dir := filepath.Join(core.OutDir, "debug", target.Label.Subrepo, target.Label.PackageName)
 
-	sandbox := target.Sandbox || !shareMount
+	targetSandbox := target.Sandbox
 	if target.IsTest() {
-		sandbox = target.Test.Sandbox
+		targetSandbox = target.Test.Sandbox
 		env = append(env, "TESTS="+strings.Join(args, " "))
 	}
 	// Append passed in arguments to the debug command.
@@ -31,7 +31,8 @@ func Debug(state *core.BuildState, label core.BuildLabel, args, env []string, sh
 
 	// The value of `port` takes priority in deciding whether the network namespace should
 	// be shared or not, otherwise clients (i.e. IDEs) might not be able to connect to the debugger.
-	shareNetwork = shareNetwork || state.DebugPort != 0 || !sandbox
+	shareNetwork = shareNetwork || state.DebugPort != 0 || !targetSandbox
+	shareMount = shareMount || !targetSandbox
 
-	return exec.Exec(state, core.AnnotatedOutputLabel{BuildLabel: label}, dir, env, cmd, nil, state.DebugPort == 0, process.NewSandboxConfig(!shareNetwork, sandbox))
+	return exec.Exec(state, core.AnnotatedOutputLabel{BuildLabel: label}, dir, env, cmd, nil, state.DebugPort == 0, process.NewSandboxConfig(!shareNetwork, !shareMount))
 }
