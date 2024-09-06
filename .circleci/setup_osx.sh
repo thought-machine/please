2f9c841aae2e466b21044ebc9e1845eb70aeb492
Peter Ebden <peter.ebden@gmail.com>
2024-09-06 12:02:21 +0100

    Update OSX too

--- .circleci/setup_osx.sh
+++ .circleci/setup_osx.sh
@@ -4,7 +4,7 @@ set -eu
 
 # /usr/local/go might get cached.
 if [ ! -d "/usr/local/go" ]; then
-    curl -fsSL https://dl.google.com/go/go1.21.5.darwin-arm64.tar.gz | sudo tar -xz -C /usr/local
+    curl -fsSL https://dl.google.com/go/go1.23.1.darwin-arm64.tar.gz | sudo tar -xz -C /usr/local
 fi
 sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
 
