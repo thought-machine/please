dab80b1806f75bb46d9bf30c13d2098977ca5351
Peter Ebden <peter.ebden@gmail.com>
2024-10-08 16:46:02 +0100

    Make = not a word break character

--- tools/misc/plz_complete.sh
+++ tools/misc/plz_complete.sh
@@ -8,10 +8,10 @@
 
 _plz_complete_bash() {
     COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
-    LINE="${COMP_LINE:0:$COMP_POINT}"
-    ARGS="${LINE#$COMP_WORDS[0]}"
+    COMP_WORDBREAKS=${COMP_WORDBREAKS//=}
+    args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
     local IFS=$'\n'
-    COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} -p -v 0 --noupdate "$ARGS"))
+    COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} -p -v 0 --noupdate "${args[@]}"))
     return 0
 }
 
