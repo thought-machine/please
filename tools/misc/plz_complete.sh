04cbb40e38ae156a2d762a55e8e60680513cebc1
Peter Ebden <peter.ebden@gmail.com>
2024-10-10 12:00:01 +0100

    Fix bash completion around = character (#3269)
    * Update complation script to avoid breaking words
    
    * Make = not a word break character

--- tools/misc/plz_complete.sh
+++ tools/misc/plz_complete.sh
@@ -8,6 +8,7 @@
 
 _plz_complete_bash() {
     COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
+    COMP_WORDBREAKS=${COMP_WORDBREAKS//=}
     args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
     local IFS=$'\n'
     COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} -p -v 0 --noupdate "${args[@]}"))
