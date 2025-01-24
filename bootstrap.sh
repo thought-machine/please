3997e5601b705d79f78d1e2f57776225d62a49ac
Jonathan Poole <jpoole@thoughtmachine.net>
2025-01-24 12:35:18 +0000

    Log at notice level during CI (#3338)
    * Log at notice level during CI
    
    * Do it this way so it doesn't affect the e2e tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ PLZ_ARGS="${PLZ_ARGS:-}"
 
 # Now invoke Go to run Please to build itself.
 notice "Bootstrapping please..."
-go run -race src/please.go $PLZ_ARGS --log_file plz-out/log/bootstrap_build.log build //src:please
+go run -race src/please.go -p -v2 $PLZ_ARGS --log_file plz-out/log/bootstrap_build.log build //src:please
 
 if [ $# -gt 0 ] && [ "$1" == "--skip_tests" ]; then
     notice "Skipping tests... done."
