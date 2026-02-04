57a4e178bb3523dcdd9fba8bcc5d76db8ca9654e
Andrzej J. Skalski <andrzej.j.skalski@gmail.com>
2026-02-04 18:26:56 +0100

    fix: set only one hash per version to "arcat" binary for compatibility with Buildbarn and SRI standard  (#3472)
    Buildbarn assumes that only one SHA per binary is provided, or the SHAs
    are of different algorithms.
    
    To enable Buildbarn as a backend to Please, this change is necessary.
    
    With this change, current Buildbarn works as a backend.
    
    ---------
    
    Co-authored-by: Andrzej J Skalski <gitstuff@s5i.ch>

--- src/parse/internal_package.go
+++ src/parse/internal_package.go
@@ -27,15 +27,33 @@ func GetInternalPackage(config *core.Configuration) (string, error) {
 		url = fmt.Sprintf("%s/%s_%s/%s/please_tools_%s.tar.xz", config.Please.DownloadLocation, runtime.GOOS, runtime.GOARCH, version.PleaseVersion, version.PleaseVersion)
 	}
 
+	var arcatHash string
+	switch fmt.Sprintf("%s_%s", runtime.GOOS, runtime.GOARCH) {
+	case "darwin_amd64":
+		arcatHash = "6af2cf108592535701aa9395f3a5deeb48a5dfbe8174a8ebe3d56bb93de2c255"
+	case "darwin_arm64":
+		arcatHash = "5070ef05d14c66a85d438f400c6ff734a23833929775d6824b69207b704034bf"
+	case "freebsd_amd64":
+		arcatHash = "05ad6ac45be3a4ca1238bb1bd09207a596f8ff5f885415f8df4ff2dc849fa04e"
+	case "linux_amd64":
+		arcatHash = "aec85425355291e515cd10ac0addec3a5bc9e05c9d07af01aca8c34aaf0f1222"
+	case "linux_arm64":
+		arcatHash = "8266cb95cc84b23642bca6567f8b4bd18de399c887cb5845ab6a901d0dba54d2"
+	default:
+		return "", fmt.Errorf("arcat tool not supported for platform: %s_%s", runtime.GOOS, runtime.GOARCH)
+	}
+
 	data := struct {
-		ToolsURL string
-		Tools    []string
+		ToolsURL  string
+		Tools     []string
+		ArcatHash string
 	}{
 		ToolsURL: url,
 		Tools: []string{
 			"build_langserver",
 			"please_sandbox",
 		},
+		ArcatHash: arcatHash,
 	}
 
 	var buf bytes.Buffer
