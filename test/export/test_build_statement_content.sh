b071dad8871d8ebc5a73b14a69865ff302871930
duarte <duarte@thoughtmachine.net>
2026-03-19 17:11:51 +0000

    test: move to export_e2e build rule

--- test/export/test_build_statement_content.sh
+++ test/export/test_build_statement_content.sh
@@ -1,29 +1,25 @@
 #!/usr/bin/env bash
 
-
 gen_file=$1
+orig_file=$2
+stmt_identifier=$3
+
 if ! test -f "$gen_file"; then
   echo "$gen_file" doesnt exist
   exit 1
 fi
 
-# Statement identifier of type path/file:name
-stmt_identifier=$2
-# string BEFORE the last colon
-file_path="${stmt_identifier%:*}"
-# string AFTER the last colon
-statement_name="${stmt_identifier##*:}"
-
-if ! test -f "$file_path"; then
-  echo "$file_path" doesnt exist
+if ! test -f "$orig_file"; then
+  echo "$orig_file" doesnt exist
   exit 1
 fi
 
 # Extract the block into a variable
-ORIG_CONTENT=$(sed -n "/# Start BStmt ${statement_name}/,/# End BStmt/{ /# /d; p; }" "$file_path")
+ORIG_CONTENT=$(sed -n "/# Start BStmt ${stmt_identifier}/,/# End BStmt/{ /# /d; p; }" "$orig_file")
 
 if ! grep -Fq "$ORIG_CONTENT" "$gen_file"; then
-  printf '%s\n%s\n%s\n%s\n%s\n' \
+  printf '%s\n%s\n%s\n%s\n%s\n%s\n' \
+    "BUILD statements mismatch" \
     "${gen_file} doesnt contain" \
     "${ORIG_CONTENT}" \
     "---- it contains ----" \
