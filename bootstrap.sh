c4d8681052e37abe2ba4d5754ccacf083b43969a
Jan Lorenz <jan.lorenz@pascom.net>
2026-06-08 21:20:37 +0200

    Bootstrap with CGO disabled, same setting as final build

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,10 @@ source ./log.sh
 PLZ_ARGS="${PLZ_ARGS:-}"
 
 # Now invoke Go to run Please to build itself.
+# CGO_ENABLED=0 avoids segfaults from go-m1cpu's IOKit code on Apple Silicon during bootstrap.
+# The final binary built by Please also disables CGO, so this is consistent.
 notice "Bootstrapping please..."
-go run -race src/please.go -p -v2 $PLZ_ARGS --log_file plz-out/log/bootstrap_build.log build //src:please
+CGO_ENABLED=0 go run -race src/please.go -p -v2 $PLZ_ARGS --log_file plz-out/log/bootstrap_build.log build //src:please
 
 if [ $# -gt 0 ] && [ "$1" == "--skip_tests" ]; then
     notice "Skipping tests... done."
