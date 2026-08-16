193c8bcb76180bf6f4946b6919e3a4b6525ce193
Peter Ebden <peter.ebden@gmail.com>
2026-08-16 10:44:57 +0100

    Fix compile

--- src/parse/asp/main/main.go
+++ src/parse/asp/main/main.go
@@ -233,7 +233,7 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(opts.NumThreads)
 	total := len(opts.Args.BuildFiles)
-	p := asp.NewParser(state, nil, nil)
+	p := asp.NewParser(state, nil)
 
 	log.Debug("Loading built-in build rules...")
 	dir, _ := rules.AllAssets()
