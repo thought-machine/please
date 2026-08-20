867b4653a28ba363ca57a7f3865f40434bb7040b
Peter Ebden <peter.ebden@gmail.com>
2026-08-20 12:00:08 +0100

    Convert cross-compile tests to Go (#3575)
    These are kind of annoying - they require a gcc multilib setup, which I
    guess I used to have, but currently don't, so `plz test //test/...`
    keeps hitting them and failing. (You can of course fix that with
    `--exclude` as bootstrap does, but I never think of it).
    
    Just use Go instead, we know we have it and it can cross-compile out of
    the box. Some minor changes since it can't target `linux_x86` - which
    doesn't specifically matter, but it means the produced binary isn't
    runnable as the old one used to be, so we have to check its architecture
    (which I'm also doing in go to avoid needing extra things like `file`).

--- test/cross_compile/location_test.sh
+++ test/cross_compile/location_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# $DATA is arch_location.txt, whose contents are the $(location) of :record_arch. That must come
+# out the same whether or not we're cross-compiling, which is what #582 was about.
 
-if [ "`cat $DATA`" != "test/cross_compile/arch_location.txt" ]; then
+if [ "`cat $DATA`" != "test/cross_compile/arch.txt" ]; then
     echo "Unexpected contents of file: `cat $DATA`"
     exit 1
 fi
