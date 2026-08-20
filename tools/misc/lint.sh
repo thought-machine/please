df28c69d7184c9e863f2d35d3c02b5ec58f80971
Peter Ebden <peter.ebden@gmail.com>
2026-08-20 09:19:58 +0100

    Update golangci etc

--- tools/misc/lint.sh
+++ tools/misc/lint.sh
@@ -4,7 +4,7 @@ set -eu
 [ -f "plz-out/please/plz" ] && PLZ="plz-out/please/plz" || PLZ="./pleasew"
 
 
-$PLZ run //third_party/binary:golangci-lint -p -- run --sort-results src/... tools/...
+$PLZ run //third_party/binary:golangci-lint -p -- run src/... tools/...
 $PLZ fmt -q || {
     echo "BUILD files are not correctly formatted; run plz fmt -w to fix."
     exit 1
